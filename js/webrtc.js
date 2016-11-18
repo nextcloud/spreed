6d499d716f6ca34ee7d2e0b668fec9e59c469f15
Morris Jobke <hey@morrisjobke.de>
2016-11-18 12:06:03 +0100

    also show error message on event
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- js/webrtc.js
+++ js/webrtc.js
@@ -263,6 +263,11 @@ var spreedMappingTable = [];
 					message = t('spreed', 'Access to microphone & camera was denied');
 					$('#emptycontent p').hide();
 				}
+			} else if(!OCA.SpreedMe.webrtc.capabilities.support) {
+				console.log('WebRTC not supported');
+
+				message = t('spreed', 'WebRTC is not supported in your browser :-/');
+				messageAdditional = t('spreed', 'Please use a different browser like Firefox or Chrome');
 			} else {
 				message = t('spreed', 'Error while accessing microphone & camera');
 				messageAdditional = error.message || error.name;
