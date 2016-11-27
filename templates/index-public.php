83b14ab7d1a5e2b035ed6a2a6e923fbbe43862c3
Julie Anne Noying <jule@tollwerk.de>
2016-11-28 00:44:47 +0100

    personalize caller experience with avatar and name
    Signed-off-by: Julie Anne Noying <jule@tollwerk.de>

--- templates/index-public.php
+++ templates/index-public.php
@@ -64,9 +64,9 @@ script(
 
 
 		<div id="emptycontent">
-			<div class="icon-video"></div>
-			<h2><?php p($l->t('Looking great today! :)')) ?></h2>
-			<p class="uploadmessage"><?php p($l->t('Time to call your friends')) ?></p>
+			<div id="displayavatar"><div class="avatardiv"></div></div>
+			<h2><?php p($l->t('Waiting for %s to join the call', [$_['shareOwner']])) ?></h2>
+			<p class="uploadmessage"><?php p($l->t('Smile in 3… 2… 1!')) ?></p>
 		</div>
 	</div>
 </div>
