f65d6f27a55b46f4ef4fb7dbfe6932794b980baa
Joas Schilling <coding@schilljs.com>
2017-10-27 10:16:04 +0200

    Change tables to talk_*
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Signaling/Messages.php
+++ lib/Signaling/Messages.php
@@ -49,7 +49,7 @@ class Messages {
 	 */
 	public function deleteMessages(array $sessionIds) {
 		$query = $this->db->getQueryBuilder();
-		$query->delete('videocalls_signaling')
+		$query->delete('talk_signaling')
 			->where($query->expr()->in('recipient', $query->createNamedParameter($sessionIds, IQueryBuilder::PARAM_STR_ARRAY)))
 			->orWhere($query->expr()->in('sender', $query->createNamedParameter($sessionIds, IQueryBuilder::PARAM_STR_ARRAY)));
 		$query->execute();
@@ -62,7 +62,7 @@ class Messages {
 	 */
 	public function addMessage($senderSessionId, $recipientSessionId, $message) {
 		$query = $this->db->getQueryBuilder();
-		$query->insert('videocalls_signaling')
+		$query->insert('talk_signaling')
 			->values(
 				[
 					'sender' => $query->createNamedParameter($senderSessionId),
@@ -80,7 +80,7 @@ class Messages {
 	 */
 	public function addMessageForAllParticipants(Room $room, $message) {
 		$query = $this->db->getQueryBuilder();
-		$query->insert('videocalls_signaling')
+		$query->insert('talk_signaling')
 			->values(
 				[
 					'sender' => $query->createParameter('sender'),
@@ -114,7 +114,7 @@ class Messages {
 
 		$query = $this->db->getQueryBuilder();
 		$query->select('*')
-			->from('videocalls_signaling')
+			->from('talk_signaling')
 			->where($query->expr()->eq('recipient', $query->createNamedParameter($sessionId)))
 			->andWhere($query->expr()->lte('timestamp', $query->createNamedParameter($time)));
 		$result = $query->execute();
@@ -125,7 +125,7 @@ class Messages {
 		$result->closeCursor();
 
 		$query = $this->db->getQueryBuilder();
-		$query->delete('videocalls_signaling')
+		$query->delete('talk_signaling')
 			->where($query->expr()->eq('recipient', $query->createNamedParameter($sessionId)))
 			->andWhere($query->expr()->lte('timestamp', $query->createNamedParameter($time)));
 		$query->execute();
@@ -142,7 +142,7 @@ class Messages {
 		$time = $this->time->getTime() - $olderThan;
 
 		$query = $this->db->getQueryBuilder();
-		$query->delete('videocalls_signaling')
+		$query->delete('talk_signaling')
 			->where($query->expr()->lt('timestamp', $query->createNamedParameter($time)));
 		$query->execute();
 	}
