79cc943984fc4745680906c3dc69415cbbfdc91b
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-03-26 13:00:17 +0200

    Damn you firefox autocomplete
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- js/views/callinfoview.js
+++ js/views/callinfoview.js
@@ -28,8 +28,6 @@
 	OCA.SpreedMe = OCA.SpreedMe || {};
 	OCA.SpreedMe.Views = OCA.SpreedMe.Views || {};
 
-	var uiChannel = Backbone.Radio.channel('ui');
-
 	var TEMPLATE =
 		'<div class="room-name"></div>' +
 		'{{#if showShareLink}}' +
@@ -188,7 +186,7 @@
 		},
 
 		renderWhenInactive: function() {
-			if (this.ui.passwordInput.length === 0 || this.ui.passwordInput.val() === '') {
+			if (this.ui.passwordInput.length === 0 || (OC._currentMenu && OC._currentMenu.hasClass('password-menu')) || this.ui.passwordInput.val() === '') {
 				this.render();
 				return;
 			}
