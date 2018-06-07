4c10dbd24b8a25442311fd03320b535a7c5650cf
Mario Danic <mario@lovelyhq.com>
2018-06-07 23:38:29 +0200

    Fixes #956
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- js/richobjectstringparser.js
+++ js/richobjectstringparser.js
@@ -25,7 +25,7 @@
 		 */
 		parseMessage: function(subject, parameters) {
 			var self = this,
-				regex = /\{([a-z0-9-]+)\}/gi,
+				regex = /\B@[a-z0-9_\-@.']+/i,
 				matches = subject.match(regex);
 
 			_.each(matches, function(parameter) {
