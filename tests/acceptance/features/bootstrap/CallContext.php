56a23018f859608f5d8ee0c147eff55189f39b08
Daniel Calviño Sánchez <danxuliu@gmail.com>
2018-06-19 21:45:54 +0200

    Add acceptance tests for one-to-one calls
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- tests/acceptance/features/bootstrap/CallContext.php
+++ tests/acceptance/features/bootstrap/CallContext.php
@@ -91,6 +91,73 @@ class CallContext implements Context, ActorAwareInterface {
 	}
 
 	/**
+	 * @return Locator
+	 */
+	public static function promotedContainer() {
+		return Locator::forThe()->css(".videoContainer.promoted")->
+				descendantOf(TalkAppContext::mainView())->
+				describedAs("Promoted container of the call in the main view");
+	}
+
+	/**
+	 * @return Locator
+	 */
+	public static function promotedDummyContainer() {
+		// A dummy container is used to show the user name and the media
+		// permissions at the same place shown in the unpromoted container for
+		// the user (as the promoted container is centered and the unpromoted
+		// container can be anywhere in the bottom area).
+		return Locator::forThe()->css(".videoContainer-dummy")->
+				descendantOf(TalkAppContext::mainView())->
+				describedAs("Promoted dummy container of the call in the main view");
+	}
+
+	/**
+	 * @return Locator
+	 */
+	public static function promotedVideo() {
+		return Locator::forThe()->css("video")->
+				descendantOf(self::promotedContainer())->
+				describedAs("Video in the promoted container of the call in the main view");
+	}
+
+	/**
+	 * @return Locator
+	 */
+	public static function promotedAvatar() {
+		return Locator::forThe()->css(".avatar-container")->
+				descendantOf(self::promotedContainer())->
+				describedAs("Avatar in the promoted container of the call in the main view");
+	}
+
+	/**
+	 * @return Locator
+	 */
+	public static function promotedUserName() {
+		return Locator::forThe()->css(".nameIndicator")->
+				descendantOf(self::promotedDummyContainer())->
+				describedAs("Name indicator in the promoted dummy container of the call in the main view");
+	}
+
+	/**
+	 * @return Locator
+	 */
+	public static function promotedAudioEnabledIndicator() {
+		return Locator::forThe()->css(".audio-on")->
+				descendantOf(self::promotedDummyContainer())->
+				describedAs("Audio enabled indicator in the promoted dummy container of the call in the main view");
+	}
+
+	/**
+	 * @return Locator
+	 */
+	public static function promotedVideoEnabledIndicator() {
+		return Locator::forThe()->css(".icon-video")->
+				descendantOf(self::promotedDummyContainer())->
+				describedAs("Video enabled indicator in the promoted dummy container of the call in the main view");
+	}
+
+	/**
 	 * @Then I see that the local audio is enabled
 	 */
 	public function iSeeThatTheLocalAudioIsEnabled() {
@@ -164,6 +231,91 @@ class CallContext implements Context, ActorAwareInterface {
 				$timeout = 10 * $this->actor->getFindTimeoutMultiplier())) {
 			PHPUnit_Framework_Assert::fail("The local avatar is still shown after $timeout seconds");
 		}
+	}
+
+	/**
+	 * @Then I see that the promoted video is shown
+	 */
+	public function iSeeThatThePromotedVideoIsShown() {
+		if (!WaitFor::elementToBeEventuallyShown(
+				$this->actor,
+				self::promotedVideo(),
+				$timeout = 10 * $this->actor->getFindTimeoutMultiplier())) {
+			PHPUnit_Framework_Assert::fail("The promoted video was not shown yet after $timeout seconds");
+		}
+	}
+
+	/**
+	 * @Then I see that the promoted video is not shown
+	 */
+	public function iSeeThatThePromotedVideoIsNotShown() {
+		if (!WaitFor::elementToBeEventuallyNotShown(
+				$this->actor,
+				self::promotedVideo(),
+				$timeout = 10 * $this->actor->getFindTimeoutMultiplier())) {
+			PHPUnit_Framework_Assert::fail("The promoted video is still shown after $timeout seconds");
+		}
+	}
+
+	/**
+	 * @Then I see that the promoted avatar is shown
+	 */
+	public function iSeeThatThePromotedAvatarIsShown() {
+		if (!WaitFor::elementToBeEventuallyShown(
+				$this->actor,
+				self::promotedAvatar(),
+				$timeout = 10 * $this->actor->getFindTimeoutMultiplier())) {
+			PHPUnit_Framework_Assert::fail("The promoted avatar was not shown yet after $timeout seconds");
+		}
+	}
+
+	/**
+	 * @Then I see that the promoted avatar is not shown
+	 */
+	public function iSeeThatThePromotedAvatarIsNotShown() {
+		if (!WaitFor::elementToBeEventuallyNotShown(
+				$this->actor,
+				self::promotedAvatar(),
+				$timeout = 10 * $this->actor->getFindTimeoutMultiplier())) {
+			PHPUnit_Framework_Assert::fail("The promoted avatar is still shown after $timeout seconds");
+		}
+	}
+
+	/**
+	 * @Then I see that the promoted user is :userName
+	 */
+	public function iSeeThatThePromotedUserIs($userName) {
+		$promotedUserNameMatchedCallback = function() use($userName) {
+			try {
+				$foundUserName = $this->actor->find(self::promotedUserName())->getText();
+			} catch (NoSuchElementException $exception) {
+				return false;
+			}
+
+			if ($foundUserName == $userName) {
+			    return true;
+			}
+
+			return false;
+		};
+
+		if (!Utils::waitFor($promotedUserNameMatchedCallback, $timeout = 10 * $this->actor->getFindTimeoutMultiplier(), $timeoutStep = 1)) {
+			PHPUnit_Framework_Assert::fail("The promoted user name was not $userName yet after $timeout seconds");
+		}
+	}
+
+	/**
+	 * @Then I see that the promoted audio is enabled
+	 */
+	public function iSeeThatThePromotedAudioIsEnabled() {
+		PHPUnit_Framework_Assert::assertNotNull($this->actor->find(self::promotedAudioEnabledIndicator(), 10));
+	}
+
+	/**
+	 * @Then I see that the promoted video is enabled
+	 */
+	public function iSeeThatThePromotedVideoIsEnabled() {
+		PHPUnit_Framework_Assert::assertNotNull($this->actor->find(self::promotedVideoEnabledIndicator(), 10));
 	}
 
 }
--- tests/acceptance/features/call.feature
+++ tests/acceptance/features/call.feature
@@ -53,3 +53,91 @@ Feature: call
     And I see that the "Waiting for admin to join the call …" empty content message is shown in the main view
     When I leave the call
     Then I see that the chat is shown in the main view
+
+  Scenario: join a call
+    Given I act as "Kermit with microphone and camera"
+    And I am logged in
+    And I have opened the Talk app
+    And I create a one-to-one conversation with "admin"
+    And I see that the "admin" conversation is active
+    And I join the call
+    And I see that the chat is shown in the sidebar
+    And I see that the "Waiting for admin to join the call …" empty content message is shown in the main view
+    And I act as "April with microphone and camera"
+    And I am logged in as the admin
+    And I have opened the Talk app
+    And I open the "user0" conversation
+    And I see that the "user0" conversation is active
+    When I join the call
+    Then I see that the chat is shown in the sidebar
+    And I see that the local audio is enabled
+    And I see that the local video is enabled
+    And I see that the local video is shown
+    And I see that the local avatar is not shown
+    And I see that the promoted audio is enabled
+    And I see that the promoted video is enabled
+    And I see that the promoted video is shown
+    And I see that the promoted avatar is not shown
+    And I see that the promoted user is "user0"
+    And I act as "Kermit with microphone and camera"
+    And I see that the promoted audio is enabled
+    And I see that the promoted video is enabled
+    And I see that the promoted video is shown
+    And I see that the promoted avatar is not shown
+    And I see that the promoted user is "admin"
+
+  Scenario: join a call without camera
+    Given I act as "Kermit with microphone and camera"
+    And I am logged in
+    And I have opened the Talk app
+    And I create a one-to-one conversation with "admin"
+    And I see that the "admin" conversation is active
+    And I join the call
+    And I see that the chat is shown in the sidebar
+    And I see that the "Waiting for admin to join the call …" empty content message is shown in the main view
+    And I act as "Karen with microphone"
+    And I am logged in as the admin
+    And I have opened the Talk app
+    And I open the "user0" conversation
+    And I see that the "user0" conversation is active
+    When I join the call
+    Then I see that the chat is shown in the sidebar
+    And I see that the local audio is enabled
+    And I see that the local avatar is shown
+    And I see that the local video is not available
+    And I see that the local video is not shown
+    And I see that the promoted audio is enabled
+    And I see that the promoted video is enabled
+    And I see that the promoted video is shown
+    And I see that the promoted avatar is not shown
+    And I see that the promoted user is "user0"
+    And I act as "Kermit with microphone and camera"
+    And I see that the promoted audio is enabled
+    And I see that the promoted avatar is shown
+    And I see that the promoted video is not shown
+    And I see that the promoted user is "admin"
+
+  Scenario: leave a call with another participant
+    Given I act as "Kermit with microphone and camera"
+    And I am logged in
+    And I have opened the Talk app
+    And I create a one-to-one conversation with "admin"
+    And I see that the "admin" conversation is active
+    And I join the call
+    And I see that the chat is shown in the sidebar
+    And I see that the "Waiting for admin to join the call …" empty content message is shown in the main view
+    And I act as "April with microphone and camera"
+    And I am logged in as the admin
+    And I have opened the Talk app
+    And I open the "user0" conversation
+    And I see that the "user0" conversation is active
+    And I join the call
+    And I see that the chat is shown in the sidebar
+    And I see that the promoted video is shown
+    And I act as "Kermit with microphone and camera"
+    And I see that the promoted video is shown
+    When I leave the call
+    Then I see that the chat is shown in the main view
+    And I act as "April with microphone and camera"
+    And I see that the chat is shown in the sidebar
+    And I see that the "Waiting for user0 to join the call …" empty content message is shown in the main view
