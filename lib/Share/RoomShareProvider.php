c673564a4ebb1130ec377509776d0339b2569cc8
Daniel Calviño Sánchez <danxuliu@gmail.com>
2018-07-04 10:22:20 +0200

    Implement moving room shares
    When a recipient moves or deletes a room share the share should be moved
    or deleted only for that recipient and be left untouched for the other
    users in the room. Therefore, when a recipient moves or deletes a share
    a copy of the original share is added to keep the share status
    (permissions (to mark a share as deleted) and file target) from the
    point of view of the recipient; when the share is later accessed by the
    recipient RoomShareProvider returns it as seen by that recipient.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/Share/RoomShareProvider.php
+++ lib/Share/RoomShareProvider.php
@@ -53,6 +53,9 @@ use OCP\Share\IShareProvider;
  */
 class RoomShareProvider implements IShareProvider {
 
+	// Special share type for user modified room shares
+	const SHARE_TYPE_USERROOM = 11;
+
 	/** @var IDBConnection */
 	private $dbConnection;
 
@@ -313,7 +316,50 @@ class RoomShareProvider implements IShareProvider {
 	 * @return IShare
 	 */
 	public function move(IShare $share, $recipient) {
-		throw new \Exception("Not implemented");
+		// Check if there is a userroom share
+		$qb = $this->dbConnection->getQueryBuilder();
+		$stmt = $qb->select('id')
+			->from('share')
+			->where($qb->expr()->eq('share_type', $qb->createNamedParameter(self::SHARE_TYPE_USERROOM)))
+			->andWhere($qb->expr()->eq('share_with', $qb->createNamedParameter($recipient)))
+			->andWhere($qb->expr()->eq('parent', $qb->createNamedParameter($share->getId())))
+			->andWhere($qb->expr()->orX(
+				$qb->expr()->eq('item_type', $qb->createNamedParameter('file')),
+				$qb->expr()->eq('item_type', $qb->createNamedParameter('folder'))
+			))
+			->setMaxResults(1)
+			->execute();
+
+		$data = $stmt->fetch();
+		$stmt->closeCursor();
+
+		if ($data === false) {
+			// No userroom share yet. Create one.
+			$qb = $this->dbConnection->getQueryBuilder();
+			$qb->insert('share')
+				->values([
+					'share_type' => $qb->createNamedParameter(self::SHARE_TYPE_USERROOM),
+					'share_with' => $qb->createNamedParameter($recipient),
+					'uid_owner' => $qb->createNamedParameter($share->getShareOwner()),
+					'uid_initiator' => $qb->createNamedParameter($share->getSharedBy()),
+					'parent' => $qb->createNamedParameter($share->getId()),
+					'item_type' => $qb->createNamedParameter($share->getNodeType()),
+					'item_source' => $qb->createNamedParameter($share->getNodeId()),
+					'file_source' => $qb->createNamedParameter($share->getNodeId()),
+					'file_target' => $qb->createNamedParameter($share->getTarget()),
+					'permissions' => $qb->createNamedParameter($share->getPermissions()),
+					'stime' => $qb->createNamedParameter($share->getShareTime()->getTimestamp()),
+				])->execute();
+		} else {
+			// Already a userroom share. Update it.
+			$qb = $this->dbConnection->getQueryBuilder();
+			$qb->update('share')
+				->set('file_target', $qb->createNamedParameter($share->getTarget()))
+				->where($qb->expr()->eq('id', $qb->createNamedParameter($data['id'])))
+				->execute();
+		}
+
+		return $share;
 	}
 
 	/**
@@ -405,7 +451,74 @@ class RoomShareProvider implements IShareProvider {
 			throw new ShareNotFound();
 		}
 
-		return $this->createShareObject($data);
+		$share = $this->createShareObject($data);
+
+		if ($recipientId !== null) {
+			$share = $this->resolveSharesForRecipient([$share], $recipientId)[0];
+		}
+
+		return $share;
+	}
+
+	/**
+	 * Returns each given share as seen by the given recipient.
+	 *
+	 * If the recipient has not modified the share the original one is returned
+	 * instead.
+	 *
+	 * @param Share[] $shares
+	 * @param string $userId
+	 * @return Share[]
+	 */
+	private function resolveSharesForRecipient(array $shares, string $userId): array {
+		$result = [];
+
+		$start = 0;
+		while (true) {
+			/** @var Share[] $shareSlice */
+			$shareSlice = array_slice($shares, $start, 100);
+			$start += 100;
+
+			if ($shareSlice === []) {
+				break;
+			}
+
+			/** @var int[] $ids */
+			$ids = [];
+			/** @var Share[] $shareMap */
+			$shareMap = [];
+
+			foreach ($shareSlice as $share) {
+				$ids[] = (int)$share->getId();
+				$shareMap[$share->getId()] = $share;
+			}
+
+			$qb = $this->dbConnection->getQueryBuilder();
+
+			$query = $qb->select('*')
+				->from('share')
+				->where($qb->expr()->in('parent', $qb->createNamedParameter($ids, IQueryBuilder::PARAM_INT_ARRAY)))
+				->andWhere($qb->expr()->eq('share_with', $qb->createNamedParameter($userId)))
+				->andWhere($qb->expr()->orX(
+					$qb->expr()->eq('item_type', $qb->createNamedParameter('file')),
+					$qb->expr()->eq('item_type', $qb->createNamedParameter('folder'))
+				));
+
+			$stmt = $query->execute();
+
+			while ($data = $stmt->fetch()) {
+				$shareMap[$data['parent']]->setPermissions((int)$data['permissions']);
+				$shareMap[$data['parent']]->setTarget($data['file_target']);
+			}
+
+			$stmt->closeCursor();
+
+			foreach ($shareMap as $share) {
+				$result[] = $share;
+			}
+		}
+
+		return $result;
 	}
 
 	/**
@@ -495,6 +608,8 @@ class RoomShareProvider implements IShareProvider {
 			}
 			$cursor->closeCursor();
 		}
+
+		$shares = $this->resolveSharesForRecipient($shares, $userId);
 
 		return $shares;
 	}
