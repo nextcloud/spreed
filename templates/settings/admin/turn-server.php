a8db5e8ead9429969f5b7a1f0c47da8fba46a173
Marius Blüm <marius@lineone.io>
2018-08-18 22:08:32 +0200

    Change <h3> to <h2> in admin-settings
    Signed-off-by: Marius Blüm <marius@lineone.io>

--- templates/settings/admin/turn-server.php
+++ templates/settings/admin/turn-server.php
@@ -6,7 +6,7 @@ style('spreed', ['settings-admin']);
 ?>
 
 <div class="videocalls section">
-	<h3><?php p($l->t('TURN server')) ?></h3>
+	<h2><?php p($l->t('TURN server')) ?></h2>
 	<p class="settings-hint"><?php p($l->t('The TURN server is used to proxy the traffic from participants behind a firewall.')); ?></p>
 
 	<div class="turn-servers" data-servers="<?php p($_['turnServer']) ?>">
