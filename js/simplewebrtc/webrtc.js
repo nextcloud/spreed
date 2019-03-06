c2b5e259d023affdecccf3e9cd70d4942a05b6db
Daniel Calviño Sánchez <danxuliu@gmail.com>
2019-03-06 19:55:02 +0100

    Remove Google STUN server
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- js/simplewebrtc/webrtc.js
+++ js/simplewebrtc/webrtc.js
@@ -12,7 +12,7 @@ function WebRTC(opts) {
 			debug: false,
 			// makes the entire PC config overridable
 			peerConnectionConfig: {
-				iceServers: [{'urls': 'stun:stun.l.google.com:19302'}]
+				iceServers: []
 			},
 			peerConnectionConstraints: {
 				optional: []
