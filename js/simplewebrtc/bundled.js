nextcloud/spreed

commit af8e16b8aade7da8b649864763e080db9e0ae200

Replace "getUserMedia" with custom wrapper
The normalization of errors by the "getUserMedia" module should be
covered by current webrtc-adapter releases, so it should not be needed.
Besides that, the NPM package contains a "node_modules" directory with
the code of an old "webrtc-adapter" release, and the bundling tool gets
confused when this directory is present and bundles that old code
instead of the most recent one.

Due to all this, the module is replaced by a custom wrapper that just
calls the given callbacks as needed.

Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

js/simplewebrtc/getscreenmedia.js
@@ -1,5 +1,4 @@
 // getScreenMedia helper by @HenrikJoreteg
-var getUserMedia = require('getusermedia');
 
 // cache for constraints and callback
 var cache = {};
@@ -14,6 +13,14 @@ module.exports = function (mode, constraints, cb) {
 		error.name = 'HTTPS_REQUIRED';
 		return callback(error);
 	}
+
+	var getUserMedia = function(constraints, callback) {
+		window.navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+			callback(null, stream);
+		}).catch(function(error) {
+			callback(error, null);
+		});
+	};
 
 	if (window.navigator.userAgent.match('Chrome')) {
 		var chromever = parseInt(window.navigator.userAgent.match(/Chrome\/(\d+)\./)[1], 10);

commit 3680a79f9f632e54e36e5696399866dba1d8a724

Make getUserMedia global again so it's defined in the second export
Regression from af8e16b8aade7da8b649864763e080db9e0ae200

Signed-off-by: Joas Schilling <coding@schilljs.com>

js/simplewebrtc/bundled.js
@@ -2,7 +2,15 @@
 "use strict";
 
 // getScreenMedia helper by @HenrikJoreteg
-// cache for constraints and callback
+var getUserMedia = function getUserMedia(constraints, callback) {
+  window.navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    callback(null, stream);
+  }).catch(function (error) {
+    callback(error, null);
+  });
+}; // cache for constraints and callback
+
+
 var cache = {};
 
 module.exports = function (mode, constraints, cb) {
@@ -15,14 +23,6 @@ module.exports = function (mode, constraints, cb) {
     error.name = 'HTTPS_REQUIRED';
     return callback(error);
   }
-
-  var getUserMedia = function getUserMedia(constraints, callback) {
-    window.navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-      callback(null, stream);
-    }).catch(function (error) {
-      callback(error, null);
-    });
-  };
 
   if (navigator.webkitGetUserMedia) {
     var chromever = parseInt(window.navigator.userAgent.match(/Chrome\/(\d+)\./)[1], 10);

js/simplewebrtc/getscreenmedia.js
@@ -1,4 +1,11 @@
 // getScreenMedia helper by @HenrikJoreteg
+var getUserMedia = function(constraints, callback) {
+	window.navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		callback(null, stream);
+	}).catch(function(error) {
+		callback(error, null);
+	});
+};
 
 // cache for constraints and callback
 var cache = {};
@@ -13,14 +20,6 @@ module.exports = function (mode, constraints, cb) {
 		error.name = 'HTTPS_REQUIRED';
 		return callback(error);
 	}
-
-	var getUserMedia = function(constraints, callback) {
-		window.navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-			callback(null, stream);
-		}).catch(function(error) {
-			callback(error, null);
-		});
-	};
 
 	if (navigator.webkitGetUserMedia) {
 		var chromever = parseInt(window.navigator.userAgent.match(/Chrome\/(\d+)\./)[1], 10);