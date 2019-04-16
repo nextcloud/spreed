07af18081c07b456f9effd4ace1547087d323550
Daniel Calviño Sánchez <danxuliu@gmail.com>
2019-04-16 20:42:54 +0200

    Override screen sharing support from "webrtcsupport" module
    The "webrtcsupport" module does not take into account if the standard
    "getDisplayMedia" is available, and it only sees Firefox and
    Chromium/Chrome as supported; screen sharing in Edge was reported as
    supported only because it was mismatched for Chrome. Now the default
    checks are overridden to match the behaviour of the custom
    "getScreenMedia" module.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- js/simplewebrtc/bundled.js
+++ js/simplewebrtc/bundled.js
@@ -998,8 +998,14 @@ function SimpleWebRTC(opts) {
     if (options.hasOwnProperty(item)) {
       this.config[item] = options[item];
     }
-  } // attach detected support for convenience
+  } // Override screensharing support detection to fit the custom
+  // "getScreenMedia" module.
+  // Note that this is a coarse check; calling "getScreenMedia" may fail even
+  // if "supportScreenSharing" is true.
 
+
+  var screenSharingSupported = window.navigator.mediaDevices && window.navigator.mediaDevices.getDisplayMedia || window.navigator.webkitGetUserMedia || window.navigator.userAgent.match('Firefox');
+  webrtcSupport.supportScreenSharing = window.location.protocol === 'https:' && screenSharingSupported; // attach detected support for convenience
 
   this.capabilities = webrtcSupport; // call WildEmitter constructor
 
