6dacc254d006436c600c8fcf10971e4b28685f0f
Omar <oomarusman@gmail.com>
2019-06-27 00:01:21 +0800

    Remove existing files.

--- compile-handlebars-templates.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Prefer the local handlebars script, and fall back to the global one.
-export PATH=./node_modules/.bin/:$PATH
-
-handlebars -n OCA.Talk.Views.Templates js/views/templates/ -f js/views/templates.js
-
-echo "OK"
