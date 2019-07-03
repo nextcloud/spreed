4eaadd250175ebba55f7f7fb4bd0a7cc5bc9c457
Daniel Calviño Sánchez <danxuliu@gmail.com>
2019-07-03 12:08:20 +0200

    Get target browsers for Babel in "vue/" from browserslist
    If the target browsers are not explicitly set in the Babel configuration
    they will be got from the browserslist configuration. The browserslist
    configuration for the "vue/" directory is set in its "package.json" file
    with the same targets that there were in the Babel configuration, so
    removing them from the Babel configuration does not change anything at
    this point. However, this is a preparatory step to keep the target
    browsers in sync with the ones used in Nextcloud server by means of the
    browserslist configuration.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- vue/.babelrc.js
+++ vue/.babelrc.js
@@ -2,12 +2,7 @@ module.exports = {
 	plugins: ['@babel/plugin-syntax-dynamic-import'],
 	presets: [
 		[
-			'@babel/preset-env',
-			{
-				targets: {
-					browsers: ['last 2 versions', 'ie >= 11']
-				}
-			}
+			'@babel/preset-env'
 		]
 	]
 }
