b3b09de38f63603c59cc0fd26b4287e5886c6ff8
Joas Schilling <coding@schilljs.com>
2019-07-16 14:33:02 +0200

    Also run acceptance tests when CSS was modified
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/drone-run-acceptance-tests.sh
+++ tests/drone-run-acceptance-tests.sh
@@ -12,4 +12,6 @@ echo "========================="
 
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c "^js/") -gt 0 ]] && echo "JS files are modified" && exit 0
 
+[[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c "^css/") -gt 0 ]] && echo "CSS files are modified" && exit 0
+
 exit 1
