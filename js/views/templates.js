7dec802cfac990d429b445ab193aa92c7cd6c260
Daniel Calviño Sánchez <danxuliu@gmail.com>
2019-08-26 01:40:19 +0200

    Remove no longer needed parent element of "Copy link" button
    The "share-link-options" element acts as a spacer to move the room
    moderation menu button to the right. However, given that the call button
    width is limited to the 50% of its parent flex element and that element
    has the full width of the sidebar the same effect can be achieved by
    setting "margin-left: auto" on the room moderation button.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- js/views/templates.js
+++ js/views/templates.js
@@ -48,7 +48,7 @@ templates['callinfoview'] = template({"1":function(container,depth0,helpers,part
     + alias4(((helper = (helper = helpers.fileLinkTitle || (depth0 != null ? depth0.fileLinkTitle : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"fileLinkTitle","hash":{},"data":data}) : helper)))
     + "\">\n		<span class=\"icon icon-file\"></span>\n	</a>\n";
 },"3":function(container,depth0,helpers,partials,data) {
-    return "	<div class=\"share-link-options\">\n		<div class=\"clipboard-button\"><button class=\"button icon-clippy\"></button></div>\n	</div>\n";
+    return "	<div class=\"clipboard-button\"><button class=\"button icon-clippy\"></button></div>\n";
 },"5":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
