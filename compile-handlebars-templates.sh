fca60845347e9213ed5d5acbba2e345f7da5b04b
Joas Schilling <coding@schilljs.com>
2019-10-01 10:26:09 +0200

    Use handlebar from node_modules
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- compile-handlebars-templates.sh
+++ compile-handlebars-templates.sh
@@ -3,6 +3,6 @@
 # Prefer the local handlebars script, and fall back to the global one.
 export PATH=./node_modules/.bin/:$PATH
 
-handlebars -n OCA.Talk.Views.Templates js/views/templates/ -f js/views/templates.js
+node node_modules/handlebars/bin/handlebars -n OCA.Talk.Views.Templates js/views/templates/ -f js/views/templates.js
 
 echo "OK"
