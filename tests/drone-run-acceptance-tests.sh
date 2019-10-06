2a61fa00cb44c98765322280a0a9b47dcba8bab7
Daniel Calviño Sánchez <danxuliu@gmail.com>
2019-10-07 01:33:53 +0200

    Do not run tests when PHP files of other types of tests are modified
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- tests/drone-run-acceptance-tests.sh
+++ tests/drone-run-acceptance-tests.sh
@@ -8,7 +8,7 @@ echo "========================="
 
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | wc -l) -eq 0 ]] && echo "No files are modified => merge commit" && exit 0
 
-[[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c ".php$") -gt 0 ]] && echo "PHP files are modified" && exit 0
+[[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep --invert-match "^tests/" | grep -c ".php$") -gt 0 ]] && echo "PHP files are modified" && exit 0
 
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c "^tests/acceptance/") -gt 0 ]] && echo "Acceptance test files are modified" && exit 0
 
