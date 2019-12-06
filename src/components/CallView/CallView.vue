4babf5795e1bf43de7583f4f13f3a05f9dce670c
Joas Schilling <coding@schilljs.com>
2019-12-06 14:39:34 +0100

    Add a CallView when user is in call
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ src/components/CallView/CallView.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="stage" />
+</template>
+
+<script>
+export default {
+	name: 'CallView',
+}
+</script>
+
+<style lang="scss" scoped>
+.stage {
+	background: #000000;
+	width: 100%;
+	height: 100%;
+}
+</style>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,18 +1,28 @@
 <template>
 	<div class="chatview">
 		<TopBar />
-		<MessagesList :token="token" />
-		<NewMessageForm />
+
+		<template v-if="!showChatInSidebar">
+			<MessagesList :token="token" />
+			<NewMessageForm />
+		</template>
+		<template v-else>
+			<CallView :token="token" />
+		</template>
 	</div>
 </template>
 
 <script>
+import CallView from '../components/CallView/CallView'
 import MessagesList from '../components/MessagesList/MessagesList'
 import NewMessageForm from '../components/NewMessageForm/NewMessageForm'
 import TopBar from '../components/TopBar/TopBar'
+import { PARTICIPANT } from '../constants'
+
 export default {
 	name: 'ChatView',
 	components: {
+		CallView,
 		MessagesList,
 		NewMessageForm,
 		TopBar,
@@ -21,6 +31,29 @@ export default {
 		token: {
 			type: String,
 			required: true,
+		},
+	},
+
+	computed: {
+		participant() {
+			if (typeof this.token === 'undefined') {
+				return {
+					inCall: PARTICIPANT.CALL_FLAG.DISCONNECTED,
+				}
+			}
+
+			const participantIndex = this.$store.getters.getParticipantIndex(this.token, this.$store.getters.getParticipantIdentifier())
+			if (participantIndex !== -1) {
+				return this.$store.getters.getParticipant(this.token, participantIndex)
+			}
+
+			return {
+				inCall: PARTICIPANT.CALL_FLAG.DISCONNECTED,
+			}
+		},
+
+		showChatInSidebar() {
+			return this.participant.inCall !== PARTICIPANT.CALL_FLAG.DISCONNECTED
 		},
 	},
 }
