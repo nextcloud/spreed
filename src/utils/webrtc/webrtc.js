8288d49c7edaf3d6d242712e35f036b64e18ec7f
Joas Schilling <coding@schilljs.com>
2019-12-13 22:15:41 +0100

    Fix stylelint errors
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/utils/webrtc/webrtc.js
+++ src/utils/webrtc/webrtc.js
@@ -1,3 +1,31 @@
+/**
+ * @copyright Copyright (c) 2019 Daniel Calviño Sánchez <danxuliu@gmail.com>
+ * @copyright Copyright (c) 2019 Ivan Sein <ivan@nextcloud.com>
+ * @copyright Copyright (c) 2019 Joachim Bauch <bauch@struktur.de>
+ * @copyright Copyright (c) 2019 Joas Schilling <coding@schilljs.com>
+ *
+ * @author Daniel Calviño Sánchez <danxuliu@gmail.com>
+ * @author Ivan Sein <ivan@nextcloud.com>
+ * @author Joachim Bauch <bauch@struktur.de>
+ * @author Joas Schilling <coding@schilljs.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /* global $ */
 
 /* eslint-disable no-console */
