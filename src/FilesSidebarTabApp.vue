6a22657e74ee32d47622d58100f60e20d2e9d07d
Daniel Calviño Sánchez <danxuliu@gmail.com>
2019-12-19 14:26:51 +0100

    Check whether the Talk sidebar is supported for a file or not
    The Talk sidebar is supported for a file when it meets certain sharing
    conditions. If the Talk sidebar is not supported then a message is shown
    to the user in the tab to share the file first.
    
    In some cases it is not possible to know if the Talk sidebar is
    supported or not only from the FileInfo data and it is necessary to
    query the server. In those cases a loading icon is shown until the
    response from the server is received.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/FilesSidebarTabApp.vue
+++ src/FilesSidebarTabApp.vue
@@ -24,6 +24,17 @@
 			<div class="icon icon-talk" />
 			<h2>Conversations are not available for folders</h2>
 		</div>
+		<div v-else-if="isTalkSidebarSupportedForFile === undefined" class="emptycontent ui-not-ready-placeholder">
+			<div class="icon icon-loading" />
+		</div>
+		<div v-else-if="!isTalkSidebarSupportedForFile" class="emptycontent file-not-shared">
+			<div class="icon icon-talk" />
+			<h2>{{ t('spreed', 'Discuss this file') }}</h2>
+			<p>{{ t('spreed', 'Share this file with others to discuss it') }}</p>
+			<button class="primary" @click="openSharingTab">
+				{{ t('spreed', 'Share this file') }}
+			</button>
+		</div>
 		<p v-else>
 			Talk tab coming soon
 		</p>
@@ -48,6 +59,7 @@ export default {
 			 * Stores the cancel function returned by `cancelableLookForNewMessages`,
 			 */
 			cancelGetFileConversation: () => {},
+			isTalkSidebarSupportedForFile: undefined,
 		}
 	},
 
@@ -60,6 +72,15 @@ export default {
 		},
 		token() {
 			return this.$store.getters.getToken()
+		},
+	},
+
+	watch: {
+		fileInfo: {
+			immediate: true,
+			handler(fileInfo) {
+				this.setTalkSidebarSupportedForFile(fileInfo)
+			},
 		},
 	},
 
@@ -82,6 +103,90 @@ export default {
 					console.debug(exception)
 				}
 			}
+		},
+
+		/**
+		 * Sets whether the Talk sidebar is supported for the file or not.
+		 *
+		 * In some cases it is not possible to know if the Talk sidebar is
+		 * supported for the file or not just from the data in the FileInfo (for
+		 * example, for files in a folder shared by the current user). Due to
+		 * that this function is asynchronous; isTalkSidebarSupportedForFile
+		 * will be set as soon as possible (in some cases, immediately) with
+		 * either true or false, depending on whether the Talk sidebar is
+		 * supported for the file or not.
+		 *
+		 * The Talk sidebar is supported for a file if the file is shared with
+		 * the current user or by the current user to another user (as a user,
+		 * group...), or if the file is a descendant of a folder that meets
+		 * those conditions.
+		 *
+		 * @param {OCA.Files.FileInfo} fileInfo the FileInfo to check
+		 */
+		async setTalkSidebarSupportedForFile(fileInfo) {
+			this.isTalkSidebarSupportedForFile = undefined
+
+			if (!fileInfo) {
+				this.isTalkSidebarSupportedForFile = false
+
+				return
+			}
+
+			if (fileInfo.get('type') === 'dir') {
+				this.isTalkSidebarSupportedForFile = false
+
+				return
+			}
+
+			if (fileInfo.get('shareOwnerId')) {
+				// Shared with me
+				// TODO How to check that it is not a remote share? At least for
+				// local shares "shareTypes" is not defined when shared with me.
+				this.isTalkSidebarSupportedForFile = true
+
+				return
+			}
+
+			if (!fileInfo.get('shareTypes')) {
+				// When it is not possible to know whether the Talk sidebar is
+				// supported for a file or not only from the data in the
+				// FileInfo it is necessary to query the server.
+				// FIXME If the file is shared this will create the conversation
+				// if it does not exist yet.
+				this.isTalkSidebarSupportedForFile = (await getFileConversation({ fileId: fileInfo.id })) || false
+
+				return
+			}
+
+			const shareTypes = fileInfo.get('shareTypes').filter(function(shareType) {
+				// Ensure that shareType is an integer (as in the past shareType
+				// could be an integer or a string depending on whether the
+				// Sharing tab was opened or not).
+				shareType = parseInt(shareType)
+				return shareType === OC.Share.SHARE_TYPE_USER
+						|| shareType === OC.Share.SHARE_TYPE_GROUP
+						|| shareType === OC.Share.SHARE_TYPE_CIRCLE
+						|| shareType === OC.Share.SHARE_TYPE_ROOM
+						|| shareType === OC.Share.SHARE_TYPE_LINK
+						|| shareType === OC.Share.SHARE_TYPE_EMAIL
+			})
+
+			if (shareTypes.length === 0) {
+				// When it is not possible to know whether the Talk sidebar is
+				// supported for a file or not only from the data in the
+				// FileInfo it is necessary to query the server.
+				// FIXME If the file is shared this will create the conversation
+				// if it does not exist yet.
+				this.isTalkSidebarSupportedForFile = (await getFileConversation({ fileId: fileInfo.id })) || false
+
+				return
+			}
+
+			this.isTalkSidebarSupportedForFile = true
+		},
+
+		openSharingTab() {
+			OCA.Files.Sidebar.setActiveTab('sharing')
 		},
 	},
 }
