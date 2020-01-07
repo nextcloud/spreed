c96adaf0d9d7d071fa11d22766699f704dd4971b
Marco Ambrosini <marcoambrosini@pm.me>
2020-01-07 14:50:24 +0100

    Remove virtual list
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- src/components/MessagesList/MessagesList.vue
+++ src/components/MessagesList/MessagesList.vue
@@ -30,9 +30,7 @@ the Vue virtual scroll list component, whose docs you can find [here.](https://g
 <template>
 	<!-- size and remain refer to the amount and initial height of the items that
 	are outside of the viewport -->
-	<virtual-list :size="40"
-		:remain="8"
-		:variable="true"
+	<div
 		class="scroller">
 		<MessagesGroup
 			v-for="item of messagesGroupedByAuthor"
@@ -41,12 +39,11 @@ the Vue virtual scroll list component, whose docs you can find [here.](https://g
 			v-bind="item"
 			:messages="item"
 			@deleteMessage="handleDeleteMessage" />
-	</virtual-list>
+	</div>
 </template>
 
 <script>
 import moment from '@nextcloud/moment'
-import virtualList from 'vue-virtual-scroll-list'
 import MessagesGroup from './MessagesGroup/MessagesGroup'
 import { fetchMessages, lookForNewMessages } from '../../services/messagesService'
 import { EventBus } from '../../services/EventBus'
@@ -57,7 +54,6 @@ export default {
 	name: 'MessagesList',
 	components: {
 		MessagesGroup,
-		virtualList,
 	},
 
 	props: {
