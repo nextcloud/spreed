9e675d09c873a6c9061b793f3f54e4159bee0d45
Daniel Calviño Sánchez <danxuliu@gmail.com>
2020-03-03 13:48:39 +0100

    Add support for triggering events on LocalCallParticipantModels
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/utils/webrtc/models/LocalCallParticipantModel.js
+++ src/utils/webrtc/models/LocalCallParticipantModel.js
@@ -26,12 +26,48 @@ export default function LocalCallParticipantModel() {
 		guestName: null,
 	}
 
+	this._handlers = []
+
 }
 
 LocalCallParticipantModel.prototype = {
 
 	set: function(key, value) {
 		this.attributes[key] = value
+	},
+
+	on: function(event, handler) {
+		if (!this._handlers.hasOwnProperty(event)) {
+			this._handlers[event] = [handler]
+		} else {
+			this._handlers[event].push(handler)
+		}
+	},
+
+	off: function(event, handler) {
+		const index = this._handlers[event].indexOf(handler)
+		if (index !== -1) {
+			this._handlers[event].splice(index, 1)
+		}
+	},
+
+	_trigger: function(event, args) {
+		let handlers = this._handlers[event]
+		if (!handlers) {
+			return
+		}
+
+		if (!args) {
+			args = []
+		}
+
+		args.unshift(this)
+
+		handlers = handlers.slice(0)
+		for (let i = 0; i < handlers.length; i++) {
+			const handler = handlers[i]
+			handler.apply(handler, args)
+		}
 	},
 
 	setWebRtc: function(webRtc) {
