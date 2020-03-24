3c647e872aceea76b8bd4008032488909019a1f2
Marco Ambrosini <marcoambrosini@pm.me>
2020-03-24 10:19:51 +0100

    Remove eslint rules added to the eslint-config module
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,4 @@ module.exports = {
 	extends: [
 		'nextcloud'
 	],
-	/**
-	 * Allow jest syntax in the src folder
-	 */
-	env: {
-		jest: true
-	},
-	/**
-	 * Allow shallow import of @vue/test-utils in order to be able to use it in 
-	 * the src folder
-	 */
-	rules: {
-		"node/no-unpublished-import": ["error", {
-			"allowModules": ["@vue/test-utils"]
-		}]
-	}
 }
