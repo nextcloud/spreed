b011761bed1c0edfbf250f4812ff2e6e387c1951
Joas Schilling <coding@schilljs.com>
2020-03-26 11:38:00 +0100

    Add a link to the documentation in the admin settings as well
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/views/AdminSettings/Commands.vue
+++ src/views/AdminSettings/Commands.vue
@@ -71,9 +71,9 @@ export default {
 
 	computed: {
 		commandHint() {
-			return t('spreed', 'Commands are a new beta feature in Nextcloud Talk. They allow you to run scripts on your Nextcloud server. You can define them with our command line interface. An example of a calculator script can be found in our <a {attributes}>documentation ↗</a>.', {
-				attributes: 'target="_blank" rel="noreferrer noopener" href="https://github.com/nextcloud/spreed/blob/master/docs/commands.md"',
-			}, undefined, { escape: false })
+			return t('spreed', 'Commands are a new beta feature in Nextcloud Talk. They allow you to run scripts on your Nextcloud server. You can define them with our command line interface. An example of a calculator script can be found in our {linkstart}documentation{linkend}.')
+				.replace('{linkstart}', '<a  target="_blank" rel="noreferrer nofollow" class="external" href="https://nextcloud-talk.readthedocs.io/en/latest/commands/">')
+				.replace('{linkend}', ' ↗</a>')
 		},
 	},
 
