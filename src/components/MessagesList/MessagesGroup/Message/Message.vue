feaab485b2514283c5cc8bae2e18ac4efe0cec19
Marco Ambrosini <marcoambrosini@pm.me>
2020-03-31 12:56:11 +0200

    Fix system messages padding
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- src/components/MessagesList/MessagesGroup/Message/Message.vue
+++ src/components/MessagesList/MessagesGroup/Message/Message.vue
@@ -27,6 +27,7 @@ the main body of the message as well as a quote.
 <template>
 	<div
 		class="message"
+		:class="{'hover': showActions && !isSystemMessage, 'system' : isSystemMessage}"
 		@mouseover="showActions=true"
 		@mouseleave="showActions=false">
 		<div v-if="isFirstMessage && showAuthor" class="message__author">
@@ -327,7 +328,7 @@ export default {
 @import '../../../../assets/variables';
 
 .message {
-	padding: 4px 0 4px 0;
+	padding: 4px;
 	&__author {
 		color: var(--color-text-maxcontrast);
 	}
@@ -381,5 +382,16 @@ export default {
 			}
 		}
 	}
+}
+
+// Increase the padding for regular messages to improve readability and
+// allow some space for the reply button
+.message:not(.system) {
+	padding: 12px 8px;
+}
+
+.hover {
+	background-color: var(--color-background-hover);
+	border-radius: 8px;
 }
 </style>
