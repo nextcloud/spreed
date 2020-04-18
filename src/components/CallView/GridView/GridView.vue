5f57d48692ce3036837309959a7d92d3021dd6ad
Marco Ambrosini <marcoambrosini@pm.me>
2020-04-18 10:02:34 +0200

    Add docs
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- src/components/CallView/GridView/GridView.vue
+++ src/components/CallView/GridView/GridView.vue
@@ -69,13 +69,33 @@ export default {
 	mixins: [call],
 
 	props: {
+		/**
+		 * The width of the grid
+		 */
 		gridWidth: {
 			type: Number,
 			required: true,
 		},
+		/**
+		 * The height of the grid
+		 */
 		gridHeight: {
 			type: Number,
 			required: true,
+		},
+		/**
+		 * Minimum width of the video components
+		 */
+		minWidth: {
+			type: Number,
+			default: 200,
+		},
+		/**
+		 * Minimum height of the video components
+		 */
+		minHeight: {
+			type: Number,
+			default: 200,
 		},
 		/**
 		 * Max number of videos per page. `0`, the default value, means no cap
@@ -84,25 +104,39 @@ export default {
 			type: Number,
 			default: 0,
 		},
+		/**
+		 * Developer mode: If enabled it allows to debug the grid using dummy
+		 * videos
+		 */
 		devMode: {
 			type: Boolean,
 			default: false,
+		},
+		/**
+		 * The number of dummy videos in dev mode
+		 */
+		dummies: {
+			type: Number,
+			default: 10,
 		},
 	},
 
 	data() {
 		return {
-			// Min width and height of the video components
-			minWidth: 200,
-			minHeight: 200,
-			// Array of currently displayed videos
+			// Array of videos that are being displayed in the grid at any
+			// given moment
 			displayedVideos: [],
+			// Columns of the grid at any given moment
 			columns: 0,
+			// Rows of the grid at any given moment
 			rows: 0,
 		}
 	},
 
 	computed: {
+		// The videos array. This is the total number of grid elements.
+		// Depending on `gridWidthm`, `gridHeight`, `minWidth`, `minHeight` and
+		// `videosCap`, these videos are shown in one or more grid 'pages'.
 		videos() {
 			if (this.devMode) {
 				return Array.from(Array(40).keys())
@@ -121,12 +155,12 @@ export default {
 			}
 		},
 
-		// The aspect ratio of main-view and of the grid itself
+		// The aspect ratio of the grid (in terms of px)
 		gridAspectRatio() {
 			return this.gridWidth / this.gridHeight
 		},
 
-		// Max number of columns given the size of the parent
+		// Max number of columns possible
 		columnsMax() {
 			if (Math.floor(this.gridWidth / this.minWidth) < 1) {
 				// Return at least 1 column
@@ -136,7 +170,7 @@ export default {
 			}
 		},
 
-		// Max number of rows given the size of the parent
+		// Max number of rows possible
 		rowsMax() {
 			if (Math.floor(this.gridHeight / this.minHeight) < 1) {
 				// Return at least 1 row
@@ -146,14 +180,17 @@ export default {
 			}
 		},
 
+		// Hides or displays the `grid-navigation next` button
 		hasNextPage() {
 			if (this.displayedVideos !== []) {
-				return this.displayedVideos[this.displayedVideos.length - 1] !== this.videos[this.videos.length - 1]
+				return this.displayedVideos[this.displayedVideos.length - 1]
+				!== this.videos[this.videos.length - 1]
 			} else {
 				return false
 			}
 		},
 
+		// Hides or displays the `grid-navigation previous` button
 		hasPreviousPage() {
 			if (this.displayedVideos !== []) {
 				return this.displayedVideos[0] !== this.videos[0]
@@ -162,40 +199,52 @@ export default {
 			}
 		},
 
+		// Computed css to reactively style the grid
 		gridStyle() {
-			return { gridTemplateColumns: `repeat(${this.columns},  minmax(${this.minWidth}px, 1fr))`, gridTemplateRows: `repeat(${this.rows}, minmax(${this.minHeight}px, 1fr)))` }
+			return {
+				gridTemplateColumns: `repeat(${this.columns}, minmax(${this.minWidth}px, 1fr))`,
+				gridTemplateRows: `repeat(${this.rows}, minmax(${this.minHeight}px, 1fr)))` }
 		},
 	},
 
 	watch: {
-		gridAspectRatio() {
 		// If the aspect ratio changes, rebuild the grid
+		gridAspectRatio() {
 		// TODO: properly handle resizes when not on first page:
 		// currently if the user is not on the 'first page', upon resize the
 		// current position in the videos array is lost (first element
 		// in the grid goes back to be first video)
 			debounce(this.makeGrid(), 200)
 		},
+		// If the video array changes, rebuild the grid
 		videos() {
 			this.makeGrid()
 		},
 	},
 
 	beforeMount() {
+		// First build of the grid when mounting the component
 		this.makeGrid()
 	},
 
 	methods: {
+		/**
+		 * Find the right size if the grid in rows and columns (we already know
+		 * the size in px).
+		 */
 		makeGrid() {
-			// Start by assigning the max possible value to rows and columns. This
-			// would fit as many video components as possible given the parent's
-			// dimensions.
+			// We start by assigning the max possible value to our rows and columns
+			// variables. These variables are kept in the data and represent how the
+			// grid looks at any given moment. We do this based on `gridWidthm`,
+			// `gridHeight`, `minWidth` and `minHeight`
 			this.columns = this.columnsMax
 			this.rows = this.rowsMax
-			// However, if we have only a couple of videos to display and a very big
-			// window, we now have a lot of columns and rows, and our video components
-			// would be too small. To solve this, we shrink this 'virtual grid' we've
-			// just created to fit the number of elements that we have.
+			// This values would already work if the grid is entirely populated with
+			// video elements. However, if we'd have only a couple of videos to display
+			// and a very big screen, we'd now have a lot of columns and rows, and our
+			// video components would occupy only the first 2 slots and be too small.
+			// To solve this, we shrink this 'max grid' we've just created to fit the
+			// number of videos that we have.
 			if (this.videosCap !== 0) {
 				this.shrinkGrid(this.videosCap)
 			} else {
@@ -205,25 +254,30 @@ export default {
 			if (this.devMode) {
 				this.displayedVideos = this.videos.slice(0, this.rows * this.columns)
 			} else {
-				// Account for the localVideo component
+				// `- 1` because we a ccount for the localVideo component (see template)
 				this.displayedVideos = this.videos.slice(0, this.rows * this.columns - 1)
 			}
 		},
 
+		// Fine tune the number of rows and columns of the grid
 		shrinkGrid(videos) {
-		// Max available grid slots given parent's dimensions and the minimum video
-		// dimensions
+			// Get the max available grid slots
 			let slots = this.columns * this.rows
-			// Run this code only if we don't have an 'overflow' of videos.
+			// Run this code only if we don't have an 'overflow' of videos. If the
+			// videos are populating the grid, there's no point in shrinking it.
 			if (videos < slots) {
-			// The aspect ratio of the virtual grid while shrinking
+				// Get the aspect ratio (in terms of coulmns and rows)of the current grid
+				// while shrinking
 				const currentAspectRatio = this.columns / this.rows
-				// Compare the current aspect ratio to the grid aspectratio
+				// At each iteration of this recursive method, we want to compare the
+				// current aspect ratio of the grid (in terms of rows and columns) to
+				// the aspect ratio of the grid (in terms of px).
 				if (currentAspectRatio <= this.gridAspectRatio) {
 					this.rows--
 					// Ceck that there are still enough slots available
 					slots = this.columns * this.rows
 					if (videos > slots) {
+						// If not, revert the changes and break the loop
 						this.rows++
 						return
 					}
@@ -232,6 +286,7 @@ export default {
 					// Ceck that there are still enough slots available
 					slots = this.columns * this.rows
 					if (videos > slots) {
+						// If not, revert the changes and break the loop
 						this.rows++
 						return
 					}
@@ -240,21 +295,25 @@ export default {
 			}
 		},
 
+		// Slice the `videos` array to display the next set of videos
 		handleClickNext() {
 			const currentLastDisplayedElement = this.displayedVideos[this.displayedVideos.length - 1]
 			const firstElementOfNextPage = this.videos.indexOf(currentLastDisplayedElement) + 1
 			if (this.devMode) {
 				this.displayedVideos = this.videos.slice(firstElementOfNextPage, firstElementOfNextPage + this.rows * this.columns)
 			} else {
+				// `- 1` because we a ccount for the localVideo component (see template)
 				this.displayedVideos = this.videos.slice(firstElementOfNextPage, firstElementOfNextPage + this.rows * this.columns - 1)
 			}
 		},
+		// Slice the `videos` array to display the previous set of videos
 		handleClickPrevious() {
 			const currentFirstDisplayedElement = this.displayedVideos[0]
 			const lastElementOfPreviousPage = this.videos.indexOf(currentFirstDisplayedElement)
 			if (this.devMode) {
 				this.displayedVideos = this.videos.slice(lastElementOfPreviousPage - this.rows * this.columns, lastElementOfPreviousPage)
 			} else {
+				// `- 1` because we a ccount for the localVideo component (see template)
 				this.displayedVideos = this.videos.slice(lastElementOfPreviousPage - this.rows * this.columns, lastElementOfPreviousPage - 1)
 			}
 		},
