393e14100eec3e0adcabcd8e11fc0354e983c5b4
Joas Schilling <coding@schilljs.com>
2020-04-29 13:54:18 +0200

    Remove all direct calls to getSignaling
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/FilesSidebarTabApp.vue
+++ src/FilesSidebarTabApp.vue
@@ -57,10 +57,10 @@ import {
 } from './services/participantsService'
 import CancelableRequest from './utils/cancelableRequest'
 import {
-	getSignaling,
 	signalingKill,
 } from './utils/webrtc/index'
 import { getCurrentUser } from '@nextcloud/auth'
+import { loadState } from '@nextcloud/initial-state'
 import Axios from '@nextcloud/axios'
 import CallButton from './components/TopBar/CallButton'
 import ChatView from './components/ChatView'
@@ -184,8 +184,7 @@ export default {
 			// used), although that should not be a problem given that only the
 			// "inCall" flag (which is locally updated when joining and leaving
 			// a call) is currently used.
-			const signaling = await getSignaling()
-			if (signaling.url) {
+			if (loadState('talk', 'signaling_mode') !== 'internal') {
 				EventBus.$on('shouldRefreshConversations', OCA.Talk.fetchCurrentConversationWrapper)
 			} else {
 				// The "shouldRefreshConversations" event is triggered only when
