da1fa0f1b53859f10d337ef89ac694d6122cf494
Marco Ambrosini <marcoambrosini@pm.me>
2020-04-30 19:10:56 +0200

    Make incoming videos scrollable
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- src/components/CallView/CallView.vue
+++ src/components/CallView/CallView.vue
@@ -22,35 +22,43 @@
 	<div id="call-container" :class="callViewClass">
 		<EmptyCallView v-if="!remoteParticipantsCount && !screenSharingActive" />
 		<div id="videos">
-			<template v-for="callParticipantModel in reversedCallParticipantModels">
-				<Video
-					:key="callParticipantModel.attributes.peerId"
-					:model="callParticipantModel"
-					:shared-data="sharedDatas[callParticipantModel.attributes.peerId]"
-					:use-constrained-layout="useConstrainedLayout"
-					@switchScreenToId="_switchScreenToId" />
-				<Video
-					:key="'placeholder' + callParticipantModel.attributes.peerId"
-					:placeholder-for-promoted="true"
-					:model="callParticipantModel"
-					:shared-data="sharedDatas[callParticipantModel.attributes.peerId]"
-					:use-constrained-layout="useConstrainedLayout"
-					@switchScreenToId="_switchScreenToId" />
-			</template>
-			<LocalVideo ref="localVideo"
-				:local-media-model="localMediaModel"
-				:local-call-participant-model="localCallParticipantModel"
-				:use-constrained-layout="useConstrainedLayout"
-				@switchScreenToId="_switchScreenToId" />
-		</div>
-		<div id="screens">
-			<Screen v-if="localMediaModel.attributes.localScreen"
-				:local-media-model="localMediaModel"
-				:shared-data="localSharedData" />
-			<Screen v-for="callParticipantModel in callParticipantModelsWithScreen"
-				:key="'screen-' + callParticipantModel.attributes.peerId"
-				:call-participant-model="callParticipantModel"
-				:shared-data="sharedDatas[callParticipantModel.attributes.peerId]" />
+			<div class="videos-stripe">
+				<div class="incoming-videos">
+					<template v-for="callParticipantModel in reversedCallParticipantModels">
+						<Video
+							:key="callParticipantModel.attributes.peerId"
+							v-observe-visibility="(isVisible, entry) => visibilityChanged(isVisible, entry, callParticipantModel.attributes.peerId)"
+							:token="token"
+							:model="callParticipantModel"
+							:shared-data="sharedDatas[callParticipantModel.attributes.peerId]"
+							:use-constrained-layout="useConstrainedLayout"
+							@switchScreenToId="_switchScreenToId" />
+						<Video
+							:key="'placeholder' + callParticipantModel.attributes.peerId + '1'"
+							:placeholder-for-promoted="true"
+							:model="callParticipantModel"
+							:shared-data="sharedDatas[callParticipantModel.attributes.peerId]"
+							:use-constrained-layout="useConstrainedLayout"
+							@switchScreenToId="_switchScreenToId" />
+					</template>
+				</div>
+				<div class="local-video">
+					<LocalVideo ref="localVideo"
+						:local-media-model="localMediaModel"
+						:local-call-participant-model="localCallParticipantModel"
+						:use-constrained-layout="useConstrainedLayout"
+						@switchScreenToId="_switchScreenToId" />
+				</div>
+			</div>
+			<div id="screens">
+				<Screen v-if="localMediaModel.attributes.localScreen"
+					:local-media-model="localMediaModel"
+					:shared-data="localSharedData" />
+				<Screen v-for="callParticipantModel in callParticipantModelsWithScreen"
+					:key="'screen-' + callParticipantModel.attributes.peerId"
+					:call-participant-model="callParticipantModel"
+					:shared-data="sharedDatas[callParticipantModel.attributes.peerId]" />
+			</div>
 		</div>
 	</div>
 </template>
@@ -342,6 +350,24 @@ export default {
 	align-items: flex-end;
 }
 
+.videos-stripe {
+	height: 200px;
+	display: flex;
+	width: 100%;
+}
+
+.incoming-videos {
+	overflow-x: scroll;
+	flex-grow: 1;
+	flex-direction: row;
+	display: flex;
+	width: 100%;
+}
+
+.local-video {
+	display: flex;
+}
+
 #videos.hidden {
 	display: none;
 }
@@ -356,7 +382,6 @@ export default {
 .participants-1.screensharing .videoContainer,
 .participants-2.screensharing .videoContainer {
 	position: relative;
-	width: 100%;
 	padding: 0 2%;
 	-webkit-box-flex: auto;
 	-moz-box-flex: auto;
@@ -367,6 +392,7 @@ export default {
 	display: flex;
 	justify-content: center;
 	align-items: flex-end;
+	min-width: 200px !important;
 }
 
 .videoContainer.hidden,
@@ -478,12 +504,17 @@ export default {
 .videoContainer.promoted {
 	position: absolute;
 	width: 100%;
-	height: 100%;
 	overflow: hidden;
 	left: 0;
 	top: 0;
+	height: calc(100% - 200px);
 	z-index: 1;
 }
+
+.participants-2 .videoContainer {
+	height: 100%;
+}
+
 .videoContainer.promoted ::v-deep video,
 .participants-2 .videoContainer:not(.videoView) ::v-deep video {
 	position: absolute;
