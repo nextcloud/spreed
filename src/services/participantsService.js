73603d4e09c1c68e628263d055b1db590eed79d6
Joas Schilling <coding@schilljs.com>
2020-05-29 10:28:11 +0200

    Automatically force join if the session is "old"
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/services/participantsService.js
+++ src/services/participantsService.js
@@ -32,6 +32,7 @@ import {
 } from '../utils/webrtc/index'
 import { EventBus } from './EventBus'
 import SessionStorage from './SessionStorage'
+import { PARTICIPANT } from '../constants'
 
 /**
  * Joins the current user to a conversation specified with
@@ -40,11 +41,12 @@ import SessionStorage from './SessionStorage'
  * @param {string} token The conversation token;
  */
 const joinConversation = async(token) => {
-	const isReloading = SessionStorage.getItem('joined_conversation') === token
+	// When the token is in the last joined conversation, the user is reloading or force joining
+	const forceJoin = SessionStorage.getItem('joined_conversation') === token
 
 	try {
 		const response = await axios.post(generateOcsUrl('apps/spreed/api/v2', 2) + `room/${token}/participants/active`, {
-			force: isReloading,
+			force: forceJoin,
 		})
 
 		// FIXME Signaling should not be synchronous
@@ -54,7 +56,18 @@ const joinConversation = async(token) => {
 		return response
 	} catch (error) {
 		if (error.response.status === 409) {
-			await confirmForceJoinConversation(token)
+			const responseData = error.response.data.ocs.data
+			let maxLastPingAge = new Date().getTime() / 1000 - 40
+			if (responseData.inCall !== PARTICIPANT.CALL_FLAG.DISCONNECTED) {
+				// When the user is/was in a call, we accept 20 seconds more delay
+				maxLastPingAge -= 20
+			}
+			if (maxLastPingAge > responseData.lastPing) {
+				console.debug('Force joining automatically because the old session didn\'t ping for 40 seconds')
+				await forceJoinConversation(token)
+			} else {
+				await confirmForceJoinConversation(token)
+			}
 		} else {
 			console.debug(error)
 			showError(t('spreed', 'Failed to join the conversation. Try to reload the page.'))
@@ -78,11 +91,15 @@ const confirmForceJoinConversation = async(token) => {
 				window.location = generateUrl('/apps/spreed')
 			} else {
 				// Confirm
-				SessionStorage.setItem('joined_conversation', token)
-				joinConversation(token)
+				forceJoinConversation(token)
 			}
 		}
 	)
+}
+
+const forceJoinConversation = async(token) => {
+	SessionStorage.setItem('joined_conversation', token)
+	await joinConversation(token)
 }
 
 /**
