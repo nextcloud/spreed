43bea7d613290f7db406855f97a9dff090c95a56
Daniel Calviño Sánchez <danxuliu@gmail.com>
2020-07-16 12:40:14 +0200

    Remove unused code
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/utils/webrtc/simplewebrtc/simplewebrtc.js
+++ src/utils/webrtc/simplewebrtc/simplewebrtc.js
@@ -82,12 +82,6 @@ function SimpleWebRTC(opts) {
 		connection = this.connection = this.config.connection
 	}
 
-	connection.on('connect', function() {
-		self.emit('connectionReady', connection.getSessionId())
-		self.sessionReady = true
-		self.testReadiness()
-	})
-
 	connection.on('message', function(message) {
 		const peers = self.webrtc.getPeers(message.from, message.roomType)
 		let peer
@@ -153,11 +147,6 @@ function SimpleWebRTC(opts) {
 	if (config.debug) {
 		this.on('*', this.logger.log.bind(this.logger, 'SimpleWebRTC event:'))
 	}
-
-	// check for readiness
-	this.webrtc.on('localStream', function() {
-		self.testReadiness()
-	})
 
 	this.webrtc.on('message', function(payload) {
 		self.connection.emit('message', payload)
@@ -425,17 +414,6 @@ SimpleWebRTC.prototype.stopScreenShare = function() {
 			peer.end()
 		}
 	})
-}
-
-SimpleWebRTC.prototype.testReadiness = function() {
-	const self = this
-	if (this.sessionReady) {
-		if (!this.config.media.video && !this.config.media.audio) {
-			self.emit('readyToCall', self.connection.getSessionId())
-		} else if (this.webrtc.localStreams.length > 0) {
-			self.emit('readyToCall', self.connection.getSessionId())
-		}
-	}
 }
 
 SimpleWebRTC.prototype.createRoom = function(name, cb) {
