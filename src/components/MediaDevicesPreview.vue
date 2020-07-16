8832daf8c9fb838dabc4b140a647fe5d50d289b2
Daniel Calviño Sánchez <danxuliu@gmail.com>
2020-07-16 17:19:44 +0200

    Add components to select and preview media devices
    MediaDevicesSelector is a dropdown to select a media device of a
    specific kind.
    
    MediaDevicesPreview has selectors for audio and video inputs that modify
    the devices to be used in MediaDevicesManager. Additionally it also
    shows a preview of the selected devices with a volume bar in the case of
    audio or a video element in the case of video.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

---
+++ src/components/MediaDevicesPreview.vue
@@ -0,0 +1,407 @@
+<!--
+  - @copyright Copyright (c) 2020, Daniel Calviño Sánchez (danxuliu@gmail.com)
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+	<div class="mediaDevicesPreview">
+		<MediaDevicesSelector kind="audioinput"
+			:devices="devices"
+			:device-id="audioInputId"
+			:enabled="enabled"
+			@update:deviceId="audioInputId = $event" />
+		<div class="preview preview-audio">
+			<div v-if="!audioPreviewAvailable"
+				class="preview-not-available">
+				<div v-if="audioStreamError"
+					class="icon icon-error" />
+				<div v-else-if="!audioInputId"
+					class="icon icon-audio-off" />
+				<div v-else-if="!enabled"
+					class="icon icon-audio" />
+				<div v-else-if="!audioStream"
+					class="icon icon-loading" />
+			</div>
+			<!-- v-show has to be used instead of v-if/else to ensure that the
+				 reference is always valid once mounted. -->
+			<div v-show="audioPreviewAvailable"
+				class="volume-indicator-wrapper">
+				<div class="icon icon-audio" />
+				<span ref="volumeIndicator"
+					class="volume-indicator"
+					:style="{ 'height': currentVolumeIndicatorHeight + 'px' }" />
+			</div>
+		</div>
+		<MediaDevicesSelector kind="videoinput"
+			:devices="devices"
+			:device-id="videoInputId"
+			:enabled="enabled"
+			@update:deviceId="videoInputId = $event" />
+		<div class="preview preview-video">
+			<div v-if="!videoPreviewAvailable"
+				class="preview-not-available">
+				<div v-if="videoStreamError"
+					class="icon icon-error" />
+				<div v-else-if="!videoInputId"
+					class="icon icon-video-off" />
+				<div v-else-if="!enabled"
+					class="icon icon-video" />
+				<div v-else-if="!videoStream"
+					class="icon icon-loading" />
+			</div>
+			<!-- v-show has to be used instead of v-if/else to ensure that the
+				 reference is always valid once mounted. -->
+			<video v-show="videoPreviewAvailable"
+				ref="video"
+				tabindex="-1" />
+		</div>
+	</div>
+</template>
+
+<script>
+import attachMediaStream from 'attachmediastream'
+import hark from 'hark'
+import { mediaDevicesManager } from '../utils/webrtc/index'
+import MediaDevicesSelector from './MediaDevicesSelector'
+
+export default {
+
+	name: 'MediaDevicesPreview',
+
+	components: {
+		MediaDevicesSelector,
+	},
+
+	props: {
+		enabled: {
+			type: Boolean,
+			required: true,
+		},
+	},
+
+	data() {
+		return {
+			mounted: false,
+			mediaDevicesManager: mediaDevicesManager,
+			audioStream: null,
+			audioStreamError: false,
+			videoStream: null,
+			videoStreamError: false,
+			hark: null,
+			currentVolume: -100,
+			volumeThreshold: -100,
+		}
+	},
+
+	computed: {
+		devices() {
+			return mediaDevicesManager.attributes.devices
+		},
+
+		audioInputId: {
+			get() {
+				return mediaDevicesManager.attributes.audioInputId
+			},
+			set(value) {
+				mediaDevicesManager.attributes.audioInputId = value
+			},
+		},
+
+		videoInputId: {
+			get() {
+				return mediaDevicesManager.attributes.videoInputId
+			},
+			set(value) {
+				mediaDevicesManager.attributes.videoInputId = value
+			},
+		},
+
+		audioPreviewAvailable() {
+			return this.audioInputId && this.audioStream
+		},
+
+		videoPreviewAvailable() {
+			return this.videoInputId && this.videoStream
+		},
+
+		currentVolumeIndicatorHeight() {
+			// refs can not be accessed on the initial render, only after the
+			// component has been mounted.
+			if (!this.mounted) {
+				return 0
+			}
+
+			// WebRTC volume goes from -100 (silence) to 0 (loudest sound in the
+			// system); for the volume indicator only sounds above the threshold
+			// are taken into account.
+			let currentVolumeProportion = 0
+			if (this.currentVolume > this.volumeThreshold) {
+				currentVolumeProportion = (this.volumeThreshold - this.currentVolume) / this.volumeThreshold
+			}
+
+			const volumeIndicatorStyle = window.getComputedStyle ? getComputedStyle(this.$refs.volumeIndicator, null) : this.$refs.volumeIndicator.currentStyle
+
+			const maximumVolumeIndicatorHeight = this.$refs.volumeIndicator.parentElement.clientHeight - (parseInt(volumeIndicatorStyle.bottom, 10) * 2)
+
+			return maximumVolumeIndicatorHeight * currentVolumeProportion
+		},
+
+	},
+
+	watch: {
+		enabled(enabled) {
+			if (this.enabled) {
+				this.mediaDevicesManager.enableDeviceEvents()
+				this.updateAudioStream()
+				this.updateVideoStream()
+			} else {
+				this.mediaDevicesManager.disableDeviceEvents()
+				this.stopAudioStream()
+				this.stopVideoStream()
+			}
+		},
+
+		audioInputId(audioInputId) {
+			if (!this.enabled) {
+				return
+			}
+
+			this.updateAudioStream()
+		},
+
+		videoInputId(videoInputId) {
+			if (!this.enabled) {
+				return
+			}
+
+			this.updateVideoStream()
+		},
+	},
+
+	mounted() {
+		this.mounted = true
+
+		if (!this.mediaDevicesManager.isSupported()) {
+			// DOMException constructor is not supported in Internet Explorer,
+			// so a plain object is used instead.
+			this.audioStreamError = {
+				message: 'MediaDevicesManager is not supported',
+				name: 'NotSupportedError',
+			}
+			this.videoStreamError = {
+				message: 'MediaDevicesManager is not supported',
+				name: 'NotSupportedError',
+			}
+		}
+	},
+
+	destroyed() {
+		this.stopAudioStream()
+		this.stopVideoStream()
+
+		if (this.enabled) {
+			this.mediaDevicesManager.disableDeviceEvents()
+		}
+	},
+
+	methods: {
+
+		updateAudioStream() {
+			if (!this.mediaDevicesManager.isSupported()) {
+				return
+			}
+
+			// When the audio input device changes the previous stream must be
+			// stopped before a new one is requested, as for example currently
+			// Firefox does not support having two different audio input devices
+			// active at the same time:
+			// https://bugzilla.mozilla.org/show_bug.cgi?id=1468700
+			this.stopAudioStream()
+
+			this.audioStreamError = false
+
+			if (!this.audioInputId) {
+				return
+			}
+
+			this.mediaDevicesManager.getUserMedia({ audio: true })
+				.then(stream => {
+					this.setAudioStream(stream)
+				})
+				.catch(error => {
+					console.error('Error getting audio stream: ' + error.name + ': ' + error.message)
+					this.audioStreamError = true
+					this.setAudioStream(null)
+				})
+		},
+
+		updateVideoStream() {
+			if (!this.mediaDevicesManager.isSupported()) {
+				return
+			}
+
+			// Video stream is stopped too to avoid potential issues similar to
+			// the audio ones (see "updateAudioStream").
+			this.stopVideoStream()
+
+			this.videoStreamError = false
+
+			if (!this.videoInputId) {
+				return
+			}
+
+			this.mediaDevicesManager.getUserMedia({ video: true })
+				.then(stream => {
+					this.setVideoStream(stream)
+				})
+				.catch(error => {
+					console.error('Error getting video stream: ' + error.name + ': ' + error.message)
+					this.videoStreamError = true
+					this.setVideoStream(null)
+				})
+		},
+
+		setAudioStream(audioStream) {
+			this.audioStream = audioStream
+
+			if (!audioStream) {
+				return
+			}
+
+			this.hark = hark(this.audioStream)
+			this.hark.on('volume_change', (volume, volumeThreshold) => {
+				this.currentVolume = volume
+				this.volumeThreshold = volumeThreshold
+			})
+		},
+
+		setVideoStream(videoStream) {
+			this.videoStream = videoStream
+
+			if (!this.$refs.video) {
+				return
+			}
+
+			if (!videoStream) {
+				return
+			}
+
+			const options = {
+				autoplay: true,
+				mirror: true,
+				muted: true,
+			}
+			attachMediaStream(videoStream, this.$refs.video, options)
+		},
+
+		stopAudioStream() {
+			if (!this.audioStream) {
+				return
+			}
+
+			this.audioStream.getTracks().forEach(function(track) {
+				track.stop()
+			})
+
+			this.audioStream = null
+
+			if (this.hark) {
+				this.hark.stop()
+				this.hark.off('volume_change')
+				this.hark = null
+			}
+		},
+
+		stopVideoStream() {
+			if (!this.videoStream) {
+				return
+			}
+
+			this.videoStream.getTracks().forEach(function(track) {
+				track.stop()
+			})
+
+			this.videoStream = null
+
+			if (this.$refs.video) {
+				this.$refs.video.srcObject = null
+			}
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.preview {
+	display: flex;
+	justify-content: center;
+
+	.icon {
+		background-size: 64px;
+		width: 64px;
+		height: 64px;
+		opacity: 0.4;
+	}
+}
+
+.preview-audio {
+	.preview-not-available .icon {
+		margin-top: 16px;
+		margin-bottom: 16px;
+	}
+
+	.volume-indicator-wrapper {
+		/* Make the wrapper the positioning context of the volume indicator. */
+		position: relative;
+
+		margin-top: 16px;
+		margin-bottom: 16px;
+	}
+
+	.volume-indicator {
+		position: absolute;
+
+		width: 6px;
+		right: 0;
+
+		/* The button height is 64px; the volume indicator button is 56px at
+		 * maximum, but its value will be changed based on the current volume;
+		 * the height change will reveal more or less of the gradient, which has
+		 * absolute dimensions and thus does not change when the height
+		 * changes. */
+		height: 56px;
+		bottom: 4px;
+
+		background: linear-gradient(0deg, green, yellow, red 54px);
+
+		opacity: 0.7;
+	}
+}
+
+.preview-video {
+	.preview-not-available .icon {
+		margin-top: 64px;
+		margin-bottom: 64px;
+	}
+
+	video {
+		display: block;
+		max-height: 192px;
+	}
+}
+</style>
---
+++ src/components/MediaDevicesSelector.vue
@@ -0,0 +1,166 @@
+<!--
+  - @copyright Copyright (c) 2020, Daniel Calviño Sánchez (danxuliu@gmail.com)
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+	<div class="media-devices-selector">
+		<label :for="deviceSelectorId">{{ deviceSelectorLabel }}</label>
+		<Multiselect :id="deviceSelectorId"
+			v-model="deviceSelectedOption"
+			:options="deviceOptions"
+			track-by="id"
+			label="label"
+			:allow-empty="false"
+			:placeholder="deviceSelectorPlaceholder"
+			:disabled="!enabled || !deviceOptionsAvailable" />
+	</div>
+</template>
+
+<script>
+import Multiselect from '@nextcloud/vue/dist/Components/Multiselect'
+
+export default {
+
+	name: 'MediaDevicesSelector',
+
+	components: {
+		Multiselect,
+	},
+
+	props: {
+		kind: {
+			validator(value) {
+				return ['audioinput', 'videoinput'].indexOf(value) !== -1
+			},
+			required: true,
+		},
+		devices: {
+			type: Array,
+			required: true,
+		},
+		deviceId: {
+			type: String,
+			default: undefined,
+		},
+		enabled: {
+			type: Boolean,
+			default: true,
+		},
+	},
+
+	data() {
+		return {
+			deviceSelectedOption: null,
+		}
+	},
+
+	computed: {
+		deviceSelectorId() {
+			return 'device-selector-' + this.kind
+		},
+
+		deviceSelectorLabel() {
+			if (this.kind === 'audioinput') {
+				return t('spreed', 'Microphone:')
+			}
+
+			if (this.kind === 'videoinput') {
+				return t('spreed', 'Camera:')
+			}
+
+			return null
+		},
+
+		deviceOptionsAvailable() {
+			return this.deviceOptions.length > 0
+		},
+
+		deviceSelectorPlaceholder() {
+			if (this.kind === 'audioinput') {
+				return this.audioInputSelectorPlaceholder
+			}
+
+			if (this.kind === 'videoinput') {
+				return this.videoInputSelectorPlaceholder
+			}
+
+			return null
+		},
+
+		audioInputSelectorPlaceholder() {
+			if (!this.deviceOptionsAvailable) {
+				return t('spreed', 'No microphone available')
+			}
+
+			return t('spreed', 'Select microphone')
+		},
+
+		videoInputSelectorPlaceholder() {
+			if (!this.deviceOptionsAvailable) {
+				return t('spreed', 'No camera available')
+			}
+
+			return t('spreed', 'Select camera')
+		},
+
+		deviceOptions() {
+			return this.devices.filter(device => device.kind === this.kind).map(device => {
+				return {
+					id: device.deviceId,
+					label: device.label ? device.label : device.fallbackLabel,
+				}
+			})
+		},
+
+		deviceSelectedOptionFromDeviceId() {
+			return this.deviceOptions.find(option => option.id === this.deviceId)
+		},
+	},
+
+	watch: {
+		deviceSelectedOptionFromDeviceId(deviceSelectedOptionFromDeviceId) {
+			this.deviceSelectedOption = deviceSelectedOptionFromDeviceId
+		},
+
+		deviceSelectedOption(deviceSelectedOption) {
+			if (deviceSelectedOption && deviceSelectedOption.id === null) {
+				this.$emit('update:deviceId', null)
+				return
+			}
+			this.$emit('update:deviceId', deviceSelectedOption ? deviceSelectedOption.id : undefined)
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.media-devices-selector {
+	display: flex;
+	align-items: center;
+
+	margin-top: 5px;
+	margin-bottom: 5px;
+
+	.multiselect {
+		flex-grow: 1;
+
+		margin-left: 5px;
+	}
+}
+</style>
