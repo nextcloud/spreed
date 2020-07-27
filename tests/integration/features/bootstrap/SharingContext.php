gomberg5264/maxwell333

Also sort the sharee suggestions by token
Signed-off-by: Joas Schilling <coding@schilljs.com>

tests/integration/features/bootstrap/SharingContext.php
@@ -647,6 +647,12 @@ class SharingContext implements Context {
 				$sharees[] = $expectedSharee;
 			}
 			$respondedArray = $this->getArrayOfShareesResponded($this->response, $shareeType);
+			usort($sharees, function($a, $b) {
+				return $a[2] <=> $b[2]; // Sort by token
+			});
+			usort($respondedArray, function($a, $b) {
+				return $a[2] <=> $b[2]; // Sort by token
+			});
 			\PHPUnit\Framework\Assert::assertEquals($sharees, $respondedArray);
 		} else {
 			$respondedArray = $this->getArrayOfShareesResponded($this->response, $shareeType);

nextcloud/spreed

tests/integration/features/bootstrap/SharingContext.php
@@ -647,6 +647,12 @@ class SharingContext implements Context {
 				$sharees[] = $expectedSharee;
 			}
 			$respondedArray = $this->getArrayOfShareesResponded($this->response, $shareeType);
+			usort($sharees, function ($a, $b) {
+				return $a[2] <=> $b[2]; // Sort by token
+			});
+			usort($respondedArray, function ($a, $b) {
+				return $a[2] <=> $b[2]; // Sort by token
+			});
 			\PHPUnit\Framework\Assert::assertEquals($sharees, $respondedArray);
 		} else {
 			$respondedArray = $this->getArrayOfShareesResponded($this->response, $shareeType);