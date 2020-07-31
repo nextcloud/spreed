25052fae56cf547d5a836759c0bf11c176df15d4
Joachim Bauch <bauch@struktur.de>
2020-07-31 16:59:57 +0200

    Pass base URL of Nextcloud to signaling server in backend requests.
    Signed-off-by: Joachim Bauch <bauch@struktur.de>

--- lib/Signaling/BackendNotifier.php
+++ lib/Signaling/BackendNotifier.php
@@ -29,6 +29,7 @@ use OCA\Talk\Participant;
 use OCA\Talk\Room;
 use OCP\Http\Client\IClientService;
 use OCP\ILogger;
+use OCP\IUrlGenerator;
 use OCP\Security\ISecureRandom;
 
 class BackendNotifier {
@@ -107,6 +108,7 @@ class BackendNotifier {
 		$hash = hash_hmac('sha256', $random . $body, $this->config->getSignalingSecret());
 		$headers['Spreed-Signaling-Random'] = $random;
 		$headers['Spreed-Signaling-Checksum'] = $hash;
+		$headers['Spreed-Signaling-Backend'] = $this->urlGenerator->getBaseUrl();
 
 		$params = [
 			'headers' => $headers,
