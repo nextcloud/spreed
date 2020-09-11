25c24778145ea0728eda67b827c5776afba1828d
Daniel Calviño Sánchez <danxuliu@gmail.com>
2020-09-11 21:18:10 +0200

    Unregister handler once SpeakingWhileMutedWarner is no longer needed
    This fixes a memory leak of the view associated to the
    SpeakingWhileMutedWarner as well as any other component that the view is
    part of (due to the parent component reference of Vue).
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/components/CallView/shared/LocalMediaControls.vue
+++ src/components/CallView/shared/LocalMediaControls.vue
@@ -339,6 +339,10 @@ export default {
 		this.speakingWhileMutedWarner = new SpeakingWhileMutedWarner(this.model, this)
 	},
 
+	beforeDestroy() {
+		this.speakingWhileMutedWarner.destroy()
+	},
+
 	methods: {
 
 		toggleAudio() {
