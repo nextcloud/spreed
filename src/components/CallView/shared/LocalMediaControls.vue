9f66cd18cfaeb6404ccc79046eb696dfa3bdf6da
Marco Ambrosini <marcoambrosini@pm.me>
2020-10-13 11:27:01 +0200

    Max width on quality warnings
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- src/components/CallView/shared/LocalMediaControls.vue
+++ src/components/CallView/shared/LocalMediaControls.vue
@@ -589,9 +589,10 @@ export default {
 }
 
 .hint {
-	padding: 4px;
+	padding: 12px;
+	max-width: 300px;
 	text-align: left;
-	&__actions{
+	&__actions {
 		display: flex;
 		flex-direction: row-reverse;
 		justify-content: space-between;
