d1343e28de06115aa4a61e69587de343d8ed90fa
Vincent Petry <vincent@nextcloud.com>
2020-10-21 18:18:09 +0200

    Clear error notification when leaving call
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- src/components/CallView/shared/LocalVideo.vue
+++ src/components/CallView/shared/LocalVideo.vue
@@ -121,6 +121,12 @@ export default {
 		}
 	},
 
+	data() {
+		return {
+			notificationHandle: null,
+		}
+	},
+
 	computed: {
 
 		videoContainerClass() {
@@ -295,15 +301,15 @@ export default {
 			handler: function(error) {
 				if (error) {
 					if (error.name === 'NotAllowedError') {
-						showError(t('spreed', 'Access to camera was denied'))
+						this.notificationHandle = showError(t('spreed', 'Access to camera was denied'))
 					} else if (error.name === 'NotReadableError' || error.name === 'AbortError') {
 						// when camera in use, Chrome gives NotReadableError, Firefox gives AbortError
-						showError(t('spreed', 'Error while accessing camera: it is likely in use by another program'), {
+						this.notificationHandle = showError(t('spreed', 'Error while accessing camera: it is likely in use by another program'), {
 							timeout: TOAST_PERMANENT_TIMEOUT,
 						})
 					} else {
 						console.error('Error while accessing camera: ', error.message, error.name)
-						showError(t('spreed', 'Error while accessing camera'), {
+						this.notificationHandle = showError(t('spreed', 'Error while accessing camera'), {
 							timeout: TOAST_PERMANENT_TIMEOUT,
 						})
 					}
@@ -333,6 +339,9 @@ export default {
 	},
 
 	destroyed() {
+		if (this.notificationHandle) {
+			this.notificationHandle.hideToast()
+		}
 		if (this.localCallParticipantModel) {
 			this.localCallParticipantModel.off('forcedMute', this._handleForcedMute)
 		}
