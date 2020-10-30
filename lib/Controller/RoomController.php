4b5118fbbcd7d22bff75537325e01b8fdd7a52c2
Joas Schilling <coding@schilljs.com>
2020-10-30 09:29:24 +0100

    Keep file rooms
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/RoomController.php
+++ lib/Controller/RoomController.php
@@ -1059,6 +1059,7 @@ class RoomController extends AEnvironmentAwareController {
 		}
 
 		if ($room->getType() !== Room::CHANGELOG_CONVERSATION &&
+			$room->getObjectType() !== 'file' &&
 			$room->getNumberOfParticipants() === 1) {
 			$room->deleteRoom();
 			return new DataResponse();
