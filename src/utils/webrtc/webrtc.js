954d9601a04391cb99c9fbd828994d6db79fa1fb
Joas Schilling <coding@schilljs.com>
2020-11-11 12:01:14 +0100

    Don't show the SIP connection as guest
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/utils/webrtc/webrtc.js
+++ src/utils/webrtc/webrtc.js
@@ -237,6 +237,9 @@ function usersChanged(signaling, newUsers, disconnectedSessionIds) {
 			})
 		}
 		callParticipantModel.setUserId(userId)
+		if (user.internal) {
+			callParticipantModel.set('internal', true)
+		}
 
 		// When the MCU is used and the other participant has no streams or
 		// when no MCU is used and neither the local participant nor the
