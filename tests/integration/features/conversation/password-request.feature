nextcloud/spreed

commit 4afa2d7946f121d07610eeb0165b20ee135fb819

Do not create the participant when we don't need them
Signed-off-by: Joas Schilling <coding@schilljs.com>

lib/PublicShareAuth/Listener.php
@@ -24,6 +24,7 @@ declare(strict_types=1);
 
 namespace OCA\Spreed\PublicShareAuth;
 
+use OCA\Spreed\Exceptions\ParticipantNotFoundException;
 use OCA\Spreed\Participant;
 use OCA\Spreed\Room;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -92,15 +93,15 @@ class Listener {
 			return;
 		}
 
-		$participants = $room->getParticipants();
-		$users = $participants['users'];
-		$guests = $participants['guests'];
-
-		if (array_key_exists($userId, $users) && $users[$userId]['participantType'] === Participant::OWNER) {
-			return;
+		try {
+			$participant = $room->getParticipant($userId);
+			if ($participant->getParticipantType() === Participant::OWNER) {
+				return;
+			}
+		} catch (ParticipantNotFoundException $e) {
 		}
 
-		if (\count($users) > 1 || \count($guests) > 0) {
+		if ($room->getActiveGuests() > 0 || \count($room->getParticipantUserIds()) > 1) {
 			throw new \OverflowException('Only the owner and another participant are allowed in rooms to request the password for a share');
 		}
 	}
@@ -119,11 +120,7 @@ class Listener {
 			return;
 		}
 
-		$participants = $room->getParticipants();
-		$users = $participants['users'];
-		$guests = $participants['guests'];
-
-		if (\count($users) > 1 || \count($guests) > 0) {
+		if ($room->getActiveGuests() > 0 || \count($room->getParticipantUserIds()) > 1) {
 			throw new \OverflowException('Only the owner and another participant are allowed in rooms to request the password for a share');
 		}
 	}

lib/Activity/Hooks.php
@@ -77,8 +77,7 @@ class Hooks {
 		}
 
 		$duration = $this->timeFactory->getTime() - $activeSince->getTimestamp();
-		$participants = $room->getParticipants($activeSince->getTimestamp());
-		$userIds = array_map('\strval', array_keys($participants['users']));
+		$userIds = $room->getParticipantUserIds($activeSince->getTimestamp());
 
 		if (empty($userIds) || (\count($userIds) === 1 && $room->getActiveGuests() === 0)) {
 			// Single user pinged or guests only => no activity

lib/Chat/AutoComplete/SearchPlugin.php
@@ -58,10 +58,9 @@ class SearchPlugin implements ISearchPlugin {
 	 * @since 13.0.0
 	 */
 	public function search($search, $limit, $offset, ISearchResult $searchResult) {
-		$participants = $this->room->getParticipants();
 
-		$this->searchUsers($search, array_map('strval', array_keys($participants['users'])), $searchResult);
 		// FIXME Handle guests
+		$this->searchUsers($search, $this->room->getParticipantUserIds(), $searchResult);
 
 		return false;
 	}

lib/Controller/RoomController.php
@@ -769,7 +769,7 @@ class RoomController extends OCSController {
 			return new DataResponse([], Http::STATUS_FORBIDDEN);
 		}
 
-		$participants = $room->getParticipants();
+		$participants = $room->getParticipantUserIds();
 
 		$updateRoomType = $room->getType() === Room::ONE_TO_ONE_CALL ? Room::GROUP_CALL : false;
 		$participantsToAdd = [];
@@ -779,7 +779,7 @@ class RoomController extends OCSController {
 				return new DataResponse([], Http::STATUS_NOT_FOUND);
 			}
 
-			if (isset($participants['users'][$newParticipant])) {
+			if (\in_array($newParticipant, $participants, true)) {
 				return new DataResponse([]);
 			}
 
@@ -794,7 +794,7 @@ class RoomController extends OCSController {
 
 			$usersInGroup = $group->getUsers();
 			foreach ($usersInGroup as $user) {
-				if (isset($participants['users'][$user->getUID()])) {
+				if (\in_array($user->getUID(), $participants, true)) {
 					continue;
 				}
 

lib/Room.php
@@ -828,6 +828,32 @@ class Room {
 	}
 
 	/**
+	 * @param int $lastPing When the last ping is older than the given timestamp, the user is ignored
+	 * @return string[]
+	 */
+	public function getParticipantUserIds(int $lastPing = 0): array {
+		$query = $this->db->getQueryBuilder();
+		$query->select('*')
+			->from('talk_participants')
+			->where($query->expr()->eq('room_id', $query->createNamedParameter($this->getId(), IQueryBuilder::PARAM_INT)))
+			->andWhere($query->expr()->nonEmptyString('user_id'));
+
+		if ($lastPing > 0) {
+			$query->andWhere($query->expr()->gt('last_ping', $query->createNamedParameter($lastPing, IQueryBuilder::PARAM_INT)));
+		}
+
+		$result = $query->execute();
+
+		$users = [];
+		while ($row = $result->fetch()) {
+			$users[] = $row['user_id'];
+		}
+		$result->closeCursor();
+
+		return $users;
+	}
+
+	/**
 	 * @param int $notificationLevel
 	 * @return Participant[] Array of participants
 	 */

lib/Share/Helper/DeletedShareAPIController.php
@@ -27,6 +27,7 @@ namespace OCA\Spreed\Share\Helper;
 use OCA\Spreed\Exceptions\RoomNotFoundException;
 use OCA\Spreed\Manager;
 use OCA\Spreed\Room;
+use OCP\IUser;
 use OCP\IUserManager;
 use OCP\Share\IShare;
 
@@ -87,10 +88,15 @@ class DeletedShareAPIController {
 		// the other participant.
 		$roomName = $room->getName();
 		if ($room->getType() === Room::ONE_TO_ONE_CALL) {
-			$participantsList = $room->getParticipants()['users'];
-			unset($participantsList[$this->userId]);
-
-			$roomName = $this->userManager->get(key($participantsList))->getDisplayName();
+			$userIds = $room->getParticipantUserIds();
+			foreach ($userIds as $userId) {
+				if ($this->userId !== $userId) {
+					$user = $this->userManager->get($userId);
+					if ($user instanceof IUser) {
+						$roomName = $user->getDisplayName();
+					}
+				}
+			}
 		}
 
 		$result['share_with_displayname'] = $roomName;

lib/Share/Helper/ShareAPIController.php
@@ -30,6 +30,7 @@ use OCA\Spreed\Manager;
 use OCA\Spreed\Room;
 use OCP\AppFramework\OCS\OCSNotFoundException;
 use OCP\IL10N;
+use OCP\IUser;
 use OCP\IUserManager;
 use OCP\Share\IShare;
 
@@ -94,10 +95,15 @@ class ShareAPIController {
 		// the other participant.
 		$roomName = $room->getName();
 		if ($room->getType() === Room::ONE_TO_ONE_CALL) {
-			$participantsList = $room->getParticipants()['users'];
-			unset($participantsList[$this->userId]);
-
-			$roomName = $this->userManager->get(key($participantsList))->getDisplayName();
+			$userIds = $room->getParticipantUserIds();
+			foreach ($userIds as $userId) {
+				if ($this->userId !== $userId) {
+					$user = $this->userManager->get($userId);
+					if ($user instanceof IUser) {
+						$roomName = $user->getDisplayName();
+					}
+				}
+			}
 		}
 
 		$result['share_with_displayname'] = $roomName;

lib/Share/RoomShareProvider.php
@@ -902,14 +902,14 @@ class RoomShareProvider implements IShareProvider {
 					continue;
 				}
 
-				$userList = $room->getParticipants()['users'];
-				foreach ($userList as $uid => $participantData) {
-					$users[$uid] = isset($users[$uid]) ? $users[$uid] : [];
+				$userList = $room->getParticipantUserIds();
+				foreach ($userList as $uid) {
+					$users[$uid] = $users[$uid] ?? [];
 					$users[$uid][$row['id']] = $row;
 				}
 			} else if ($type === self::SHARE_TYPE_USERROOM && $currentAccess === true) {
 				$uid = $row['share_with'];
-				$users[$uid] = isset($users[$uid]) ? $users[$uid] : [];
+				$users[$uid] = $users[$uid] ?? [];
 				$users[$uid][$row['id']] = $row;
 			}
 		}

commit 88aa97a5f90cad4f74d0a986f5f1dc1aba4f3c8d

Fix additional participants being able to join a password request room
This fixes a regression introduced in 4afa2d7946.

The active guests are only those who are currently in a call, and not
those who are currently in the conversation. Therefore other guests or
users were not prevented from joining a password request conversation if
a guest was in the conversation but not in the call.

Fortunately in practice this was not a problem, as the Web UI starts a
call immediately after joining the conversation, which made the guest
immediately active and thus prevented others from joining.

Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

lib/PublicShareAuth/Listener.php
@@ -91,8 +91,7 @@ class Listener {
 		}
 
 		$participantService = \OC::$server->get(ParticipantService::class);
-		$users = $participantService->getParticipantUserIds($room);
-		if ($room->getActiveGuests() > 0 || \count($users) > 1) {
+		if ($participantService->getNumberOfActors($room) > 1) {
 			throw new \OverflowException('Only the owner and another participant are allowed in rooms to request the password for a share');
 		}
 	}
@@ -112,8 +111,7 @@ class Listener {
 		}
 
 		$participantService = \OC::$server->get(ParticipantService::class);
-		$users = $participantService->getParticipantUserIds($room);
-		if ($room->getActiveGuests() > 0 || \count($users) > 1) {
+		if ($participantService->getNumberOfActors($room) > 1) {
 			throw new \OverflowException('Only the owner and another participant are allowed in rooms to request the password for a share');
 		}
 	}

tests/integration/features/bootstrap/FeatureContext.php
@@ -489,6 +489,10 @@ class FeatureContext implements Context, SnippetAcceptingContext {
 		);
 		$this->assertStatusCode($this->response, $statusCode);
 
+		if ($statusCode !== '200') {
+			return;
+		}
+
 		$response = $this->getDataFromResponse($this->response);
 		if (array_key_exists('sessionId', $response)) {
 			// In the chat guest users are identified by their sessionId. The

tests/integration/features/conversation/password-request.feature
@@ -3,6 +3,7 @@ Feature: conversation/password-request
   Background:
     Given user "participant1" exists
     Given user "participant2" exists
+    Given user "participant3" exists
 
   Scenario: create password-request room for file shared by link
     Given user "participant1" shares "welcome.txt" by link with OCS 100
@@ -73,6 +74,42 @@ Feature: conversation/password-request
       | sendPasswordByTalk | true |
     And user "guest" creates the password request room for last share with 201
     When user "participant1" joins room "password request for last share room" with 200
+
+  Scenario: other guests can not join the password request room when a guest already joined
+    Given user "participant1" shares "welcome.txt" by link with OCS 100
+      | password | 123456 |
+      | sendPasswordByTalk | true |
+    And user "guest" creates the password request room for last share with 201
+    And user "guest" joins room "password request for last share room" with 200
+    When user "guest2" joins room "password request for last share room" with 404
+    Then user "guest2" is not participant of room "password request for last share room"
+
+  Scenario: other guests can not join the password request room when a user already joined
+    Given user "participant1" shares "welcome.txt" by link with OCS 100
+      | password | 123456 |
+      | sendPasswordByTalk | true |
+    And user "participant2" creates the password request room for last share with 201
+    And user "participant2" joins room "password request for last share room" with 200
+    When user "guest" joins room "password request for last share room" with 404
+    Then user "guest" is not participant of room "password request for last share room"
+
+  Scenario: other users can not join the password request room when a guest already joined
+    Given user "participant1" shares "welcome.txt" by link with OCS 100
+      | password | 123456 |
+      | sendPasswordByTalk | true |
+    And user "guest" creates the password request room for last share with 201
+    And user "guest" joins room "password request for last share room" with 200
+    When user "participant2" joins room "password request for last share room" with 404
+    Then user "participant2" is not participant of room "password request for last share room"
+
+  Scenario: other users can not join the password request room when a user already joined
+    Given user "participant1" shares "welcome.txt" by link with OCS 100
+      | password | 123456 |
+      | sendPasswordByTalk | true |
+    And user "participant2" creates the password request room for last share with 201
+    And user "participant2" joins room "password request for last share room" with 200
+    When user "participant3" joins room "password request for last share room" with 404
+    Then user "participant3" is not participant of room "password request for last share room"
 
 
 