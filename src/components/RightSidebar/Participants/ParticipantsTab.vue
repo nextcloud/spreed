27bb14bf1c9dd24d9e7b82a561bd0d4fe42f76d3
Daniel Calviño Sánchez <danxuliu@gmail.com>
2020-11-24 10:14:01 +0100

    Inform the user if an error occurred when adding participants
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/components/RightSidebar/Participants/ParticipantsTab.vue
+++ src/components/RightSidebar/Participants/ParticipantsTab.vue
@@ -209,6 +209,7 @@ export default {
 				this.cancelableGetParticipants()
 			} catch (exception) {
 				console.debug(exception)
+				showError(t('spreed', 'An error occurred while adding the participants'))
 			}
 		},
 
