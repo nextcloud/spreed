614dd5088dbfb13fb2a10d6085f2b4ac1c1bce5d
Daniel Calviño Sánchez <danxuliu@gmail.com>
2021-01-14 20:25:13 +0100

    Account for the local video in the "slots" property
    This will be needed for proper calculations once pagination is enabled
    in the full grid view, as the local video will be shown in all pages and
    thus its slot will not be available.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/components/CallView/Grid/Grid.vue
+++ src/components/CallView/Grid/Grid.vue
@@ -310,18 +310,14 @@ export default {
 			}
 		},
 
-		// Number of video components (includes localvideo if not stripe)
+		// Number of video components (it does not include the local video)
 		videosCount() {
-			if (this.isStripe) {
-				return this.videos.length
-			} else {
+			if (!this.isStripe && this.videos.length === 0) {
 				// Count the emptycontent as a grid element
-				if (this.videos.length === 0) {
-					return 2
-				}
-				// Add the local video to the count
-				return this.videos.length + 1
+				return 1
 			}
+
+			return this.videos.length
 		},
 		videoWidth() {
 			return this.gridWidth / this.columns
@@ -363,8 +359,10 @@ export default {
 		},
 
 		// Number of grid slots at any given moment
+		// The local video always takes one slot if the grid view is not shown
+		// as a stripe.
 		slots() {
-			return this.rows * this.columns
+			return this.isStripe ? this.rows * this.columns : this.rows * this.columns - 1
 		},
 
 		// Hides or displays the `grid-navigation next` button
@@ -599,7 +597,7 @@ export default {
 
 			let currentColumns = this.columns
 			let currentRows = this.rows
-			let currentSlots = currentColumns * currentRows
+			let currentSlots = this.isStripe ? currentColumns * currentRows : currentColumns * currentRows - 1
 
 			// Run this code only if we don't have an 'overflow' of videos. If the
 			// videos are populating the grid, there's no point in shrinking it.
@@ -632,7 +630,7 @@ export default {
 						currentColumns--
 					}
 
-					currentSlots = currentColumns * currentRows
+					currentSlots = this.isStripe ? currentColumns * currentRows : currentColumns * currentRows - 1
 
 					// Check that there are still enough slots available
 					if (numberOfVideos > currentSlots) {
@@ -645,7 +643,7 @@ export default {
 						currentRows--
 					}
 
-					currentSlots = currentColumns * currentRows
+					currentSlots = this.isStripe ? currentColumns * currentRows : currentColumns * currentRows - 1
 
 					// Check that there are still enough slots available
 					if (numberOfVideos > currentSlots) {
