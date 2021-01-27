c9517c2095f4e608f135ed4f2378369c3da05883
Vincent Petry <vincent@nextcloud.com>
2021-01-27 14:27:44 +0100

    Align wording about free space with server
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- src/store/fileUploadStore.js
+++ src/store/fileUploadStore.js
@@ -251,7 +251,7 @@ const actions = {
 				let reason = 'failed-upload'
 				if (exception.response.status === 507) {
 					reason = 'quota'
-					showError(t('spreed', 'Error while uploading file "{fileName}": insufficient storage', { fileName }))
+					showError(t('spreed', 'Not enough free space for uploading "{fileName}"', { fileName }))
 				} else {
 					showError(t('spreed', 'Error while uploading file "{fileName}"', { fileName }))
 				}
