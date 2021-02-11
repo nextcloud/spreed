345950641a92b85afeab4c84c3cbdebdb21c07b2
Joas Schilling <coding@schilljs.com>
2021-02-11 09:07:05 +0100

    Run all tests on info.xml modifications
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/drone-run-php-tests.sh
+++ tests/drone-run-php-tests.sh
@@ -8,6 +8,8 @@ echo "========================="
 
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | wc -l) -eq 0 ]] && echo "No files are modified => merge commit" && exit 0
 
+[[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep "appinfo/info.xml") -gt 0 ]] && echo "info.xml is modified" && exit 0
+
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep --invert-match "^tests/" | grep -c ".php$") -gt 0 ]] && echo "PHP files are modified" && exit 0
 
 [[ $(git diff --name-only origin/$DRONE_TARGET_BRANCH...$DRONE_COMMIT_SHA | grep -c "^tests/php/") -gt 0 ]] && echo "PHP test files are modified" && exit 0
