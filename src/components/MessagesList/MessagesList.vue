579fbdc791f32be262d668a218ee38bad7449a32
Vincent Petry <vincent@nextcloud.com>
2021-03-04 14:10:40 +0100

    Remove delay for unread marker on focus
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- src/components/MessagesList/MessagesList.vue
+++ src/components/MessagesList/MessagesList.vue
@@ -257,14 +257,14 @@ export default {
 	},
 
 	watch: {
-		isWindowVisible: debounce(function(visible) {
+		isWindowVisible(visible) {
 			if (visible) {
-				this.scrollToFocussedMessage()
 				this.onWindowFocus()
+				// FIXME: the sidebar chat takes much longer to open, this is why we need a higher value here
+				// need to investigate why the sidebar takes that long to open and is not even animated
+				window.setTimeout(() => this.scrollToFocussedMessage(), 100)
 			}
-			// FIXME: the sidebar takes much longer to open, this is why we need a higher value here
-			// need to investigate why the sidebar takes that long to open and is not even animated
-		}, 100),
+		},
 		chatIdentifier: {
 			immediate: true,
 			handler() {
