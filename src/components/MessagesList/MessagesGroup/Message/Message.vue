b19eef1ca98097b46d0f4733277378457fce4000
Vincent Petry <vincent@nextcloud.com>
2021-03-04 09:31:56 +0100

    Force menu in message row
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>
    
    Co-authored-by: Joas Schilling <213943+nickvergessen@users.noreply.github.com>

--- src/components/MessagesList/MessagesGroup/Message/Message.vue
+++ src/components/MessagesList/MessagesGroup/Message/Message.vue
@@ -125,6 +125,7 @@ the main body of the message as well as a quote.
 					</Actions>
 					<Actions
 						v-show="hasActionsMenu"
+						:force-menu="true"
 						container="#content-vue">
 						<ActionButton
 							v-if="isPrivateReplyable"
