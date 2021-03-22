9e962b22df80f04aca0bd3c3fd237e5d8f3e70db
Joas Schilling <coding@schilljs.com>
2021-03-22 12:27:12 +0100

    Temporary OCS API for user avatar upload and delete
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -90,6 +90,7 @@ class Capabilities implements IPublicCapability {
 				'raise-hand',
 				'room-description',
 				'rich-object-sharing',
+				'temp-user-avatar-api',
 			],
 			'config' => [
 				'attachments' => [
