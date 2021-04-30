d7f207e63c255a69d4b92482b8c1d2537e1f0136
Vincent Petry <vincent@nextcloud.com>
2021-04-30 10:58:21 +0200

    Add JS tests for new group conversation store
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

---
+++ src/store/newGroupConversationStore.spec.js
@@ -0,0 +1,52 @@
+import Vuex from 'vuex'
+import { cloneDeep } from 'lodash'
+import { createLocalVue } from '@vue/test-utils'
+
+import newGroupConversationStore from './newGroupConversationStore'
+
+describe('newGroupConversationStore', () => {
+	let localVue = null
+	let store = null
+
+	beforeEach(() => {
+		localVue = createLocalVue()
+		localVue.use(Vuex)
+
+		store = new Vuex.Store(cloneDeep(newGroupConversationStore))
+	})
+
+	afterEach(() => {
+		jest.clearAllMocks()
+	})
+
+	test('toggles selected participants', () => {
+		store.dispatch('updateSelectedParticipants', { id: 'participant-1' })
+		store.dispatch('updateSelectedParticipants', { id: 'participant-2' })
+		store.dispatch('updateSelectedParticipants', { id: 'participant-3' })
+
+		expect(store.getters.selectedParticipants).toStrictEqual([
+			{ id: 'participant-1' },
+			{ id: 'participant-2' },
+			{ id: 'participant-3' },
+		])
+
+		store.dispatch('updateSelectedParticipants', { id: 'participant-2' })
+
+		expect(store.getters.selectedParticipants).toStrictEqual([
+			{ id: 'participant-1' },
+			{ id: 'participant-3' },
+		])
+	})
+
+	test('purges selection', () => {
+		expect(store.getters.selectedParticipants).toStrictEqual([])
+
+		store.dispatch('updateSelectedParticipants', { id: 'participant-1' })
+		store.dispatch('updateSelectedParticipants', { id: 'participant-2' })
+
+		store.dispatch('purgeNewGroupConversationStore')
+
+		expect(store.getters.selectedParticipants).toStrictEqual([])
+	})
+
+})
