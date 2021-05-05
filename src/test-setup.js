7cdd5b077402b0e95e86ff1a66be17730d230105
Vincent Petry <vincent@nextcloud.com>
2021-05-05 11:12:10 +0200

    Added tests for RoomSelector
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- src/test-setup.js
+++ src/test-setup.js
@@ -22,6 +22,7 @@
 
 // eslint-disable-next-line node/no-unpublished-import
 import 'regenerator-runtime/runtime'
+import Vue from 'vue'
 
 global.OC = {
 	requestToken: '123',
@@ -36,4 +37,12 @@ global.OC = {
 		return 'en_GB'
 	},
 }
+global.OCA = {
+	Talk: {
+	},
+}
 global.t = jest.fn().mockImplementation((app, text) => text)
+
+Vue.prototype.t = global.t
+Vue.prototype.OC = OC
+Vue.prototype.OCA = OCA
