2d0c0bc7617fdd212d15794c7744e2c3474b48cc
Daniel Calviño Sánchez <danxuliu@gmail.com>
2021-05-20 02:53:06 +0200

    Do not show an error if explicitly joining a call without media
    "TypeError" is thrown by "getUserMedia" when requesting media without
    audio nor video. In that case no error message should be shown, as the
    devices would have been explicitly disabled by the user.
    
    That could also happen when audio was disabled and video was enabled but
    failed. If the initial "getUserMedia" call done when starting a call
    failed and video was requested it was retried again without video in all
    cases. Now it is only retried if audio is also enabled, as otherwise it
    would end requesting no audio and no video (and no error message would
    be shown, as a "TypeError" would be thrown).
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/utils/webrtc/webrtc.js
+++ src/utils/webrtc/webrtc.js
@@ -960,6 +960,12 @@ export default function initWebRTC(signaling, _callParticipantCollection, _local
 
 		clearLocalStreamRequestedTimeoutAndHideNotification()
 
+		if (error.name === 'TypeError') {
+			// Both audio and video were explicitly disabled, no need to show an
+			// error.
+			return
+		}
+
 		let message
 		let timeout = TOAST_PERMANENT_TIMEOUT
 		if ((error.name === 'NotSupportedError'
