445b0b8b9380e30a37484bbbfd67fd0103cb1a02
Daniel Calviño Sánchez <danxuliu@gmail.com>
2021-06-25 12:39:46 +0200

    Convert "_connectionQuality" attribute to an array
    This makes the attribute consistent with the stats, and will also make
    possible to directly get the quality value given a string identifying
    its kind ("audio" or "video").
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/utils/webrtc/analyzers/PeerConnectionAnalyzer.js
+++ src/utils/webrtc/analyzers/PeerConnectionAnalyzer.js
@@ -121,8 +121,10 @@ function PeerConnectionAnalyzer() {
 	this._handleIceConnectionStateChangedBound = this._handleIceConnectionStateChanged.bind(this)
 	this._processStatsBound = this._processStats.bind(this)
 
-	this._connectionQualityAudio = CONNECTION_QUALITY.UNKNOWN
-	this._connectionQualityVideo = CONNECTION_QUALITY.UNKNOWN
+	this._connectionQuality = {
+		audio: CONNECTION_QUALITY.UNKNOWN,
+		video: CONNECTION_QUALITY.UNKNOWN,
+	}
 }
 PeerConnectionAnalyzer.prototype = {
 
@@ -162,28 +164,28 @@ PeerConnectionAnalyzer.prototype = {
 	},
 
 	getConnectionQualityAudio() {
-		return this._connectionQualityAudio
+		return this._connectionQuality.audio
 	},
 
 	getConnectionQualityVideo() {
-		return this._connectionQualityVideo
+		return this._connectionQuality.video
 	},
 
 	_setConnectionQualityAudio(connectionQualityAudio) {
-		if (this._connectionQualityAudio === connectionQualityAudio) {
+		if (this._connectionQuality.audio === connectionQualityAudio) {
 			return
 		}
 
-		this._connectionQualityAudio = connectionQualityAudio
+		this._connectionQuality.audio = connectionQualityAudio
 		this._trigger('change:connectionQualityAudio', [connectionQualityAudio])
 	},
 
 	_setConnectionQualityVideo(connectionQualityVideo) {
-		if (this._connectionQualityVideo === connectionQualityVideo) {
+		if (this._connectionQuality.video === connectionQualityVideo) {
 			return
 		}
 
-		this._connectionQualityVideo = connectionQualityVideo
+		this._connectionQuality.video = connectionQualityVideo
 		this._trigger('change:connectionQualityVideo', [connectionQualityVideo])
 	},
 
