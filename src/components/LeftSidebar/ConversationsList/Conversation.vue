c4c29ebfb8935510a8e91976cdaa644fe02ea50d
Marco Ambrosini <marcoambrosini@pm.me>
2021-07-06 17:35:16 +0200

    Fix highlighted counter bug
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- src/components/LeftSidebar/ConversationsList/Conversation.vue
+++ src/components/LeftSidebar/ConversationsList/Conversation.vue
@@ -154,10 +154,11 @@ export default {
 			},
 		},
 	},
+
 	computed: {
 
 		counterShouldBePrimary() {
-			return this.item.unreadMention || (this.item.unreadMessages && this.item.type === CONVERSATION.TYPE.ONE_TO_ONE)
+			return this.item.unreadMention || (this.item.unreadMessages !== 0 && this.item.type === CONVERSATION.TYPE.ONE_TO_ONE)
 		},
 
 		linkToConversation() {
