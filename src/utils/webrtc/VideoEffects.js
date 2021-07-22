77df53cd979f5848cf92ac63f2a7c965b68670c7
Immanuel Pasanec <immanuel.pasanec@compaso.de>
2021-07-22 10:46:52 +0200

    Integrated tflite selfieSegmentation. Following problems need to be solved:
    Stopping a call does not stop the tflite processes. Restarting
    the call is possible but starts new instances of the segmentation
    processes which results in rising cpu load with every newly started
    call.
    
    The segmentation pipeline is chosen by availability. That results in bad
    performance if webgl is available but 3D acceleration is turned off or
    your graphics card is weak. Canvas 2d offers much better performance in
    these cases. The easiest solution would be to let the user choose the
    pipeline.
    
    Signed-off-by: Immanuel Pasanec <immanuel.pasanec@compaso.de>

--- src/utils/webrtc/VideoEffects.js
+++ src/utils/webrtc/VideoEffects.js
@@ -1,42 +1,66 @@
 import * as bodyPix from '@tensorflow-models/body-pix'
 // import * as tf from '@tensorflow/tfjs'
 import '@tensorflow/tfjs'
+import { blur } from '../videofx/src/core/vanilla/blur'
+import '../videofx/public/models/segm_full_v679.tflite'
+import '../videofx/public/models/segm_lite_v681.tflite'
+import '../videofx/public/models/selfiesegmentation_mlkit-256x256-2021_01_19-v1215.f16.tflite'
+import '../videofx/public/tflite/tflite.wasm'
+import '../videofx/public/tflite/tflite-simd.wasm'
 
 export default function VideoEffects() {
 	this._videoSource = document.createElement('video')
 	this._videoSource.muted = 'muted'
 	this._temporaryCanvas = document.createElement('canvas')
 	this._playing = false
-	this._videoSource.addEventListener('loadeddata', (this._videoSourceListener.bind(this)))
 }
 
 VideoEffects.prototype = {
-	getBlurredVideoStream(stream) {
-		this._stream = stream
-		this._videoSource.height = this._stream.getVideoTracks()[0].getSettings().height
-		this._videoSource.width = this._stream.getVideoTracks()[0].getSettings().width
-		this._temporaryCanvas.height = this._stream.getVideoTracks()[0].getSettings().height
-		this._temporaryCanvas.width = this._stream.getVideoTracks()[0].getSettings().width
-		this._videoSource.srcObject = this._stream
-		this._videoSource.play()
-		this._playing = true
-		this._canvasBlurredStream = this._temporaryCanvas.captureStream()
-		let extractedAudio = false
-		extractedAudio = this._stream.getTracks().filter(function(track) {
-			return track.kind === 'audio'
-		})[0]
-		if (extractedAudio) {
-			this._canvasBlurredStream.addTrack(extractedAudio)
+	getBlurredVideoStream(stream, model = 1) {
+		this._model = model
+		switch (model) {
+		case 0:
+			return this._useBodyPix(stream)
+			// break
+		case 1:
+			return this._useTfLite(stream)
+			// break
+		default:
+			return this._useBodyPix(stream)
+			// break
 		}
-		this._stopStreamBound = this._stopStream.bind(this)
-		// mainStreamEnded is sent in
-		this._canvasBlurredStream.addEventListener('mainStreamEnded', this._stopStreamBound)
-		return this._canvasBlurredStream
 	},
 
 	_stopStream() {
+		switch (this._model) {
+		case 0:
+			this._stopBodyPixStream()
+			break
+		case 1:
+			this._stopTfLiteStream()
+			break
+		default:
+			this._stopBodyPixStream()
+			break
+		}
+	},
+
+	_stopBodyPixStream() {
 		this._playing = false
 		this._videoSource.removeEventListener('loadeddata', this._videoSourceListener)
+		this._attachment = null
+		this._temporaryCanvas = null
+		const tracks = this._stream.getTracks()
+		tracks.forEach(track => {
+			track.stop()
+		})
+		this._stream = null
+	},
+
+	_stopTfLiteStream() {
+		this._playing = false
+		this._attachment = null
+		this._temporaryCanvas = null
 		const tracks = this._stream.getTracks()
 		tracks.forEach(track => {
 			track.stop()
@@ -80,6 +104,64 @@ VideoEffects.prototype = {
 
 	_videoSourceListener(e) {
 		this._loadBodyPix()
+	},
+
+	_useBodyPix(stream) {
+		this._stream = stream
+		this._videoSource.addEventListener('loadeddata', (this._videoSourceListener.bind(this)))
+		this._videoSource.height = this._stream.getVideoTracks()[0].getSettings().height
+		this._videoSource.width = this._stream.getVideoTracks()[0].getSettings().width
+		this._temporaryCanvas.height = this._stream.getVideoTracks()[0].getSettings().height
+		this._temporaryCanvas.width = this._stream.getVideoTracks()[0].getSettings().width
+		this._videoSource.srcObject = this._stream
+		this._videoSource.play()
+		this._playing = true
+		this._canvasBlurredStream = this._temporaryCanvas.captureStream()
+		let extractedAudio = false
+		extractedAudio = this._stream.getTracks().filter(function(track) {
+			return track.kind === 'audio'
+		})[0]
+		if (extractedAudio) {
+			this._canvasBlurredStream.addTrack(extractedAudio)
+		}
+		this._stopStreamBound = this._stopStream.bind(this)
+		// mainStreamEnded is sent in localmedia
+		this._canvasBlurredStream.addEventListener('mainStreamEnded', this._stopStreamBound)
+		return this._canvasBlurredStream
+	},
+
+	_useTfLite(stream) {
+		this._stream = stream
+		this._videoSource.height = this._stream.getVideoTracks()[0].getSettings().height
+		this._videoSource.width = this._stream.getVideoTracks()[0].getSettings().width
+		this._temporaryCanvas.height = this._stream.getVideoTracks()[0].getSettings().height
+		this._temporaryCanvas.width = this._stream.getVideoTracks()[0].getSettings().width
+		this._videoSource.srcObject = this._stream
+		this._videoSource.play()
+		this._playing = true
+		// this._attachment = document.body.appendChild(this._temporaryCanvas)
+		// this._attachment.style.position = 'absolute'
+		// this._attachment.style.top = '30vh'
+		// this._attachment.style.left = '30vh'
+		// this._attachment.style.zIndex = '99999'
+
+		// setupVideo(this._videoSource, this._temporaryCanvas)
+		blur(this._videoSource, this._temporaryCanvas)
+
+		this._canvasBlurredStream = this._temporaryCanvas.captureStream()
+		let extractedAudio = false
+		extractedAudio = this._stream.getTracks().filter(function(track) {
+			return track.kind === 'audio'
+		})[0]
+		if (extractedAudio) {
+			this._canvasBlurredStream.addTrack(extractedAudio)
+		}
+		this._stopStreamBound = this._stopStream.bind(this)
+		// mainStreamEnded is sent in
+		this._canvasBlurredStream.addEventListener('mainStreamEnded', this._stopStreamBound)
+		// console.log(this._canvasBlurredStream)
+		return this._canvasBlurredStream
+
 	},
 
 	_stream: null,
