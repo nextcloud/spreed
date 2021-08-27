3da5277deefdaa29aa1032c7d8b9a9b6c5ee139a
Joas Schilling <coding@schilljs.com>
2021-08-27 11:42:25 +0200

    Fix JS CS
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/collections.js
+++ src/collections.js
@@ -50,7 +50,7 @@ import Vue from 'vue'
 							// on the store need to be explicitly injected.
 							container: window.store ? window.store.getters.getMainContainerSelector() : undefined,
 						},
-					})
+					}),
 				})
 				ComponentVM.$mount(container)
 				ComponentVM.$root.$on('close', () => {
