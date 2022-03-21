8b979bbfca1aa09ff62e2d4d61349c369a9d2852
Joas Schilling <coding@schilljs.com>
2022-03-21 17:30:34 +0100

    Do not load the session in getParticipantByActor() as no caller needs it
    Also fixes a problem with MariaDB ONLY_FULL_GROUP_BY and Oracle
    
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Chat/Parser/UserMention.php
+++ lib/Chat/Parser/UserMention.php
@@ -132,7 +132,7 @@ class UserMention {
 				];
 			} elseif ($mention['type'] === 'guest') {
 				try {
-					$participant = $chatMessage->getRoom()->getParticipantByActor(Attendee::ACTOR_GUESTS, substr($mention['id'], strlen('guest/')), false);
+					$participant = $chatMessage->getRoom()->getParticipantByActor(Attendee::ACTOR_GUESTS, substr($mention['id'], strlen('guest/')));
 					$displayName = $participant->getAttendee()->getDisplayName() ?: $this->l->t('Guest');
 				} catch (ParticipantNotFoundException $e) {
 					$displayName = $this->l->t('Guest');
