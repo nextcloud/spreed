3e514ada8accad984048712f3f5574db9aa8190d
Joas Schilling <coding@schilljs.com>
2022-03-23 09:35:48 +0100

    Require new HPB version
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/utils/signaling.js
+++ src/utils/signaling.js
@@ -977,7 +977,7 @@ Signaling.Standalone.prototype.helloResponseReceived = function(data) {
 		}
 	}
 
-	if (!this.hasFeature('audio-video-permissions')) {
+	if (!this.hasFeature('audio-video-permissions') || !this.hasFeature('incall-all')) {
 		showError(
 			t('spreed', 'The configured signaling server needs to be updated to be compatible with this version of Talk. Please contact your administrator.'),
 			{
