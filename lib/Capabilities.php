3ab61a3437d6ec82d0df1d6598694097f87ca603
Vitor Mattos <vitor@php.rio>
2022-05-13 12:55:30 -0300

    Only set ttl capability if systemcron is enabled
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -111,7 +111,6 @@ class Capabilities implements IPublicCapability {
 				'silent-send',
 				'silent-call',
 				'send-call-notification',
-				'time-to-live',
 			],
 			'config' => [
 				'attachments' => [
@@ -134,6 +133,10 @@ class Capabilities implements IPublicCapability {
 			],
 			'version' => $this->appManager->getAppVersion('spreed'),
 		];
+
+		if ($this->serverConfig->getAppValue('core', 'backgroundjobs_mode', '') === 'cron') {
+			$capabilities['features'][] = 'time-to-live';
+		}
 
 		if ($this->commentsManager->supportReactions()) {
 			$capabilities['features'][] = 'reactions';
