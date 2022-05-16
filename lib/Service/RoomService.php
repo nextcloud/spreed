0c26145e59ac7f34c5049c59f178c47cd918ee59
Joas Schilling <coding@schilljs.com>
2022-05-16 15:10:15 +0200

    Cache display name of owner as well
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Service/RoomService.php
+++ lib/Service/RoomService.php
@@ -151,6 +151,7 @@ class RoomService {
 			$this->participantService->addUsers($room, [[
 				'actorType' => Attendee::ACTOR_USERS,
 				'actorId' => $owner->getUID(),
+				'displayName' => $owner->getDisplayName(),
 				'participantType' => Participant::OWNER,
 			]], null);
 		}
