855c7883508e0313dc3425fd463026aa9bb54d01
marco <marcoambrosini@pm.me>
2022-05-18 16:31:28 +0200

    tidy up types folder
    Signed-off-by: marco <marcoambrosini@pm.me>

---
+++ src/types/vendor/@nextcloud/vue.d.ts
@@ -0,0 +1,4 @@
+declare module '@nextcloud/vue/dist/Directives/Tooltip.vue';
+declare module '@nextcloud/vue/dist/Components/Button.vue';
+declare module '@nextcloud/vue/dist/Components/Actions.vue';
+declare module '@nextcloud/vue/dist/Components/ActionButton.vue';
---
+++ src/types/vendor/vue-material-design-icons.d.ts
@@ -0,0 +1,2 @@
+declare module 'vue-material-design-icons/VideoOff.vue'
+declare module 'vue-material-design-icons/MenuDown.vue'
