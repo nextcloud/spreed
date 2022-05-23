69b239b33d36c0d22269db46155d5cf09d7b3237
marco <marcoambrosini@pm.me>
2022-05-23 17:08:00 +0200

    Use wildcards
    Signed-off-by: marco <marcoambrosini@pm.me>

--- src/types/vendor/@nextcloud/vue.d.ts
+++ src/types/vendor/@nextcloud/vue.d.ts
@@ -1,4 +1,1 @@
-declare module '@nextcloud/vue/dist/Directives/Tooltip.vue';
-declare module '@nextcloud/vue/dist/Components/Button.vue';
-declare module '@nextcloud/vue/dist/Components/Actions.vue';
-declare module '@nextcloud/vue/dist/Components/ActionButton.vue';
+declare module '@nextcloud/vue/*';
