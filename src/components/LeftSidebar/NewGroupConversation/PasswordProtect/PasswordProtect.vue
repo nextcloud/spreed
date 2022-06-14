6f0108f8217def5c2da39c201781270bdfa9a3b2
Joas Schilling <coding@schilljs.com>
2022-06-14 11:36:00 +0200

    Validate password with a debounce
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/components/LeftSidebar/NewGroupConversation/PasswordProtect/PasswordProtect.vue
+++ src/components/LeftSidebar/NewGroupConversation/PasswordProtect/PasswordProtect.vue
@@ -22,15 +22,21 @@
 <template>
 	<input ref="password"
 		v-observe-visibility="visibilityChanged"
+		v-tooltip.bottom="reason"
 		type="password"
 		autocomplete="new-password"
 		:value="value"
 		class="password-protect"
+		:class="{'weak-password': validPassword === false}"
 		:placeholder="t('spreed', 'Choose a password')"
+		:aria-label="reason"
 		@input="handleInput">
 </template>
 
 <script>
+
+import { validatePassword } from '../../../../services/conversationsService.js'
+import debounce from 'debounce'
 
 export default {
 	name: 'PasswordProtect',
@@ -42,10 +48,41 @@ export default {
 		},
 	},
 
+	data() {
+		return {
+			validPassword: null,
+			reason: '',
+		}
+	},
+
 	methods: {
 		handleInput(event) {
 			this.$emit('input', event.target.value)
+			if (event.target.value !== '') {
+				this.debounceValidatePassword()
+			}
 		},
+
+		debounceValidatePassword: debounce(function() {
+			this.validatePassword()
+		}, 250),
+
+		async validatePassword() {
+			try {
+				const response = await validatePassword(this.value)
+				this.validPassword = response.data.ocs.data.passed
+				if (!this.validPassword) {
+					this.reason = response.data.ocs.data.reason
+				} else {
+					this.reason = ''
+				}
+			} catch (e) {
+				console.debug('Password policy app seems not enabled')
+				this.validPassword = null
+				this.reason = ''
+			}
+		},
+
 		visibilityChanged(isVisible) {
 			if (isVisible) {
 				// Focus the input field of the current component.
@@ -63,5 +100,11 @@ export default {
 .password-protect {
 	width: calc(100% - 18px);
 	margin-left: 18px;
+
+	&.weak-password {
+		background-color: var(--color-error) !important;
+		border-color: var(--color-error) !important;
+		color: #fff !important;
+	}
 }
 </style>
