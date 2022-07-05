511a2628fc21b0a5694e760f3f626229c9782cf8
Joas Schilling <coding@schilljs.com>
2022-07-05 10:06:55 +0200

    Explicitly log SIP attempts as such
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/RoomController.php
+++ lib/Controller/RoomController.php
@@ -28,6 +28,7 @@ declare(strict_types=1);
 namespace OCA\Talk\Controller;
 
 use InvalidArgumentException;
+use OC\Security\Bruteforce\Throttler;
 use OCA\Talk\Chat\ChatManager;
 use OCA\Talk\Chat\MessageParser;
 use OCA\Talk\Config;
@@ -90,6 +91,7 @@ class RoomController extends AEnvironmentAwareController {
 	protected IL10N $l10n;
 	protected IConfig $config;
 	protected Config $talkConfig;
+	protected Throttler $throttler;
 	protected LoggerInterface $logger;
 
 	protected array $commonReadMessages = [];
@@ -115,6 +117,7 @@ class RoomController extends AEnvironmentAwareController {
 								IConfig $config,
 								Config $talkConfig,
 								ICloudIdManager $cloudIdManager,
+								Throttler $throttler,
 								LoggerInterface $logger) {
 		parent::__construct($appName, $request);
 		$this->session = $session;
@@ -136,6 +139,7 @@ class RoomController extends AEnvironmentAwareController {
 		$this->config = $config;
 		$this->talkConfig = $talkConfig;
 		$this->cloudIdManager = $cloudIdManager;
+		$this->throttler = $throttler;
 		$this->logger = $logger;
 	}
 
@@ -267,9 +271,11 @@ class RoomController extends AEnvironmentAwareController {
 		try {
 			$isSIPBridgeRequest = $this->validateSIPBridgeRequest($token);
 		} catch (UnauthorizedException $e) {
-			$response = new DataResponse([], Http::STATUS_UNAUTHORIZED);
-			$response->throttle();
-			return $response;
+			$ip = $this->request->getRemoteAddress();
+			$action = 'talkSipBridgeSecret';
+			$this->throttler->sleepDelay($ip, $action);
+			$this->throttler->registerAttempt($action, $ip);
+			return new DataResponse([], Http::STATUS_UNAUTHORIZED);
 		}
 
 		// The SIP bridge only needs room details (public, sip enabled, lobby state, etc)
