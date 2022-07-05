46f37e8e0ff9761f0d752a65a900df4ec5e7470b
Joas Schilling <coding@schilljs.com>
2022-07-05 11:34:59 +0200

    Also throttle on call/
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -119,6 +119,7 @@ class PageController extends Controller {
 	 * @PublicPage
 	 * @NoCSRFRequired
 	 * @UseSession
+	 * @BruteForceProtection(action=talkRoomToken)
 	 *
 	 * @param string $token
 	 * @return Response
