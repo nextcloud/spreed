52fd57e1a0edbab389161eb510b272ad81b22dc0
Joas Schilling <coding@schilljs.com>
2022-07-13 17:23:56 +0200

    Fix custom SVGs to match the new lib version
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/components/missingMaterialDesignIcons/Lobby.vue
+++ src/components/missingMaterialDesignIcons/Lobby.vue
@@ -1,17 +1,17 @@
-<template functional>
-	<span :aria-hidden="props.decorative"
-		:aria-label="props.title"
-		:class="[data.class, data.staticClass]"
-		class="material-design-icon present-to-all-icon"
+<template>
+	<span :aria-hidden="!title"
+		:aria-label="title"
+		class="material-design-icon lobby-icon"
 		role="img"
-		v-bind="data.attrs"
-		v-on="listeners">
-		<svg :fill="props.fillColor"
+		v-bind="$attrs"
+		@click="$emit('click', $event)">
+		<svg :fill="fillColor"
 			class="material-design-icon__svg"
-			:width="props.size"
-			:height="props.size"
+			:width="size"
+			:height="size"
 			viewBox="0 0 24 24">
 			<path d="M2 17h20v2H2zm11.84-9.21c.1-.24.16-.51.16-.79 0-1.1-.9-2-2-2s-2 .9-2 2c0 .28.06.55.16.79C6.25 8.6 3.27 11.93 3 16h18c-.27-4.07-3.25-7.4-7.16-8.21z" />
+			<title v-if="title">{{ title }}</title>
 		</svg>
 	</span>
 </template>
@@ -22,11 +22,7 @@ export default {
 	props: {
 		title: {
 			type: String,
-			default: 'Lobby icon',
-		},
-		decorative: {
-			type: Boolean,
-			default: false,
+			default: '',
 		},
 		fillColor: {
 			type: String,
--- src/components/missingMaterialDesignIcons/PresentToAll.vue
+++ src/components/missingMaterialDesignIcons/PresentToAll.vue
@@ -1,18 +1,18 @@
-<template functional>
-	<span :aria-hidden="props.decorative"
-		:aria-label="props.title"
-		:class="[data.class, data.staticClass]"
+<template>
+	<span :aria-hidden="!title"
+		:aria-label="title"
 		class="material-design-icon present-to-all-icon"
 		role="img"
-		v-bind="data.attrs"
-		v-on="listeners">
-		<svg :fill="props.fillColor"
+		v-bind="$attrs"
+		@click="$emit('click', $event)">
+		<svg :fill="fillColor"
 			class="material-design-icon__svg"
-			:width="props.size"
-			:height="props.size"
+			:width="size"
+			:height="size"
 			viewBox="0 0 24 24">
 			<path d="M0 0h24v24H0V0z" fill="none" />
 			<path d="M21 3H3c-1.11 0-2 .89-2 2v14c0 1.11.89 2 2 2h18c1.11 0 2-.89 2-2V5c0-1.11-.89-2-2-2zm0 16.02H3V4.98h18v14.04zM10 12H8l4-4 4 4h-2v4h-4v-4z" />
+			<title v-if="title">{{ title }}</title>
 		</svg>
 	</span>
 </template>
@@ -23,11 +23,7 @@ export default {
 	props: {
 		title: {
 			type: String,
-			default: 'Present To All icon',
-		},
-		decorative: {
-			type: Boolean,
-			default: false,
+			default: '',
 		},
 		fillColor: {
 			type: String,
