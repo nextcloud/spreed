0bd80674098c236e308e2b095f9fb5d0615b92db
Daniel Calviño Sánchez <danxuliu@gmail.com>
2022-08-24 17:15:34 +0200

    Remove unneeded assignment
    When "usersChanged(signaling, [], previousUsersInRoom)" is called
    "previousUsersInRoom" is assigned to the difference with itself and,
    therefore, to an empty array, so there is no need to explicitly do it
    afterwards.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/utils/webrtc/webrtc.js
+++ src/utils/webrtc/webrtc.js
@@ -921,7 +921,6 @@ export default function initWebRtc(signaling, _callParticipantCollection, _local
 
 		usersChanged(signaling, [], previousUsersInRoom)
 		usersInCallMapping = {}
-		previousUsersInRoom = []
 
 		// Reconnects with a new session id will trigger "usersChanged"
 		// with the users in the room and that will re-establish the
@@ -1705,7 +1704,6 @@ export default function initWebRtc(signaling, _callParticipantCollection, _local
 
 		usersChanged(signaling, [], previousUsersInRoom)
 		usersInCallMapping = {}
-		previousUsersInRoom = []
 	})
 
 	return webrtc
