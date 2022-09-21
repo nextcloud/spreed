9ed14991f63264b021e93d09878d283111d26a16
Joas Schilling <coding@schilljs.com>
2022-09-21 09:35:27 +0200

    Fix console error 'Missing required prop: "isForwarderOpen"' in jest
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/components/MessagesList/MessagesGroup/Message/MessageButtonsBar/MessageButtonsBar.spec.js
+++ src/components/MessagesList/MessagesGroup/Message/MessageButtonsBar/MessageButtonsBar.spec.js
@@ -52,6 +52,7 @@ describe('MessageButtonsBar.vue', () => {
 			canReact: true,
 			isReactionsMenuOpen: false,
 			isLastRead: false,
+			isForwarderOpen: false,
 			timestamp: new Date('2020-05-07 09:23:00').getTime() / 1000,
 			token: TOKEN,
 			systemMessage: '',
