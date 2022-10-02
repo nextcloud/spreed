53d842e475854b6172ddc76304769d2fa04966f6
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2022-10-03 01:03:46 +0200

    Added parameter to disallow creating of rooms with public access
    Added spreed application config parameter `public_rooms_allowed` to disallow
    creating of rooms with public (guest via link) access (i.e. for internal
    Nextcloud setups). Use
    
    ```
    occ config:app:set spreed public_rooms_allowed --value 'yes'
    ```
    
    to allow (default if not set) and
    
    ```
    occ config:app:set spreed public_rooms_allowed --value 'no'
    ```
    
    to disallow creating of rooms with public access.
    
    Related: https://github.com/nextcloud/spreed/issues/521
    Author-Change-Id: IB#1126215
    Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

--- lib/Manager.php
+++ lib/Manager.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2016 Joas Schilling <coding@schilljs.com>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -156,6 +157,7 @@ class Manager {
 		}
 
 		return new Room(
+			$this->config,
 			$this,
 			$this->db,
 			$this->dispatcher,
@@ -910,6 +912,12 @@ class Manager {
 	 * @return Room
 	 */
 	public function createRoom(int $type, string $name = '', string $objectType = '', string $objectId = ''): Room {
+
+		// Force group room if public room was requested and public rooms are disallowed.
+		if (($type === Room::TYPE_PUBLIC) && ($this->config->getAppValue('spreed', 'public_rooms_allowed', 'yes') === 'no')) {
+			$type = Room::TYPE_GROUP;
+		}
+
 		$token = $this->getNewToken();
 
 		$insert = $this->db->getQueryBuilder();
