nextcloud/spreed

commit a38fdfd0c2faada01a90b4dbdc31838bb2f8d694

Added parameter to disallow creating of rooms with public access
Added spreed application config parameter `public_rooms_allowed` to disallow
creating of rooms with public (guest via link) access (i.e. for internal
Nextcloud setups). Use

```
occ config:app:set spreed public_rooms_allowed --value 'yes'
```

to allow (default if not set) and

```
occ config:app:set spreed public_rooms_allowed --value 'no'
```

to disallow creating of rooms with public access.

Related: https://github.com/nextcloud/spreed/issues/521
Author-Change-Id: IB#1126215
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

lib/TInitialState.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2020 Joas Schilling <coding@schilljs.com>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -157,6 +158,11 @@ trait TInitialState {
 		$this->initialState->provideInitialState(
 			'enable_matterbridge',
 			$this->serverConfig->getAppValue('spreed', 'enable_matterbridge', '0') === '1'
+		);
+
+		$this->initialState->provideInitialState(
+			'public_rooms_allowed',
+			$this->serverConfig->getAppValue('spreed', 'public_rooms_allowed', 'yes') === 'yes'
 		);
 	}
 

lib/Manager.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2016 Joas Schilling <coding@schilljs.com>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -156,6 +157,7 @@ class Manager {
 		}
 
 		return new Room(
+			$this->config,
 			$this,
 			$this->db,
 			$this->dispatcher,
@@ -908,6 +910,12 @@ class Manager {
 	 * @return Room
 	 */
 	public function createRoom(int $type, string $name = '', string $objectType = '', string $objectId = ''): Room {
+
+		// Force group room if public room was requested and public rooms are disallowed.
+		if (($type === Room::TYPE_PUBLIC) && ($this->config->getAppValue('spreed', 'public_rooms_allowed', 'yes') === 'no')) {
+			$type = Room::TYPE_GROUP;
+		}
+
 		$token = $this->getNewToken();
 
 		$insert = $this->db->getQueryBuilder();

lib/Room.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2016 Lukas Reschke <lukas@statuscode.ch>
  * @copyright Copyright (c) 2016 Joas Schilling <coding@schilljs.com>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Lukas Reschke <lukas@statuscode.ch>
  * @author Joas Schilling <coding@schilljs.com>
@@ -38,6 +39,7 @@ use OCA\Talk\Service\ParticipantService;
 use OCA\Talk\Service\RoomService;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\Comments\IComment;
+use OCP\IConfig;
 use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IDBConnection;
@@ -185,8 +187,10 @@ class Room {
 
 	protected ?string $currentUser = null;
 	protected ?Participant $participant = null;
+	protected IConfig $config;
 
-	public function __construct(Manager $manager,
+	public function __construct(IConfig $config,
+								Manager $manager,
 								IDBConnection $db,
 								IEventDispatcher $dispatcher,
 								ITimeFactory $timeFactory,
@@ -216,6 +220,7 @@ class Room {
 								?\DateTime $lobbyTimer,
 								string $objectType,
 								string $objectId) {
+		$this->config = $config;
 		$this->manager = $manager;
 		$this->db = $db;
 		$this->dispatcher = $dispatcher;
@@ -257,6 +262,11 @@ class Room {
 	}
 
 	public function setType(int $type): void {
+		// Force group room if public room was requested and public rooms are disallowed.
+		if (($type === self::TYPE_PUBLIC) && ($this->config->getAppValue('spreed', 'public_rooms_allowed', 'yes') === 'no')) {
+			$type = self::TYPE_GROUP;
+		}
+
 		$this->type = $type;
 	}
 

lib/Settings/Admin/AdminSettings.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2019 Joas Schilling <coding@schilljs.com>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -141,6 +142,11 @@ class AdminSettings implements ISettings {
 		$this->initialState->provideInitialState(
 			'matterbridge_enable',
 			$this->serverConfig->getAppValue('spreed', 'enable_matterbridge', '0') === '1'
+		);
+
+		$this->initialState->provideInitialState(
+			'public_rooms_allowed',
+			$this->serverConfig->getAppValue('spreed', 'public_rooms_allowed', 'yes') === 'yes'
 		);
 	}
 

src/components/ConversationSettings/ConversationSettingsDialog.vue
@@ -1,5 +1,6 @@
 <!--
   - @copyright Copyright (c) 2020 Vincent Petry <vincent@nextcloud.com>
+  - @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
   -
   - @author Vincent Petry <vincent@nextcloud.com>
   -
@@ -53,7 +54,7 @@
 		</NcAppSettingsSection>
 
 		<!-- Guest access -->
-		<NcAppSettingsSection v-if="canFullModerate"
+		<NcAppSettingsSection v-if="canFullModerate && publicRoomsEnabled"
 			id="guests"
 			:title="t('spreed', 'Guests access')">
 			<LinkShareSettings ref="linkShareSettings" />
@@ -147,6 +148,7 @@ export default {
 		return {
 			showSettings: false,
 			matterbridgeEnabled: loadState('spreed', 'enable_matterbridge'),
+			publicRoomsEnabled: loadState('spreed', 'public_rooms_allowed'),
 			isEditingDescription: false,
 			isDescriptionLoading: false,
 			showDeviceChecker: false,
@@ -224,9 +226,11 @@ export default {
 		handleShowSettings({ token }) {
 			this.$store.dispatch('updateConversationSettingsToken', token)
 			this.showSettings = true
-			this.$nextTick(() => {
-				this.$refs.linkShareSettings.focus()
-			})
+			if (loadState('spreed', 'public_rooms_allowed')) {
+				this.$nextTick(() => {
+					this.$refs.linkShareSettings.focus()
+				})
+			}
 		},
 
 		handleHideSettings() {

src/components/LeftSidebar/NewGroupConversation/NewGroupConversation.vue
@@ -1,5 +1,6 @@
 <!--
   - @copyright Copyright (c) 2019 Marco Ambrosini <marcoambrosini@icloud.com>
+  - @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
   -
   - @author Marco Ambrosini <marcoambrosini@icloud.com>
   -
@@ -45,7 +46,7 @@
 					<template v-if="page === 0">
 						<SetConversationName v-model="conversationNameInput"
 							@click-enter="handleEnter" />
-						<NcCheckboxRadioSwitch :checked.sync="isPublic">
+						<NcCheckboxRadioSwitch :checked.sync="isPublic" v-if="publicRoomsEnabled">
 							{{ t('spreed', 'Allow guests to join via link') }}
 						</NcCheckboxRadioSwitch>
 						<!-- Password protection -->
@@ -138,6 +139,7 @@ import isInCall from '../../../mixins/isInCall.js'
 import participant from '../../../mixins/participant.js'
 import Tooltip from '@nextcloud/vue/dist/Directives/Tooltip.js'
 import { EventBus } from '../../../services/EventBus.js'
+import { loadState } from '@nextcloud/initial-state'
 
 export default {
 
@@ -177,6 +179,7 @@ export default {
 			password: '',
 			passwordProtect: false,
 			listable: CONVERSATION.LISTABLE.NONE,
+			publicRoomsEnabled: loadState('spreed', 'public_rooms_allowed'),
 		}
 	},
 

tests/php/Service/RoomServiceTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2020 Joas Schilling <coding@schilljs.com>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -38,6 +39,7 @@ use OCA\Talk\Webinary;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\BackgroundJob\IJobList;
 use OCP\EventDispatcher\IEventDispatcher;
+use OCP\IConfig;
 use OCP\IDBConnection;
 use OCP\IUser;
 use OCP\Security\IHasher;
@@ -354,6 +356,7 @@ class RoomServiceTest extends TestCase {
 		);
 
 		$room = new Room(
+			$this->createMock(IConfig::class),
 			$this->createMock(Manager::class),
 			$this->createMock(IDBConnection::class),
 			$dispatcher,

commit 4c645ce22f8c58d8d76bf1ed57efc239d569ccce

Adding e-mail addresses to room disabled when public rooms are disallowed
Fixes: a38fdfd0c2faada01a90b4dbdc31838bb2f8d694
Related: https://github.com/nextcloud/spreed/pull/8082#pullrequestreview-1128225548
Author-Change-Id: IB#1126215
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

lib/Controller/RoomController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2016 Lukas Reschke <lukas@statuscode.ch>
  * @copyright Copyright (c) 2016 Joas Schilling <coding@schilljs.com>
+ * @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
  *
  * @author Lukas Reschke <lukas@statuscode.ch>
  * @author Joas Schilling <coding@schilljs.com>
@@ -1115,6 +1116,11 @@ class RoomController extends AEnvironmentAwareController {
 
 			$this->participantService->addCircle($this->room, $circle, $participants);
 		} elseif ($source === 'emails') {
+			// E-mails cannot be added if public rooms are disabled.
+			if ($this->config->getAppValue('spreed', 'public_rooms_allowed', 'yes') === 'no') {
+				return new DataResponse([], Http::STATUS_BAD_REQUEST);
+			}
+
 			$data = [];
 			if ($this->roomService->setType($this->room, Room::TYPE_PUBLIC)) {
 				$data = ['type' => $this->room->getType()];

lib/TInitialState.php
@@ -107,6 +107,11 @@ trait TInitialState {
 		);
 
 		$this->initialState->provideInitialState(
+			'public_rooms_allowed',
+			$this->serverConfig->getAppValue('spreed', 'public_rooms_allowed', 'yes') === 'yes'
+		);
+
+		$this->initialState->provideInitialState(
 			'guests_accounts_enabled',
 			$appManager->isEnabledForUser('guests', $user)
 		);

src/components/RightSidebar/Participants/ParticipantsSearchResults/ParticipantsSearchResults.vue
@@ -1,5 +1,6 @@
 <!--
   - @copyright Copyright (c) 2020 Marco Ambrosini <marcoambrosini@icloud.com>
+  - @copyright Copyright (c) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
   -
   - @author Marco Ambrosini <marcoambrosini@icloud.com>
   -
@@ -34,7 +35,7 @@
 				@click="handleClickParticipant" />
 		</template>
 
-		<template v-if="addableEmails.length !== 0">
+		<template v-if="addableEmails.length !== 0 && publicRoomsEnabled">
 			<NcAppNavigationCaption :title="t('spreed', 'Add emails')" />
 			<ParticipantsList :items="addableEmails"
 				@click="handleClickParticipant" />
@@ -102,6 +103,7 @@ import NcAppNavigationCaption from '@nextcloud/vue/dist/Components/NcAppNavigati
 import Hint from '../../../Hint.vue'
 import AccountPlus from 'vue-material-design-icons/AccountPlus.vue'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
+import { loadState } from '@nextcloud/initial-state'
 
 export default {
 	name: 'ParticipantsSearchResults',
@@ -161,6 +163,12 @@ export default {
 			type: String,
 			default: '',
 		},
+	},
+
+	data() {
+		return {
+			publicRoomsEnabled: loadState('spreed', 'public_rooms_allowed'),
+		}
 	},
 
 	computed: {