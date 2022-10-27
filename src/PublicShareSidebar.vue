c1c105a1eb0a2d61fef23a70c9872224a98b7e55
Daniel Calviño Sánchez <danxuliu@gmail.com>
2022-10-28 01:19:31 +0200

    Show error when failing to join a conversation in the public share page
    Joining a conversation in the public share page fails if Talk use is
    limited to certain groups and the public share page is opened by a user
    not in those groups. The incognito mode is enabled when the public share
    page is loaded, so from the point of view of the event handler that adds
    the Talk sidebar script the user is always a guest, and therefore adding
    the script can not be prevented for users not allowed to use Talk.
    
    Moreover, when those users tried to join the conversation in the public
    share page the button was kept disabled with a loading spinner forever,
    and no error was shown to the user.
    
    For simplicity, and as it could be useful if joining the conversation
    fails for any other reason, instead of looking for other ways to hide
    the Talk sidebar if the user is not allowed to use Talk now, if joining
    the conversation in the public share page fails, the button is restored
    and an error toast is shown.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/PublicShareSidebar.vue
+++ src/PublicShareSidebar.vue
@@ -55,6 +55,7 @@
 <script>
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 import PreventUnload from 'vue-prevent-unload'
+import { showError } from '@nextcloud/dialogs'
 import { loadState } from '@nextcloud/initial-state'
 import CallView from './components/CallView/CallView.vue'
 import ChatView from './components/ChatView.vue'
@@ -154,9 +155,19 @@ export default {
 
 			this.joiningConversation = true
 
-			await this.getPublicShareConversationData()
+			try {
+				await this.getPublicShareConversationData()
 
-			await this.$store.dispatch('joinConversation', { token: this.token })
+				await this.$store.dispatch('joinConversation', { token: this.token })
+			} catch (exception) {
+				this.joiningConversation = false
+
+				showError(t('spreed', 'Error occurred when joining the conversation'))
+
+				console.error(exception)
+
+				return
+			}
 
 			// No need to wait for it, but fetching the conversation needs to be
 			// done once the user has joined the conversation (otherwise only
