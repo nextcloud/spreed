acba30f50177ddd28c6e77a6d48a780eeabbc9dd
Vitor Mattos <vitor@php.rio>
2022-11-21 11:12:54 -0300

    Changed the avatar length to exactly value
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Migration/Version16000Date20221116163301.php
+++ lib/Migration/Version16000Date20221116163301.php
@@ -47,7 +47,7 @@ class Version16000Date20221116163301 extends SimpleMigrationStep {
 		if (!$table->hasColumn('avatar')) {
 			$table->addColumn('avatar', Types::STRING, [
 				'notnull' => false,
-				'length' => 255,
+				'length' => 10,
 			]);
 			return $schema;
 		}
