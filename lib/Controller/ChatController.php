26b873883fca3bd59ddb84c48035da90b2f8bdfd
Joas Schilling <coding@schilljs.com>
2023-01-04 14:50:40 +0100

    Only call getDateTime() once
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/ChatController.php
+++ lib/Controller/ChatController.php
@@ -466,7 +466,7 @@ class ChatController extends AEnvironmentAwareController {
 		$this->preloadShares($comments);
 
 		$i = 0;
-		$now = $this->timeFactory->getDateTime();
+		$now = $this->timeFactory->getDateTime('now', new \DateTimeZone('UTC'));
 		$messages = $commentIdToIndex = $parentIds = [];
 		foreach ($comments as $comment) {
 			$id = (int) $comment->getId();
@@ -474,7 +474,7 @@ class ChatController extends AEnvironmentAwareController {
 			$this->messageParser->parseMessage($message);
 
 			$expireDate = $message->getComment()->getExpireDate();
-			if ($expireDate instanceof \DateTime && $expireDate < $this->timeFactory->getDateTime()) {
+			if ($expireDate instanceof \DateTime && $expireDate < $now) {
 				$message->setVisibility(false);
 			}
 
