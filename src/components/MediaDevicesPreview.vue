37e7889ee00f6886aeb6c45dc94af3a507c8b34a
Maksim Sukharev <antreesy.web@gmail.com>
2023-01-18 16:14:06 +0100

    apply component in Talk settings
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/MediaDevicesPreview.vue
+++ src/components/MediaDevicesPreview.vue
@@ -41,17 +41,12 @@
 					{{ audioStreamErrorMessage }}
 				</p>
 			</div>
-			<!-- v-show has to be used instead of v-if/else to ensure that the
-				 reference is always valid once mounted. -->
-			<div v-show="audioPreviewAvailable"
-				class="volume-indicator-wrapper">
-				<Microphone :size="64"
-					title=""
-					fill-color="#999" />
-				<span ref="volumeIndicator"
-					class="volume-indicator"
-					:style="{ 'height': currentVolumeIndicatorHeight + 'px' }" />
-			</div>
+			<VolumeIndicator v-else
+				:audio-preview-available="audioPreviewAvailable"
+				:audio-enabled="true"
+				:current-volume="currentVolume"
+				:volume-threshold="volumeThreshold"
+				:size="64" />
 		</div>
 		<MediaDevicesSelector kind="videoinput"
 			:devices="devices"
@@ -86,10 +81,12 @@
 
 <script>
 import AlertCircle from 'vue-material-design-icons/AlertCircle.vue'
-import Microphone from 'vue-material-design-icons/Microphone.vue'
 import MicrophoneOff from 'vue-material-design-icons/MicrophoneOff.vue'
 import VideoOff from 'vue-material-design-icons/VideoOff.vue'
+
 import MediaDevicesSelector from './MediaDevicesSelector.vue'
+import VolumeIndicator from './VolumeIndicator/VolumeIndicator.vue'
+
 import { devices } from '../mixins/devices.js'
 
 export default {
@@ -99,9 +96,9 @@ export default {
 	components: {
 		AlertCircle,
 		MediaDevicesSelector,
-		Microphone,
 		MicrophoneOff,
 		VideoOff,
+		VolumeIndicator,
 	},
 
 	mixins: [devices],
@@ -112,32 +109,6 @@ export default {
 			currentVolume: -100,
 			volumeThreshold: -100,
 		}
-	},
-
-	computed: {
-
-		currentVolumeIndicatorHeight() {
-			// refs can not be accessed on the initial render, only after the
-			// component has been mounted.
-			if (!this.mounted) {
-				return 0
-			}
-
-			// WebRTC volume goes from -100 (silence) to 0 (loudest sound in the
-			// system); for the volume indicator only sounds above the threshold
-			// are taken into account.
-			let currentVolumeProportion = 0
-			if (this.currentVolume > this.volumeThreshold) {
-				currentVolumeProportion = (this.volumeThreshold - this.currentVolume) / this.volumeThreshold
-			}
-
-			const volumeIndicatorStyle = window.getComputedStyle ? getComputedStyle(this.$refs.volumeIndicator, null) : this.$refs.volumeIndicator.currentStyle
-
-			const maximumVolumeIndicatorHeight = this.$refs.volumeIndicator.parentElement.clientHeight - (parseInt(volumeIndicatorStyle.bottom, 10) * 2)
-
-			return maximumVolumeIndicatorHeight * currentVolumeProportion
-		},
-
 	},
 
 	mounted() {
@@ -172,30 +143,7 @@ export default {
 	}
 
 	.volume-indicator-wrapper {
-		/* Make the wrapper the positioning context of the volume indicator. */
-		position: relative;
-
-		margin-top: 16px;
-		margin-bottom: 16px;
-	}
-
-	.volume-indicator {
-		position: absolute;
-
-		width: 6px;
-		right: 0;
-
-		/* The button height is 64px; the volume indicator button is 56px at
-		 * maximum, but its value will be changed based on the current volume;
-		 * the height change will reveal more or less of the gradient, which has
-		 * absolute dimensions and thus does not change when the height
-		 * changes. */
-		height: 56px;
-		bottom: 4px;
-
-		background: linear-gradient(0deg, green, yellow, red 54px);
-
-		opacity: 0.7;
+		margin: 16px auto;
 	}
 }
 
