8e1213a49b3f9fefe07a8744af892b062c11aef0
Daniel Calviño Sánchez <danxuliu@gmail.com>
2023-02-08 15:32:35 +0100

    Do not disinvite self-joined users when leaving
    If the external signaling server is used, when a participant is
    disinvited it is expected that the client leaves the conversation if
    still in it. Therefore, self-joined users should not be disinvited when
    they disconnect from a conversation, as they can still have other
    sessions in it.
    
    To ensure that self-joined users will be disinvited when their last
    session leaves the conversation now a USER_REMOVE event is explicitly
    triggered when that last session leaves and the user is, in fact,
    removed from the conversation.
    
    Note that this does not cause the system message about the participant
    leaving to be sent, as it is explicitly guarded against self-joined
    users leaving the conversation. However, this change will cause other
    handlers to be called now, like those to invalidate resources, but it is
    very likely that they should have been called and that this change is
    actually a fix too in that regard.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- tests/php/Signaling/BackendNotifierTest.php
+++ tests/php/Signaling/BackendNotifierTest.php
@@ -26,13 +26,16 @@ use OCA\Talk\AppInfo\Application;
 use OCA\Talk\Chat\CommentsManager;
 use OCA\Talk\Config;
 use OCA\Talk\Events\SignalingRoomPropertiesEvent;
+use OCA\Talk\Federation\Notifications;
 use OCA\Talk\Manager;
 use OCA\Talk\Model\Attendee;
 use OCA\Talk\Model\AttendeeMapper;
 use OCA\Talk\Model\SessionMapper;
 use OCA\Talk\Participant;
 use OCA\Talk\Room;
+use OCA\Talk\Service\MembershipService;
 use OCA\Talk\Service\ParticipantService;
+use OCA\Talk\Service\SessionService;
 use OCA\Talk\Signaling\BackendNotifier;
 use OCA\Talk\TalkSession;
 use OCA\Talk\Webinary;
@@ -40,6 +43,7 @@ use OCP\App\IAppManager;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\EventDispatcher\IEventDispatcher;
 use OCP\Http\Client\IClientService;
+use OCP\ICacheFactory;
 use OCP\IGroupManager;
 use OCP\IL10N;
 use OCP\IURLGenerator;
@@ -119,7 +123,6 @@ class BackendNotifierTest extends TestCase {
 			],
 		]));
 
-		$this->participantService = \OC::$server->get(ParticipantService::class);
 		$this->signalingManager = $this->createMock(\OCA\Talk\Signaling\Manager::class);
 		$this->signalingManager->expects($this->any())
 			->method('getSignalingServerForConversation')
@@ -127,11 +130,29 @@ class BackendNotifierTest extends TestCase {
 
 		$this->dispatcher = \OC::$server->get(IEventDispatcher::class);
 		$this->config = new Config($config, $this->secureRandom, $groupManager, $this->timeFactory, $this->dispatcher);
+
+		$dbConnection = \OC::$server->getDatabaseConnection();
+		$this->participantService = new ParticipantService(
+			$config,
+			$this->config,
+			\OC::$server->get(AttendeeMapper::class),
+			\OC::$server->get(SessionMapper::class),
+			\OC::$server->get(SessionService::class),
+			$this->secureRandom,
+			$dbConnection,
+			$this->dispatcher,
+			$this->userManager,
+			$groupManager,
+			\OC::$server->get(MembershipService::class),
+			\OC::$server->get(Notifications::class),
+			$this->timeFactory,
+			\OC::$server->get(ICacheFactory::class)
+		);
+
 		$this->recreateBackendNotifier();
 
 		$this->overwriteService(BackendNotifier::class, $this->controller);
 
-		$dbConnection = \OC::$server->getDatabaseConnection();
 		$this->manager = new Manager(
 			$dbConnection,
 			$config,
@@ -406,6 +427,12 @@ class BackendNotifierTest extends TestCase {
 		$room = $this->manager->createRoom(Room::TYPE_PUBLIC);
 		$participant = $this->participantService->joinRoom($room, $testUser, '');
 		$this->controller->clearRequests();
+
+		$this->userManager->expects($this->once())
+			->method('get')
+			->with($this->userId)
+			->willReturn($testUser);
+
 		$this->participantService->leaveRoomAsSession($room, $participant);
 
 		$this->assertMessageWasSent($room, [
