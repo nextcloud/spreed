30b466db9c879d4aae70759fa52495fa406fb303
santasonic <36194584+santasonic@users.noreply.github.com>
2023-04-08 14:22:45 +0200

    Update de.js
    line 746: Typo fixed. 
    Maunell -> Manuell
    
    Signed-off-by: santasonic <36194584+santasonic@users.noreply.github.com>

--- l10n/de.js
+++ l10n/de.js
@@ -743,7 +743,7 @@ OC.L10N.register(
     "Number of breakout rooms" : "Anzahl an Breakout-Räumen",
     "Assignment method" : "Zuweisungsmethode",
     "Automatically assign participants" : "Automatisch hinzugefügte Teilnehmer",
-    "Manually assign participants" : "Maunell hinzugefügte Teilnehmer",
+    "Manually assign participants" : "Manuell hinzugefügte Teilnehmer",
     "Allow participants to choose" : "Teilnehmern die Wahl lassen",
     "Assign participants to rooms" : "Teilnehmer Räumen zuweisen",
     "Create rooms" : "Räume erstellen",
