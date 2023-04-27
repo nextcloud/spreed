8da0b7cdf21371c18195c921d0061085aea507d7
Marco <marcoambrosini@icloud.com>
2023-04-27 11:18:20 +0100

    Add note for translators
    Signed-off-by: Marco <marcoambrosini@icloud.com>

--- src/components/MediaSettings/VideoBackgroundEditor.vue
+++ src/components/MediaSettings/VideoBackgroundEditor.vue
@@ -26,7 +26,10 @@
 			:class="{'background-editor__element--selected': selectedBackground === 'none'}"
 			@click="handleSelectBackground('none')">
 			<Cancel :size="20" />
-			{{ t('spreed', 'None') }}
+			{{
+				// TRANSLATORS: "None" refers to "No background effect applied" in videos, for context, other options are "blur" or "image"
+				t('spreed', 'None')
+			}}
 		</button>
 		<button key="blur"
 			:disabled="!blurPreviewAvailable"
