0b82954cc8bd04e2c7e4c14e6ea448df00ec70eb
Joas Schilling <coding@schilljs.com>
2023-05-11 08:46:45 +0200

    fix(participants): Fix clearing of inactive guests
    Oracle, MySQL and SQLite return ints as strings from the DB
    
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Service/ParticipantService.php
+++ lib/Service/ParticipantService.php
@@ -1010,8 +1010,9 @@ class ParticipantService {
 				continue;
 			}
 
-			if ($row['permissions'] !== Attendee::PERMISSIONS_DEFAULT
-				|| $row['participant_type'] === Participant::GUEST_MODERATOR) {
+			if ((int) $row['participant_type'] !== Participant::GUEST
+				|| ((int) $row['permissions'] !== Attendee::PERMISSIONS_DEFAULT
+					&& (int) $row['permissions'] !== Attendee::PERMISSIONS_CUSTOM)) {
 				// Keep guests with non-default permissions in case they just reconnect
 				continue;
 			}
