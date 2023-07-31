5e1bb6d3bc5f00fd6789efb772a3efee3ec7f2d5
jld3103 <jld3103yt@gmail.com>
2023-07-31 15:03:02 +0200

    Add annotations for PublicShareAuthController
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- lib/Controller/PublicShareAuthController.php
+++ lib/Controller/PublicShareAuthController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
  *
  * @copyright Copyright (c) 2018, Daniel Calviño Sánchez (danxuliu@gmail.com)
  *
+ * @author Kate Döen <kate.doeen@nextcloud.com>
+ *
  * @license GNU AGPL version 3 or any later version
  *
  * This program is free software: you can redistribute it and/or modify
@@ -52,7 +54,7 @@ class PublicShareAuthController extends OCSController {
 	}
 
 	/**
-	 * Creates a new room for requesting the password of a share.
+	 * Creates a new room for requesting the password of a share
 	 *
 	 * The new room is a public room associated with a "share:password" object
 	 * with the ID of the share token. Unlike normal rooms in which the owner is
@@ -63,10 +65,10 @@ class PublicShareAuthController extends OCSController {
 	 * The share must have "send password by Talk" enabled; an error is returned
 	 * otherwise.
 	 *
-	 * @param string $shareToken
-	 * @return DataResponse the status code is "201 Created" if a new room is
-	 *         created, "200 OK" if an existing room is returned, or "404 Not
-	 *         found" if the given share was invalid.
+	 * @param string $shareToken Token of the share
+	 * @return DataResponse<Http::STATUS_CREATED, array{token: string, name: string, displayName: string}, array{}>|DataResponse<Http::STATUS_NOT_FOUND, array<empty>, array{}>
+	 * 201: Room created successfully
+	 * 404: Share not found
 	 */
 	#[PublicPage]
 	public function createRoom(string $shareToken): DataResponse {
