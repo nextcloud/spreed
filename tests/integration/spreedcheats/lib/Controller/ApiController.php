5a68ea2a447c3b688cd7c4b8db76b2e5c9aaf144
Joas Schilling <coding@schilljs.com>
2023-08-02 16:21:07 +0200

    tests(integration): Add integration tests for the call summary bot
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/integration/spreedcheats/lib/Controller/ApiController.php
+++ tests/integration/spreedcheats/lib/Controller/ApiController.php
@@ -56,6 +56,12 @@ class ApiController extends OCSController {
 		$delete->delete('talk_attendees')->executeStatement();
 
 		$delete = $this->db->getQueryBuilder();
+		$delete->delete('talk_bots_conversation')->executeStatement();
+
+		$delete = $this->db->getQueryBuilder();
+		$delete->delete('talk_bots_server')->executeStatement();
+
+		$delete = $this->db->getQueryBuilder();
 		$delete->delete('talk_bridges')->executeStatement();
 
 		$delete = $this->db->getQueryBuilder();
@@ -71,13 +77,13 @@ class ApiController extends OCSController {
 		$delete->delete('talk_invitations')->executeStatement();
 
 		$delete = $this->db->getQueryBuilder();
-		$delete->delete('talk_rooms')->executeStatement();
-
-		$delete = $this->db->getQueryBuilder();
 		$delete->delete('talk_polls')->executeStatement();
 
 		$delete = $this->db->getQueryBuilder();
 		$delete->delete('talk_poll_votes')->executeStatement();
+
+		$delete = $this->db->getQueryBuilder();
+		$delete->delete('talk_rooms')->executeStatement();
 
 		$delete = $this->db->getQueryBuilder();
 		$delete->delete('talk_sessions')->executeStatement();
