df5a3778a41e6d2f4ee97f0b33dee7e3a6ab5b4b
Maksim Sukharev <antreesy.web@gmail.com>
2023-08-14 15:55:18 +0200

    fix scss styles to make transition work with groups
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/TransitionWrapper.vue
+++ src/components/TransitionWrapper.vue
@@ -21,6 +21,7 @@
 
 <template>
 	<TransitionGroup v-if="group"
+		class="transition-group"
 		v-bind="$attrs"
 		:name="name"
 		v-on="$listeners">
@@ -68,131 +69,205 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/variables';
 
-.fade {
+// Transition rules (inside mixins to be applied in two places)
+@mixin group-rules {
+	&-move {
+		transition: $fade-transition;
+	}
+	&-leave-active {
+		position: absolute;
+	}
+}
+
+@mixin fade-rules {
 	&-enter,
 	&-leave-to {
 		opacity: 0;
 	}
-
 	&-enter-to,
 	&-leave {
 		opacity: 1;
 	}
-
 	&-enter-active,
 	&-leave-active {
 		transition: $fade-transition;
+		transition-property: opacity;
 	}
 }
 
-.radial-reveal {
-	&-enter-active {
-		animation: radial-reveal 0.15s forwards;
-	}
-}
-
-@keyframes radial-reveal {
-	0% {
-		transform: scale(0); /* Start as a point */
+@mixin radial-reveal-rules {
+	&-enter,
+	&-leave-to {
+		transform: scale(0);
 		opacity: 0;
 	}
-	100% {
-		transform: scale(1); /* Expand to full size */
+	&-enter-to,
+	&-leave {
+		transform: scale(1);
 		opacity: 1;
+	}
+	&-enter-active,
+	&-leave-active {
+		transition: $fade-transition;
+		transition-property: transform, opacity;
+		transition-duration: 150ms;
 	}
 }
 
-.slide-up {
+@mixin slide-up-rules {
 	&-enter,
 	&-leave-to {
 		transform: translateY(-50%);
 		opacity: 0;
 	}
-
 	&-enter-to,
 	&-leave {
 		transform: translateY(0);
 		opacity: 1;
 	}
-
 	&-enter-active,
 	&-leave-active {
 		pointer-events: none;
 		transition: $fade-transition-slow;
+		transition-property: transform, opacity;
 	}
 }
 
-.slide-right {
+// TODO manipulate with transform: scaleX() instead of width
+@mixin slide-right-rules {
 	&-enter,
 	&-leave-to {
 		min-width: 0 !important;
 		max-width: 0 !important;
 	}
-
 	&-enter-to,
 	&-leave {
 		min-width: 300px;
 		max-width: 500px;
 	}
-
 	&-enter-active,
 	&-leave-active {
-		transition-duration: var(--animation-quick);
+		transition: $fade-transition;
 		transition-property: min-width, max-width;
 	}
 }
 
-.slide-down {
+@mixin slide-down-rules {
 	&-enter,
 	&-leave-to {
 		transform: translateY(50%);
 		opacity: 0;
 	}
-
 	&-enter-to,
 	&-leave {
 		transform: translateY(0);
 		opacity: 1;
 	}
-
 	&-enter-active,
 	&-leave-active {
 		transition: $fade-transition;
+		transition-property: transform, opacity;
 		/* force top container to resize during animation */
 		position: absolute !important;
 	}
 }
 
-.toast {
-	&-enter-from,
+@mixin toast-rules {
+	&-enter,
 	&-leave-to {
 		opacity: 0;
 	}
-
-	&-move,
+	&-enter-to,
+	&-leave {
+		opacity: 1;
+	}
 	&-enter-active,
 	&-leave-active {
-		transition: opacity 0.3s linear;
+		transition: $fade-transition-slow;
+		transition-property: opacity;
+		transition-timing-function: linear;
 	}
+}
+
+@mixin zoom-rules {
+	&-enter,
+	&-leave-to {
+		transform: scale(0);
+	}
+	&-enter-to,
+	&-leave {
+		transform: scale(1);
+	}
+	&-enter-active,
+	&-leave-active {
+		transition: $fade-transition;
+		transition-property: transform;
+	}
+}
+
+// Styles block for <transition> component
+.fade {
+	@include fade-rules;
+}
+
+.radial-reveal {
+	@include radial-reveal-rules;
+}
+
+.slide-up {
+	@include slide-up-rules;
+}
+
+.slide-right {
+	@include slide-right-rules;
+}
+
+.slide-down {
+	@include slide-down-rules;
+}
+
+.toast {
+	@include toast-rules;
 }
 
 .zoom {
-	&-enter-active {
-		animation: zoom-in var(--animation-quick);
-	}
+	@include zoom-rules;
+}
 
-	&-leave-active {
-		animation: zoom-in var(--animation-quick) reverse;
-		will-change: transform;
+// Styles block for for <transition-group> component
+.transition-group {
+	position: relative;
+
+	& > :deep(*) {
+		&.fade {
+			@include group-rules;
+			@include fade-rules;
+		}
+		&.radial-reveal {
+			@include group-rules;
+			@include radial-reveal-rules;
+		}
+		&.slide-up {
+			@include group-rules;
+			@include slide-up-rules;
+		}
+		&.slide-right {
+			@include group-rules;
+			@include slide-right-rules;
+		}
+		&.slide-down {
+			@include group-rules;
+			@include slide-down-rules;
+		}
+		&.toast {
+			@include group-rules;
+			@include toast-rules;
+		}
+		&.zoom {
+			@include group-rules;
+			@include zoom-rules;
+		}
 	}
 }
 
-@keyframes zoom-in {
-	0% {
-		transform: scale(0);
-	}
-	100% {
-		transform: scale(1);
-	}
-}
 </style>
