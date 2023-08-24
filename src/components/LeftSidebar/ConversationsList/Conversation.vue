e1c6b445d64146a54a6b66562bf84385bde4f310
Grigorii K. Shartsev <me@shgk.me>
2023-08-24 22:43:20 +0200

    fix(LeftSidebar): adjust conversation padding for outline
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/LeftSidebar/ConversationsList/Conversation.vue
+++ src/components/LeftSidebar/ConversationsList/Conversation.vue
@@ -22,6 +22,7 @@
 <template>
 	<NcListItem ref="listItem"
 		:title="item.displayName"
+		class="conversation-item"
 		:class="{'unread-mention-conversation': item.unreadMention}"
 		:anchor-id="`conversation_${item.token}`"
 		:actions-aria-label="t('spreed', 'Conversation actions')"
@@ -443,6 +444,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.conversation-item {
+	padding-left: 2px;
+	padding-right: 2px;
+}
+
 .subtitle {
 	font-weight: bold;
 }
