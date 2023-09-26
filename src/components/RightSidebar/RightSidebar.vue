cf50406629371ba25688570ed354f48d716ef3fd
Maksim Sukharev <antreesy.web@gmail.com>
2023-09-26 10:21:14 +0200

    wait for tabs to mount after switch conversation
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/RightSidebar/RightSidebar.vue
+++ src/components/RightSidebar/RightSidebar.vue
@@ -291,25 +291,27 @@ export default {
 
 	watch: {
 		conversation(newConversation, oldConversation) {
-			if (!this.isRenamingConversation) {
-				this.conversationName = this.conversation.displayName
-			}
+			this.$nextTick(() => {
+				if (!this.isRenamingConversation) {
+					this.conversationName = this.conversation.displayName
+				}
 
-			if (newConversation.token === oldConversation.token || this.isOneToOne) {
-				return
-			}
+				if (newConversation.token === oldConversation.token || this.isOneToOne) {
+					return
+				}
 
-			// Remain on "breakout-rooms" tab, when switching back to main room
-			if (this.breakoutRoomsConfigured && this.activeTab === 'breakout-rooms') {
-				return
-			}
+				// Remain on "breakout-rooms" tab, when switching back to main room
+				if (this.breakoutRoomsConfigured && this.activeTab === 'breakout-rooms') {
+					return
+				}
 
-			// In other case switch to other tabs
-			if (this.isInCall) {
-				this.activeTab = 'chat'
-			} else {
-				this.activeTab = 'participants'
-			}
+				// In other case switch to other tabs
+				if (this.isInCall) {
+					this.activeTab = 'chat'
+				} else {
+					this.activeTab = 'participants'
+				}
+			})
 		},
 
 		isOneToOne: {
