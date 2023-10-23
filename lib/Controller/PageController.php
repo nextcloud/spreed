f15414d4f2137672cde56b3cd91dbf711919ac0e
Joas Schilling <coding@schilljs.com>
2023-10-23 11:04:26 +0200

    fix(profile): Move rate-limiting before the action
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -148,24 +148,21 @@ class PageController extends Controller {
 		return $this->pageHandler($token, $callUser);
 	}
 
-	/**
-	 * @param string $forUser
-	 * @return ?Room
-	 */
-	protected function createPrivateRoom(string $forUser): ?Room {
-		$user = $this->userManager->get($forUser);
+	protected function createPrivateRoom(string $targetUserId): ?Room {
+		$user = $this->userManager->get($targetUserId);
 		if (!$user instanceof IUser) {
 			return null;
 		}
 
+		$l = $this->l10nFactory->get('spreed', $this->l10nFactory->getUserLanguage($user));
+
 		try {
-			$objectType = '';
-			$objectId = '';
-			$l = $this->l10nFactory->get('spreed', $this->l10nFactory->getUserLanguage($user));
-			$room = $this->roomService->createConversation(Room::TYPE_PUBLIC,
-				$l->t('Contact request'), $user, $objectType, $objectId,
+			$room = $this->roomService->createConversation(
+				Room::TYPE_PUBLIC,
+				$l->t('Contact request'),
+				$user,
 			);
-		} catch (\InvalidArgumentException $e) {
+		} catch (\InvalidArgumentException) {
 			return null;
 		}
 
@@ -191,14 +188,6 @@ class PageController extends Controller {
 		$user = $this->userSession->getUser();
 		if (!$user instanceof IUser) {
 			if ($token === '') {
-				$room = $this->createPrivateRoom($callUser);
-				if ($room === null) {
-					$response = new TemplateResponse('core', '404-profile', [], 'guest');
-					$response->throttle(['action' => 'callUser', 'callUser' => $callUser]);
-
-					return $response;
-				}
-
 				try {
 					$this->limiter->registerAnonRequest(
 						'create-anonymous-conversation',
@@ -210,10 +199,14 @@ class PageController extends Controller {
 					return new TooManyRequestsResponse();
 				}
 
-				// FIXME: add rate limiting
+				$room = $this->createPrivateRoom($callUser);
+				if ($room === null) {
+					$response = new TemplateResponse('core', '404-profile', [], TemplateResponse::RENDER_AS_GUEST);
+					$response->throttle(['action' => 'callUser', 'callUser' => $callUser]);
+					return $response;
+				}
+
 				return $this->redirectToConversation($room->getToken());
-			} else {
-				return $this->guestEnterRoom($token, $password);
 			}
 			return $this->guestEnterRoom($token, $password, $email, $accessToken);
 		}
