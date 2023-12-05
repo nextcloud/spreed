6eb0968dcbf0a42f2be7956d69e0f7d6fa2dc69c
Maksim Sukharev <antreesy.web@gmail.com>
2023-12-05 18:31:41 +0100

    fix(store): migrate getters, actions and tests
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/stores/chatExtras.js
+++ src/stores/chatExtras.js
@@ -2,6 +2,7 @@
  * @copyright Copyright (c) 2023 Maksim Sukharev <antreesy.web@gmail.com>
  *
  * @author Maksim Sukharev <antreesy.web@gmail.com>
+ * @author Marco Ambrosini <marcoambrosini@icloud.com>
  *
  * @license AGPL-3.0-or-later
  *
@@ -25,10 +26,41 @@ import Vue from 'vue'
 
 import { getUserAbsence } from '../services/participantsService.js'
 
+/**
+ * @typedef {string} Token
+ */
+
+/**
+ * @typedef {object} State
+ * @property {{[key: Token]: object}} absence - The absence status per conversation.
+ * @property {{[key: Token]: number}} messagesToBeReplied - The parent message id to reply per conversation.
+ * @property {{[key: Token]: string}} currentMessageInput -The input value per conversation.
+ */
+
+/**
+ * Store for conversation extra chat features apart from messages
+ *
+ * @param {string} id store name
+ * @param {State} options.state store state structure
+ */
 export const useChatExtrasStore = defineStore('chatExtras', {
 	state: () => ({
 		absence: {},
+		messagesToBeReplied: {},
+		currentMessageInput: {},
 	}),
+
+	getters: {
+		getMessageToBeReplied: (state) => (token) => {
+			if (state.messagesToBeReplied[token]) {
+				return state.messagesToBeReplied[token]
+			}
+		},
+
+		getCurrentMessageInput: (state) => (token) => {
+			return state.currentMessageInput[token] ?? ''
+		},
+	},
 
 	actions: {
 		/**
@@ -63,6 +95,64 @@ export const useChatExtrasStore = defineStore('chatExtras', {
 			if (this.absence[token]) {
 				Vue.delete(this.absence, token)
 			}
+		},
+
+		/**
+		 * Add a reply message id to the store
+		 *
+		 * @param {object} payload action payload
+		 * @param {string} payload.token The conversation token
+		 * @param {number} payload.id The id of message
+		 */
+		addMessageToBeReplied({ token, id }) {
+			Vue.set(this.messagesToBeReplied, token, id)
+		},
+
+		/**
+		 * Removes a reply message id from the store
+		 * (after posting message or dismissing the operation)
+		 *
+		 * @param {string} token The conversation token
+		 */
+		removeMessageToBeReplied(token) {
+			Vue.delete(this.messagesToBeReplied, token)
+		},
+
+		/**
+		 * Add a current input value to the store for a given conversation token
+		 *
+		 * @param {object} payload action payload
+		 * @param {string} payload.token The conversation token
+		 * @param {string} payload.text The string to store
+		 */
+		setCurrentMessageInput({ token, text }) {
+			// FIXME upstream: https://github.com/nextcloud-libraries/nextcloud-vue/issues/4492
+			const temp = document.createElement('textarea')
+			temp.innerHTML = text.replace(/&/gmi, '&amp;')
+			const parsedText = temp.value.replace(/&amp;/gmi, '&').replace(/&lt;/gmi, '<')
+				.replace(/&gt;/gmi, '>').replace(/&sect;/gmi, '§')
+
+			Vue.set(this.currentMessageInput, token, parsedText)
+		},
+
+		/**
+		 * Remove a current input value from the store for a given conversation token
+		 *
+		 * @param {string} token The conversation token
+		 */
+		removeCurrentMessageInput(token) {
+			Vue.delete(this.currentMessageInput, token)
+		},
+
+		/**
+		 * Clears store for a deleted conversation
+		 *
+		 * @param {string} token the token of the conversation to be deleted
+		 */
+		purgeChatExtras(token) {
+			this.removeMessageToBeReplied(token)
+			this.removeUserAbsence(token)
+			this.removeCurrentMessageInput(token)
 		},
 	},
 })
