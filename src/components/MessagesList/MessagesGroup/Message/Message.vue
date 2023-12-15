63ad366a40b5f43cc05130ba6da5fbc71a5cad76
Maksim Sukharev <antreesy.web@gmail.com>
2023-12-15 12:41:42 +0100

    fix(shares): allow to retry failed uploads
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/MessagesList/MessagesGroup/Message/Message.vue
+++ src/components/MessagesList/MessagesGroup/Message/Message.vue
@@ -695,6 +695,7 @@ export default {
 
 		sendingErrorCanRetry() {
 			return this.sendingFailure === 'timeout' || this.sendingFailure === 'other'
+				|| this.sendingFailure === 'failed-upload'
 		},
 
 		sendingErrorIconTooltip() {
@@ -810,8 +811,13 @@ export default {
 
 		handleRetry() {
 			if (this.sendingErrorCanRetry) {
-				EventBus.$emit('retry-message', this.id)
-				EventBus.$emit('focus-chat-input')
+				if (this.sendingFailure === 'failed-upload') {
+					const caption = this.renderedMessage !== this.message ? this.message : undefined
+					this.$store.dispatch('retryUploadFiles', { uploadId: this.messageObject.uploadId, caption })
+				} else {
+					EventBus.$emit('retry-message', this.id)
+					EventBus.$emit('focus-chat-input')
+				}
 			}
 		},
 
