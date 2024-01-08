f2d00d043d8b89d2fb432a9065b581118b9cfa35
Robin Appelman <robin@icewind.nl>
2024-01-08 17:28:10 +0100

    also filter by storage when getting shares in folder

--- lib/Share/RoomShareProvider.php
+++ lib/Share/RoomShareProvider.php
@@ -546,6 +546,7 @@ class RoomShareProvider implements IShareProvider {
 		}
 
 		$qb->innerJoin('s', 'filecache', 'f', $qb->expr()->eq('s.file_source', 'f.fileid'));
+		$qb->andWhere($qb->expr()->eq('f.storage', $qb->createNamedParameter($node->getMountPoint()->getNumericStorageId(), IQueryBuilder::PARAM_INT)));
 		if ($shallow) {
 			$qb->andWhere($qb->expr()->eq('f.parent', $qb->createNamedParameter($node->getId())));
 		} else {
