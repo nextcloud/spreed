1f71244d825d5a651dd7f7d3c98b8cfb5e09d1d5
Joas Schilling <coding@schilljs.com>
2024-03-25 15:15:13 +0100

    fix(openapi): Remove long int-lists now that OpenAPI understands int<min,max>
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Controller/ChatController.php
+++ lib/Controller/ChatController.php
@@ -407,8 +407,7 @@ class ChatController extends AEnvironmentAwareController {
 	 *                              (so the response is 200 instead of 304 when
 	 *                              it changes even when there are no messages)
 	 * @psalm-param non-negative-int $lastCommonReadId
-	 * @param 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30 $timeout Number of seconds to wait for new messages (30 by default, 30 at most)
-	 * @psalm-param int<0, 30> $timeout
+	 * @param int<0, 30> $timeout Number of seconds to wait for new messages (30 by default, 30 at most)
 	 * @param 0|1 $setReadMarker Automatically set the last read marker when 1,
 	 *                           if your client does this itself via chat/{token}/read set to 0
 	 * @param 0|1 $includeLastKnown Include the $lastKnownMessageId in the messages when 1 (default 0)
@@ -642,8 +641,7 @@ class ChatController extends AEnvironmentAwareController {
 	 *
 	 * @param int $messageId The focused message which should be in the "middle" of the returned context
 	 * @psalm-param non-negative-int $messageId
-	 * @param 1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100 $limit Number of chat messages to receive in both directions (50 by default, 100 at most, might return 201 messages)
-	 * @psalm-param int<1, 100> $limit
+	 * @param int<1, 100> $limit Number of chat messages to receive in both directions (50 by default, 100 at most, might return 201 messages)
 	 * @return DataResponse<Http::STATUS_OK, TalkChatMessageWithParent[], array{'X-Chat-Last-Common-Read'?: numeric-string, X-Chat-Last-Given?: numeric-string}>|DataResponse<Http::STATUS_NOT_MODIFIED, array<empty>, array<empty>>
 	 *
 	 * 200: Message context returned
@@ -1158,8 +1156,7 @@ class ChatController extends AEnvironmentAwareController {
 	/**
 	 * Get objects that are shared in the room overview
 	 *
-	 * @param 1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20 $limit Maximum number of objects
-	 * @psalm-param int<1, 20> $limit
+	 * @param int<1, 20> $limit Maximum number of objects
 	 * @return DataResponse<Http::STATUS_OK, array<string, TalkChatMessage[]>, array{}>
 	 *
 	 * 200: List of shared objects messages of each type returned
@@ -1212,8 +1209,7 @@ class ChatController extends AEnvironmentAwareController {
 	 * @param string $objectType Type of the objects
 	 * @param int $lastKnownMessageId ID of the last known message
 	 * @psalm-param non-negative-int $lastKnownMessageId
-	 * @param 1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|102|103|104|105|106|107|108|109|110|111|112|113|114|115|116|117|118|119|120|121|122|123|124|125|126|127|128|129|130|131|132|133|134|135|136|137|138|139|140|141|142|143|144|145|146|147|148|149|150|151|152|153|154|155|156|157|158|159|160|161|162|163|164|165|166|167|168|169|170|171|172|173|174|175|176|177|178|179|180|181|182|183|184|185|186|187|188|189|190|191|192|193|194|195|196|197|198|199|200 $limit Maximum number of objects
-	 * @psalm-param int<1, 200> $limit
+	 * @param int<1, 200> $limit Maximum number of objects
 	 * @return DataResponse<Http::STATUS_OK, TalkChatMessage[], array{X-Chat-Last-Given?: numeric-string}>
 	 *
 	 * 200: List of shared objects messages returned
