0b086dc7da574e85b1e417aa555cfeaa049c4bad
Joas Schilling <coding@schilljs.com>
2024-03-25 10:04:34 +0100

    fix(flow): Don't use the current participant when posting the flow message
    Otherwise the own user would not trigger a notification
    
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Flow/Operation.php
+++ lib/Flow/Operation.php
@@ -121,8 +121,8 @@ class Operation implements IOperation {
 
 				$this->chatManager->sendMessage(
 					$room,
-					$participant,
-					'bots',
+					null,
+					Attendee::ACTOR_BOTS,
 					$participant->getAttendee()->getActorId(),
 					$this->prepareMention($mode, $participant) . $message,
 					new \DateTime(),
