a4f36078b4a00dd6efef4090fca5cb7f7d214946
Grigorii K. Shartsev <me@shgk.me>
2024-04-04 11:25:26 +0200

    feat(desktop): add screen sharing support
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/utils/webrtc/simplewebrtc/getscreenmedia.js
+++ src/utils/webrtc/simplewebrtc/getscreenmedia.js
@@ -34,13 +34,52 @@ export default function(mode, constraints, cb) {
 	const callback = hasConstraints ? cb : constraints
 	let error
 
-	if (typeof window === 'undefined' || window.location.protocol === 'http:') {
+	if (!IS_DESKTOP && (typeof window === 'undefined' || window.location.protocol === 'http:')) {
 		error = new Error('NavigatorUserMediaError')
 		error.name = 'HTTPS_REQUIRED'
 		return callback(error)
 	}
 
-	if (navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
+	if (IS_DESKTOP) {
+		return window.OCA.Talk.Desktop.getDesktopMediaSource()
+			.then(({ sourceId }) => {
+				if (!sourceId) {
+					// User canceled
+					const error = new Error('NavigatorUserMediaError')
+					error.name = 'PERMISSION_DENIED'
+					throw error
+				}
+
+				// Special case for sharing all the screens with desktop audio in Electron
+				// In this case, it must have exactly these constraints
+				// "entire-desktop:0:0" is a custom sourceId for this specific case
+				const constraints = (sourceId === 'entire-desktop:0:0')
+					? {
+						audio: {
+							mandatory: {
+								chromeMediaSource: 'desktop',
+							},
+						},
+						video: {
+							mandatory: {
+								chromeMediaSource: 'desktop',
+							},
+						},
+					}
+					: {
+						audio: false,
+						video: {
+							mandatory: {
+								chromeMediaSource: 'desktop',
+								chromeMediaSourceId: sourceId,
+							},
+						},
+					}
+				return navigator.mediaDevices.getUserMedia(constraints)
+			})
+			.then((stream) => callback(null, stream))
+			.catch((error) => callback(error))
+	} else if (navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
 		navigator.mediaDevices.getDisplayMedia({
 			video: true,
 			// Disable default audio optimizations, as they are meant to be used
