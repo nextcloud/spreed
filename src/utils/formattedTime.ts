e8711a42cf77a9d0188ec219f562a192e718e51c
DorraJaouad <dorra.jaoued7@gmail.com>
2024-04-17 21:57:14 +0200

    feat(LobbySettings): add timezone and relative time
    Signed-off-by: DorraJaouad <dorra.jaoued7@gmail.com>

--- src/utils/formattedTime.ts
+++ src/utils/formattedTime.ts
@@ -20,6 +20,33 @@ function formattedTime(time: number, condensed: boolean = false): string {
 	].filter(num => !!num).join(condensed ? ':' : ' : ')
 }
 
+/**
+ * Calculates the future relative time string given the time (ms)
+ *
+ * @param time the time in ms
+ */
+function futureRelativeTime(time: number): string {
+	const diff = time - Date.now()
+	const hours = Math.floor(diff / (60 * 60 * 1000))
+	const minutes = Math.floor((diff - hours * 60 * 60 * 1000) / (60 * 1000))
+	if (hours >= 1) {
+		if (minutes === 0) {
+			// TRANSLATORS: hint for the time when the meeting starts (only hours)
+			return n('spreed', 'In %n hour', 'In %n hours', hours)
+		} else {
+			// TRANSLATORS: hint for the time when the meeting starts (hours and minutes)
+			return t('spreed', 'In {hours} and {minutes}', {
+				hours: n('spreed', '%n hour', '%n hours', hours),
+				minutes: n('spreed', '%n minute ', '%n minutes', minutes),
+			})
+		}
+	} else {
+		// TRANSLATORS: hint for the time when the meeting starts (only minutes)
+		return n('spreed', 'In %n minute', 'In %n minutes', minutes)
+	}
+}
+
 export {
 	formattedTime,
+	futureRelativeTime,
 }
