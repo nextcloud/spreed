c4e6a9a5f55f73326901c13cf8e7f07e7f20c56a
DorraJaouad <dorra.jaoued7@gmail.com>
2024-04-25 13:35:42 +0200

    feat(MediaSettings): make apply settings button sticky
    Signed-off-by: DorraJaouad <dorra.jaoued7@gmail.com>

--- src/components/MediaSettings/MediaSettings.vue
+++ src/components/MediaSettings/MediaSettings.vue
@@ -674,6 +674,7 @@ export default {
 <style lang="scss" scoped>
 .media-settings {
 	padding: calc(var(--default-grid-baseline) * 5);
+	padding-bottom: 0;
 
 	&__title {
 		text-align: center;
@@ -723,9 +724,14 @@ export default {
 
 	&__call-buttons {
 		display: flex;
+		z-index: 1;
 		align-items: center;
 		justify-content: center;
 		gap: var(--default-grid-baseline);
+		position: sticky;
+		bottom: 0;
+		background-color: var(--color-main-background);
+		padding: 10px 0 20px;
 	}
 }
 
