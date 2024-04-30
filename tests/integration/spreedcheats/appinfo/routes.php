1f8ab2aede63b9ea3eb763ef5feb7b565970a10e
Joas Schilling <coding@schilljs.com>
2024-04-30 08:38:51 +0200

    feat(CI): Add age functionality to cheats app
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/integration/spreedcheats/appinfo/routes.php
+++ tests/integration/spreedcheats/appinfo/routes.php
@@ -9,5 +9,6 @@ declare(strict_types=1);
 return [
 	'ocs' => [
 		['name' => 'Api#resetSpreed', 'url' => '/', 'verb' => 'DELETE'],
+		['name' => 'Api#ageChat', 'url' => '/age', 'verb' => 'POST'],
 	],
 ];
