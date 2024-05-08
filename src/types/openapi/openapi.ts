bf240be8cfd5a2f1ce5035e9266b0315ede3f9b5
Joas Schilling <coding@schilljs.com>
2024-05-08 12:57:52 +0200

    feat(capabilities): Expose which capabilities should be considered local vs federated
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/types/openapi/openapi.ts
+++ src/types/openapi/openapi.ts
@@ -445,6 +445,7 @@ export type components = {
     };
     Capabilities: {
       features: string[];
+      "features-local": string[];
       config: {
         attachments: {
           allowed: boolean;
@@ -490,6 +491,9 @@ export type components = {
           "session-ping-limit": number;
           "hello-v2-token-key"?: string;
         };
+      };
+      "config-local": {
+        [key: string]: string[];
       };
       version: string;
     };
