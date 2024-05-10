342eca0e018f46f282a505e7cdbeb5ffb626d4b6
Joas Schilling <coding@schilljs.com>
2024-05-10 16:06:37 +0200

    fix(polls): Remove actor info from system message
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Chat/Parser/SystemMessage.php
+++ lib/Chat/Parser/SystemMessage.php
@@ -602,6 +602,8 @@ class SystemMessage implements IEventListener {
 			$parsedParameters['poll']['id'] = (string) $parsedParameters['poll']['id'];
 			$parsedMessage = $this->l->t('Someone voted on the poll {poll}');
 			unset($parsedParameters['actor']);
+
+			$chatMessage->setActor(Attendee::ACTOR_GUESTS, Attendee::ACTOR_ID_SYSTEM, '');
 		} else {
 			throw new \OutOfBoundsException('Unknown subject');
 		}
