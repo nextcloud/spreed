be09463e2b47ee7ef10b62f691067d49ab7ba565
Maksim Sukharev <antreesy.web@gmail.com>
2024-05-15 14:23:39 +0200

    fix(directive): remove unused imports
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/CallView/Grid/Grid.vue
+++ src/components/CallView/Grid/Grid.vue
@@ -144,7 +144,6 @@ import { loadState } from '@nextcloud/initial-state'
 import { generateFilePath } from '@nextcloud/router'
 
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
-import Tooltip from '@nextcloud/vue/dist/Directives/Tooltip.js'
 
 import TransitionWrapper from '../../UIShared/TransitionWrapper.vue'
 import EmptyCallView from '../shared/EmptyCallView.vue'
@@ -170,10 +169,6 @@ export default {
 		ChevronLeft,
 		ChevronUp,
 		ChevronDown,
-	},
-
-	directives: {
-		Tooltip,
 	},
 
 	props: {
