6d34719715b6a8e7cff7fa8a025790f48b9b097b
Maksim Sukharev <antreesy.web@gmail.com>
2024-06-18 18:13:27 +0200

    fix: pass returned promise instead of function
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/NewConversationDialog/NewConversationDialog.vue
+++ src/components/NewConversationDialog/NewConversationDialog.vue
@@ -314,7 +314,7 @@ export default {
 				}
 
 				if (this.isAvatarEdited) {
-					promises.push(this.$refs.setupPage.$refs.conversationAvatar.saveAvatar)
+					promises.push(this.$refs.setupPage.$refs.conversationAvatar.saveAvatar())
 				}
 
 				if (this.newConversation.description) {
