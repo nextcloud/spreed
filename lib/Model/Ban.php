9b6b6a9ebc13514fc058bb25e3d38e94358fee72
skalidindi53 <s.teja2004@gmail.com>
2024-06-21 13:10:09 -0500

    feat: Added Ban.php and BanMapper.php
    Signed-off-by: skalidindi53 <s.teja2004@gmail.com>

---
+++ lib/Model/Ban.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Talk\Model;
+
+use OCP\AppFramework\Db\Entity;
+
+/**
+ * @method void setActorId(string $actorId)
+ * @method string getActorId()
+ * @method void setActorType(string $actorType)
+ * @method string getActorType()
+ * @method void setRoomId(int $roomId)
+ * @method int getRoomId()
+ * @method void setBannedByActorId(string $bannedByActorId)
+ * @method string getBannedByActorId()
+ * @method void setBannedByActorType(string $bannedByActorType)
+ * @method string getBannedByActorType()
+ * @method void setBannedAt(\DateTime $bannedAt)
+ * @method \DateTime getBannedAt()
+ * @method void setReason(string $reason)
+ * @method string getReason()
+ */
+class Ban extends Entity implements \JsonSerializable {
+	protected $actorId = '';
+	protected string $actorType = '';
+	protected int $roomId = 0;
+	protected string $bannedByActorId = '';
+	protected string $bannedByActorType = '';
+	protected ?\DateTime $bannedAt = null;
+	protected ?string $reason = null;
+
+	public function __construct() {
+		$this->addType('actorId', 'string');
+		$this->addType('actorType', 'string');
+		$this->addType('roomId', 'int');
+		$this->addType('bannedByActorId', 'string');
+		$this->addType('bannedByActorType', 'string');
+		$this->addType('bannedAt', 'datetime');
+		$this->addType('reason', 'string');
+	}
+
+	public function jsonSerialize(): array {
+		return [
+			'actorId' => $this->getActorId(),
+			'actorType' => $this->getActorType(),
+			'roomId' => $this->getRoomId(),
+			'bannedByActorId' => $this->getBannedByActorId(),
+			'bannedByActorType' => $this->getBannedByActorType(),
+			'bannedAt' => $this->getBannedAt() ? $this->getBannedAt()->getTimestamp() : null,
+			'reason' => $this->getReason(),
+		];
+	}
+}
---
+++ lib/Model/BanMapper.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Talk\Model;
+
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Db\QBMapper;
+use OCP\AppFramework\Db\TTransactional;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
+
+/**
+ * @method Ban mapRowToEntity(array $row)
+ * @method Ban findEntity(IQueryBuilder $query)
+ * @method Ban[] findEntities(IQueryBuilder $query)
+ * @template-extends QBMapper<Ban>
+ */
+class BanMapper extends QBMapper {
+	use TTransactional;
+
+	public function __construct(IDBConnection $db) {
+		parent::__construct($db, 'talk_bans', Ban::class);
+	}
+
+	/**
+	 * @throws DoesNotExistException
+	 */
+	public function findForActorAndRoom(string $actorId, string $actorType, int $roomId): Ban {
+		$query = $this->db->getQueryBuilder();
+		$query->select('*')
+			->from($this->getTableName())
+			->where($query->expr()->eq('actor_id', $query->createNamedParameter($actorId, IQueryBuilder::PARAM_STR)))
+			->andWhere($query->expr()->eq('actor_type', $query->createNamedParameter($actorType, IQueryBuilder::PARAM_STR)))
+			->andWhere($query->expr()->eq('room_id', $query->createNamedParameter($roomId, IQueryBuilder::PARAM_INT)));
+
+		return $this->findEntity($query);
+	}
+
+	public function deleteBanForActorAndRoom(string $actorId, string $actorType, int $roomId): void {
+		$query = $this->db->getQueryBuilder();
+		$query->delete($this->getTableName())
+			->where($query->expr()->eq('actor_id', $query->createNamedParameter($actorId, IQueryBuilder::PARAM_STR)))
+			->andWhere($query->expr()->eq('actor_type', $query->createNamedParameter($actorType, IQueryBuilder::PARAM_STR)))
+			->andWhere($query->expr()->eq('room_id', $query->createNamedParameter($roomId, IQueryBuilder::PARAM_INT)));
+
+		$query->executeStatement();
+	}
+}
