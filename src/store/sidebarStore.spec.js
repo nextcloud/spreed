b3328512e71ac978c24386528169d49b1d500c78
Maksim Sukharev <antreesy.web@gmail.com>
2024-06-26 14:57:58 +0200

    fix(RightSidebar): remove unused actions/getters/computed
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/store/sidebarStore.spec.js
+++ src/store/sidebarStore.spec.js
@@ -26,7 +26,6 @@ describe('sidebarStore', () => {
 
 	test('defaults are off', () => {
 		expect(store.getters.getSidebarStatus).toBe(true)
-		expect(store.getters.isRenamingConversation).toBe(false)
 	})
 
 	test('toggle sidebar', () => {
@@ -37,39 +36,5 @@ describe('sidebarStore', () => {
 		store.dispatch('showSidebar')
 
 		expect(store.getters.getSidebarStatus).toBe(true)
-	})
-
-	test('toggling renaming mode and remembers sidebar hidden state', () => {
-		store.dispatch('hideSidebar')
-		store.dispatch('isRenamingConversation', true)
-
-		expect(store.getters.getSidebarStatus).toBe(false)
-		expect(store.getters.isRenamingConversation).toBe(true)
-
-		store.dispatch('showSidebar')
-
-		expect(store.getters.getSidebarStatus).toBe(true)
-
-		store.dispatch('isRenamingConversation', false)
-
-		expect(store.getters.getSidebarStatus).toBe(false)
-		expect(store.getters.isRenamingConversation).toBe(false)
-	})
-
-	test('toggling renaming mode and remembers sidebar shown state', () => {
-		store.dispatch('showSidebar')
-		store.dispatch('isRenamingConversation', true)
-
-		expect(store.getters.getSidebarStatus).toBe(true)
-		expect(store.getters.isRenamingConversation).toBe(true)
-
-		store.dispatch('hideSidebar')
-
-		expect(store.getters.getSidebarStatus).toBe(false)
-
-		store.dispatch('isRenamingConversation', false)
-
-		expect(store.getters.getSidebarStatus).toBe(true)
-		expect(store.getters.isRenamingConversation).toBe(false)
 	})
 })
