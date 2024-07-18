a857d77b185693b79daf5d69403be58402276513
Daniel Calviño Sánchez <danxuliu@gmail.com>
2024-07-18 05:39:44 +0200

    fix: Adjust session id length in internal signaling messages
    The length of the "sender" and "recipient" columns in the
    "talk_internalsignaling" table was 255 (defined in
    Version11000Date20201209142525.php), but the maximum session id length
    is 512 (defined in Version10000Date20201015134000.php).
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

---
+++ lib/Migration/Version20000Date20240718031959.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Talk\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Adjust session id length in internal signaling messages to maximum Nextcloud
+ * session id length.
+ */
+class Version20000Date20240718031959 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if ($schema->hasTable('talk_internalsignaling')) {
+			$table = $schema->getTable('talk_internalsignaling');
+
+			$modified = false;
+
+			$sender = $table->getColumn('sender');
+			if ($sender->getLength() !== 512) {
+				$sender->setLength(512);
+				$modified = true;
+			}
+
+			$recipient = $table->getColumn('recipient');
+			if ($recipient->getLength() !== 512) {
+				$recipient->setLength(512);
+				$modified = true;
+			}
+
+			if ($modified) {
+				return $schema;
+			}
+		}
+
+		return null;
+	}
+}
