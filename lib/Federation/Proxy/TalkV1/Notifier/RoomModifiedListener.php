b4957ee128bbc47a364359b4dcbc97807b307870
Daniel Calviño Sánchez <danxuliu@gmail.com>
2024-07-19 01:45:12 +0200

    fix: Propagate lobby state to federated servers
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/Federation/Proxy/TalkV1/Notifier/RoomModifiedListener.php
+++ lib/Federation/Proxy/TalkV1/Notifier/RoomModifiedListener.php
@@ -24,13 +24,17 @@ declare(strict_types=1);
 namespace OCA\Talk\Federation\Proxy\TalkV1\Notifier;
 
 use OCA\Talk\Events\AAttendeeRemovedEvent;
+use OCA\Talk\Events\ALobbyModifiedEvent;
 use OCA\Talk\Events\ARoomModifiedEvent;
+use OCA\Talk\Events\LobbyModifiedEvent;
 use OCA\Talk\Events\RoomModifiedEvent;
 use OCA\Talk\Federation\BackendNotifier;
 use OCA\Talk\Model\Attendee;
+use OCA\Talk\Participant;
 use OCA\Talk\Service\ParticipantService;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
+use OCP\Federation\ICloudId;
 use OCP\Federation\ICloudIdManager;
 
 /**
@@ -45,13 +49,15 @@ class RoomModifiedListener implements IEventListener {
 	}
 
 	public function handle(Event $event): void {
-		if (!$event instanceof RoomModifiedEvent) {
+		if (!$event instanceof LobbyModifiedEvent
+				&& !$event instanceof RoomModifiedEvent) {
 			return;
 		}
 
 		if (!in_array($event->getProperty(), [
 			ARoomModifiedEvent::PROPERTY_AVATAR,
 			ARoomModifiedEvent::PROPERTY_DESCRIPTION,
+			ARoomModifiedEvent::PROPERTY_LOBBY,
 			ARoomModifiedEvent::PROPERTY_NAME,
 			ARoomModifiedEvent::PROPERTY_READ_ONLY,
 			ARoomModifiedEvent::PROPERTY_TYPE,
@@ -63,19 +69,41 @@ class RoomModifiedListener implements IEventListener {
 		foreach ($participants as $participant) {
 			$cloudId = $this->cloudIdManager->resolveCloudId($participant->getAttendee()->getActorId());
 
-			$success = $this->backendNotifier->sendRoomModifiedUpdate(
-				$cloudId->getRemote(),
-				$participant->getAttendee()->getId(),
-				$participant->getAttendee()->getAccessToken(),
-				$event->getRoom()->getToken(),
-				$event->getProperty(),
-				$event->getNewValue(),
-				$event->getOldValue(),
-			);
+			if ($event instanceof ALobbyModifiedEvent) {
+				$success = $this->notifyLobbyModified($cloudId, $participant, $event);
+			} else {
+				$success = $this->notifyRoomModified($cloudId, $participant, $event);
+			}
 
 			if ($success === null) {
 				$this->participantService->removeAttendee($event->getRoom(), $participant, AAttendeeRemovedEvent::REASON_LEFT);
 			}
 		}
+	}
+
+	private function notifyLobbyModified(ICloudId $cloudId, Participant $participant, ALobbyModifiedEvent $event) {
+		return $this->backendNotifier->sendRoomModifiedLobbyUpdate(
+			$cloudId->getRemote(),
+			$participant->getAttendee()->getId(),
+			$participant->getAttendee()->getAccessToken(),
+			$event->getRoom()->getToken(),
+			$event->getProperty(),
+			$event->getNewValue(),
+			$event->getOldValue(),
+			$event->getLobbyTimer(),
+			$event->isTimerReached(),
+		);
+	}
+
+	private function notifyRoomModified(ICloudId $cloudId, Participant $participant, ARoomModifiedEvent $event) {
+		return $this->backendNotifier->sendRoomModifiedUpdate(
+			$cloudId->getRemote(),
+			$participant->getAttendee()->getId(),
+			$participant->getAttendee()->getAccessToken(),
+			$event->getRoom()->getToken(),
+			$event->getProperty(),
+			$event->getNewValue(),
+			$event->getOldValue(),
+		);
 	}
 }
