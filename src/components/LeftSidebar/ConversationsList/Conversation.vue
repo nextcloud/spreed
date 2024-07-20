8e5a9d1d68f3fdc65fcc4397e56f565e9e4dddc6
Maksim Sukharev <antreesy.web@gmail.com>
2024-07-20 18:27:34 +0200

    style: make type icon in LeftSidebar blend with background
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/LeftSidebar/ConversationsList/Conversation.vue
+++ src/components/LeftSidebar/ConversationsList/Conversation.vue
@@ -9,6 +9,7 @@
 		:name="item.displayName"
 		:title="item.displayName"
 		:data-nav-id="`conversation_${item.token}`"
+		:class="['conversation', { 'conversation--active': isActive }]"
 		:actions-aria-label="t('spreed', 'Conversation actions')"
 		:to="to"
 		:bold="!!item.unreadMessages"
@@ -325,6 +326,28 @@ export default {
 <style lang="scss" scoped>
 .critical > :deep(.action-button) {
 	color: var(--color-error);
+}
+
+.conversation {
+	// Overwrite ConversationIcon styles to blend a type icon with NcListItem
+	& :deep(.list-item:hover .conversation-icon__type) {
+		background-color: var(--color-background-hover);
+		border-color: var(--color-background-hover);
+	}
+
+	&--active {
+		&:deep(.list-item .conversation-icon__type) {
+			color: var(--color-primary-element-text);
+			background-color: var(--color-primary-element);
+			border-color: var(--color-primary-element);
+		}
+
+		&:deep(.list-item:hover .conversation-icon__type) {
+			color: var(--color-primary-element-text);
+			background-color: var(--color-primary-element-hover);
+			border-color: var(--color-primary-element-hover);
+		}
+	}
 }
 
 :deep(.dialog) {
