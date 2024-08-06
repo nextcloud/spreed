144d64da20e3027210b26d032d380eaf9802c46e
Maksim Sukharev <antreesy.web@gmail.com>
2024-08-06 15:44:00 +0200

    fix(ContactSelectionBubble): migrate to script setup
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/UIShared/ContactSelectionBubble.vue
+++ src/components/UIShared/ContactSelectionBubble.vue
@@ -3,10 +3,32 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
+<script setup>
+import { computed } from 'vue'
+
+import { t } from '@nextcloud/l10n'
+
+import NcChip from '@nextcloud/vue/dist/Components/NcChip.js'
+
+import AvatarWrapper from '../AvatarWrapper/AvatarWrapper.vue'
+
+import { AVATAR } from '../../constants.js'
+
+const props = defineProps({
+	participant: {
+		type: Object,
+		required: true,
+	},
+})
+const emit = defineEmits(['update'])
+
+const removeLabel = computed(() => t('spreed', 'Remove participant {name}', { name: props.participant.label }))
+</script>
+
 <template>
 	<NcChip :text="participant.label"
 		:aria-label-close="removeLabel"
-		@close="$emit('update', participant)">
+		@close="emit('update', participant)">
 		<template #icon>
 			<AvatarWrapper :id="participant.id"
 				token="new"
@@ -18,48 +40,6 @@
 		</template>
 	</NcChip>
 </template>
-
-<script>
-import { t } from '@nextcloud/l10n'
-
-import NcChip from '@nextcloud/vue/dist/Components/NcChip.js'
-
-import AvatarWrapper from '../AvatarWrapper/AvatarWrapper.vue'
-
-import { AVATAR } from '../../constants.js'
-
-export default {
-	name: 'ContactSelectionBubble',
-
-	components: {
-		AvatarWrapper,
-		NcChip,
-	},
-
-	props: {
-		participant: {
-			type: Object,
-			required: true,
-		},
-	},
-
-	emits: ['update'],
-
-	setup() {
-		return { AVATAR }
-	},
-
-	computed: {
-		removeLabel() {
-			return t('spreed', 'Remove participant {name}', { name: this.participant.label })
-		},
-	},
-
-	methods: {
-		t,
-	},
-}
-</script>
 
 <style lang="scss" scoped>
 </style>
