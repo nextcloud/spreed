b65eaa499a55e861605e073c599c324af00d3561
Maksim Sukharev <antreesy.web@gmail.com>
2024-08-06 15:36:02 +0200

    fix(ContactSelectionBubble): migrate to NcChip
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/UIShared/ContactSelectionBubble.vue
+++ src/components/UIShared/ContactSelectionBubble.vue
@@ -4,34 +4,25 @@
 -->
 
 <template>
-	<div class="contact-selection-bubble">
-		<AvatarWrapper :id="participant.id"
-			token="new"
-			class="contact-selection-bubble__avatar"
-			:name="participant.label"
-			:source="participant.source"
-			:size="AVATAR.SIZE.EXTRA_SMALL"
-			disable-menu
-			disable-tooltip />
-		<span class="contact-selection-bubble__username">
-			{{ displayName }}
-		</span>
-		<NcButton type="tertiary-no-background"
-			:aria-label="removeLabel"
-			@click="$emit('update', participant)">
-			<template #icon>
-				<Close :size="16" />
-			</template>
-		</NcButton>
-	</div>
+	<NcChip :text="participant.label"
+		:aria-label-close="removeLabel"
+		@close="$emit('update', participant)">
+		<template #icon>
+			<AvatarWrapper :id="participant.id"
+				token="new"
+				:name="participant.label"
+				:source="participant.source"
+				:size="AVATAR.SIZE.EXTRA_SMALL"
+				disable-menu
+				disable-tooltip />
+		</template>
+	</NcChip>
 </template>
 
 <script>
-import Close from 'vue-material-design-icons/Close.vue'
-
 import { t } from '@nextcloud/l10n'
 
-import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
+import NcChip from '@nextcloud/vue/dist/Components/NcChip.js'
 
 import AvatarWrapper from '../AvatarWrapper/AvatarWrapper.vue'
 
@@ -42,8 +33,7 @@ export default {
 
 	components: {
 		AvatarWrapper,
-		NcButton,
-		Close,
+		NcChip,
 	},
 
 	props: {
@@ -60,14 +50,8 @@ export default {
 	},
 
 	computed: {
-		displayName() {
-			// Used to be the group of characters before the first space in the name.
-			// But it causes weird scenarios in formal companies or when people have titles.
-			return this.participant.label
-		},
-
 		removeLabel() {
-			return t('spreed', 'Remove participant {name}', { name: this.displayName })
+			return t('spreed', 'Remove participant {name}', { name: this.participant.label })
 		},
 	},
 
@@ -78,28 +62,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-// Component Variables
-$bubble-height: 24px;
-
-.contact-selection-bubble {
-	display: flex;
-	align-items: center;
-	margin: 4px;
-	background-color: var(--color-primary-element-light);
-	border-radius: $bubble-height;
-	height: $bubble-height;
-	overflow: hidden;
-	&__avatar {
-		margin-right: 4px;
-	}
-	// Limit the length of the username
-	&__username {
-		max-width: 190px;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-}
-
 </style>
