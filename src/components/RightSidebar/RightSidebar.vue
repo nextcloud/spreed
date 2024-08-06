12cfd26d6b7b806291a0730ab2c06d6529d82013
Grigorii K. Shartsev <me@shgk.me>
2024-08-06 14:39:21 +0200

    fix(RightSidebar): discard unread notifications on conversation change
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/RightSidebar/RightSidebar.vue
+++ src/components/RightSidebar/RightSidebar.vue
@@ -390,6 +390,9 @@ export default {
 			if (this.$refs.participantsTab) {
 				this.$refs.participantsTab.$el.scrollTop = 0
 			}
+
+			// Discard notification if the conversation changes or closed
+			this.notifyUnreadMessages(null)
 		},
 
 		isModeratorOrUser(newValue) {
@@ -412,9 +415,6 @@ export default {
 
 	beforeDestroy() {
 		unsubscribe('spreed:select-active-sidebar-tab', this.handleUpdateActive)
-
-		// Discard current chat notifications
-		this.notifyUnreadMessages(null)
 	},
 
 	methods: {
