16642b29dbb736ca338d722d66b72428628934c6
Joas Schilling <coding@schilljs.com>
2024-08-28 12:49:49 +0200

    fix(setupchecks): Add setup checks for current checks
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ tests/stubs/oca_settings_setupchecks_checkserverresponsetrait.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OCA\Settings\SetupChecks {
+	use Generator;
+
+	trait CheckServerResponseTrait {
+		protected function serverConfigHelp(): string {
+		}
+	}
+
+	trait CheckServerResponseTrait2 {
+		protected function serverConfigHelp(): string {
+		}
+
+		protected function runHEAD(string $url, bool $ignoreSSL = true, bool $httpErrors = true): Generator {
+		}
+
+		protected function runRequest(string $method, string $url, array $options = []): Generator {
+		}
+	}
+}
