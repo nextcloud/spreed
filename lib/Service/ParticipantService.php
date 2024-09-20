82f9d13a6bcb670b5e60141c856f77a0844aec2c
Joas Schilling <coding@schilljs.com>
2024-09-20 11:06:06 +0200

    fix(archive): Don't send notifications for default calls and at-all mentions
    Signed-off-by: Joas Schilling <coding@schilljs.com>
    
    [skip ci]

--- lib/Service/ParticipantService.php
+++ lib/Service/ParticipantService.php
@@ -1753,6 +1753,7 @@ class ParticipantService {
 			->where($query->expr()->eq('a.room_id', $query->createNamedParameter($room->getId(), IQueryBuilder::PARAM_INT)))
 			->andWhere($query->expr()->eq('a.actor_type', $query->createNamedParameter(Attendee::ACTOR_USERS)))
 			->andWhere($query->expr()->eq('a.notification_calls', $query->createNamedParameter(Participant::NOTIFY_CALLS_ON)))
+			->andWhere($query->expr()->eq('a.archived', $query->createNamedParameter(false, IQueryBuilder::PARAM_BOOL)))
 			->andWhere($query->expr()->isNull('s.in_call'));
 
 		if ($room->getLobbyState() !== Webinary::LOBBY_NONE) {
