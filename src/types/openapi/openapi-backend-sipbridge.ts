dd840f7b7591efdf322c49f977e7d28b82fe6f9f
Joas Schilling <coding@schilljs.com>
2024-10-08 14:24:02 +0200

    feat(max-duration): Add a capability
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/types/openapi/openapi-backend-sipbridge.ts
+++ src/types/openapi/openapi-backend-sipbridge.ts
@@ -144,6 +144,8 @@ export type components = {
                     "sip-dialout-enabled": boolean;
                     "can-enable-sip": boolean;
                     "start-without-media": boolean;
+                    /** Format: int64 */
+                    "max-duration": number;
                 };
                 chat: {
                     /** Format: int64 */
