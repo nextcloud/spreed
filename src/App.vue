98996eee0e9599ed50a4742e89bf6590dbd7535f
Maksim Sukharev <antreesy.web@gmail.com>
2024-10-09 17:55:36 +0200

    feat: show conversation settings, if URL hash is specified
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -475,6 +475,8 @@ export default {
 
 			if (to.hash === '#direct-call') {
 				emit('talk:media-settings:show')
+			} else if (to.hash === '#settings') {
+				emit('show-conversation-settings', { token: this.token })
 			}
 		})
 
@@ -502,6 +504,8 @@ export default {
 
 		if (this.$route.hash === '#direct-call') {
 			emit('talk:media-settings:show')
+		} else if (this.$route.hash === '#settings') {
+			emit('show-conversation-settings', { token: this.token })
 		}
 
 		subscribe('notifications:action:execute', this.interceptNotificationActions)
