2c32f4e5a6d5577eaf2c7a4e0ac53b461cea5fc6
Maksim Sukharev <antreesy.web@gmail.com>
2024-10-11 10:24:34 +0200

    fix: move version comparison to utils
    - post-review refactoring
    
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/CallView/CallView.vue
+++ src/components/CallView/CallView.vue
@@ -4,7 +4,7 @@
 -->
 
 <template>
-	<div id="call-container" :style="callContainerStyle">
+	<div id="call-container" :class="callContainerClass">
 		<ViewerOverlayCallView v-if="isViewerOverlay"
 			:token="token"
 			:model="promotedParticipantModel"
@@ -158,11 +158,12 @@ import { getTalkConfig } from '../../services/CapabilitiesManager.ts'
 import { EventBus } from '../../services/EventBus.js'
 import { useCallViewStore } from '../../stores/callView.js'
 import { useSettingsStore } from '../../stores/settings.js'
+import { satisfyVersion } from '../../utils/satisfyVersion.ts'
 import { localMediaModel, localCallParticipantModel, callParticipantCollection } from '../../utils/webrtc/index.js'
 import RemoteVideoBlocker from '../../utils/webrtc/RemoteVideoBlocker.js'
 
-const serverVersion = loadState('core', 'config', {}).versionstring ?? '29.0.0'
-const serverSupportsBackgroundBlurred = '29.0.4'.localeCompare(serverVersion) < 1
+const serverVersion = loadState('core', 'config', {}).version ?? '29.0.0.0'
+const serverSupportsBackgroundBlurred = satisfyVersion(serverVersion, '29.0.4.0')
 
 export default {
 	name: 'CallView',
@@ -210,9 +211,8 @@ export default {
 			localMediaModel.disableVideo()
 		}
 
-		const isBackgroundBlurred = ref(serverSupportsBackgroundBlurred
-			? null
-			: BrowserStorage.getItem('background-blurred') !== 'false')
+		// Fallback ref for versions before v29.0.4
+		const isBackgroundBlurred = ref(BrowserStorage.getItem('background-blurred') !== 'false')
 
 		return {
 			localMediaModel,
@@ -394,14 +394,12 @@ export default {
 		/**
 		 * Fallback style for versions before v29.0.4
 		 */
-		callContainerStyle() {
+		callContainerClass() {
 			if (serverSupportsBackgroundBlurred) {
 				return
 			}
 
-			return {
-				'backdrop-filter': this.isBackgroundBlurred ? 'blur(25px)' : 'none',
-			}
+			return this.isBackgroundBlurred ? 'call-container__blurred' : 'call-container__non-blurred'
 		}
 	},
 
@@ -813,6 +811,13 @@ export default {
 	// Default value has changed since v29.0.4: 'blur(25px)' => 'none'
 	backdrop-filter: var(--filter-background-blur);
 	--grid-gap: calc(var(--default-grid-baseline) * 2);
+
+	&.call-container__blurred {
+		backdrop-filter: blur(25px);
+	}
+	&.call-container__non-blurred {
+		backdrop-filter: none;
+	}
 }
 
 #videos {
