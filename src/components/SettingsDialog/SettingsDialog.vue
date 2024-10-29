fd3a6b323ca45bedacdc32202c7fc791d514c952
Grigorii K. Shartsev <me@shgk.me>
2024-10-29 11:31:01 +0100

    fix(settings): show correct hotkey with CMD on macOS
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/SettingsDialog/SettingsDialog.vue
+++ src/components/SettingsDialog/SettingsDialog.vue
@@ -144,7 +144,7 @@
 					</dd>
 				</div>
 				<div>
-					<dt><kbd>Ctrl</kbd> + <kbd>↑</kbd></dt>
+					<dt><kbd>{{ CmdOrCtrl }}</kbd> + <kbd>↑</kbd></dt>
 					<dd class="shortcut-description">
 						{{ t('spreed', 'Edit your last message') }}
 					</dd>
@@ -156,7 +156,7 @@
 					</dd>
 				</div>
 				<div>
-					<dt><kbd>Ctrl</kbd> + <kbd>F</kbd></dt>
+					<dt><kbd>{{ CmdOrCtrl }}</kbd> + <kbd>F</kbd></dt>
 					<dd class="shortcut-description">
 						{{ t('spreed', 'Search') }}
 					</dd>
@@ -219,6 +219,7 @@ import { useCustomSettings } from '../../services/SettingsAPI.ts'
 import { setUserConfig } from '../../services/settingsService.js'
 import { useSettingsStore } from '../../stores/settings.js'
 import { useSoundsStore } from '../../stores/sounds.js'
+import { isMac } from '../../utils/browserCheck.ts'
 import { satisfyVersion } from '../../utils/satisfyVersion.ts'
 
 const serverVersion = loadState('core', 'config', {}).version ?? '29.0.0.0'
@@ -246,8 +247,10 @@ export default {
 		const soundsStore = useSoundsStore()
 		const { customSettingsSections } = useCustomSettings()
 		const isBackgroundBlurred = ref(isBackgroundBlurredState)
+		const CmdOrCtrl = isMac ? 'Cmd' : 'Ctrl'
 
 		return {
+			CmdOrCtrl,
 			settingsStore,
 			soundsStore,
 			supportTypingStatus,
