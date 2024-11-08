2925b92011932841814374a4521c623d71b21658
Maksim Sukharev <antreesy.web@gmail.com>
2024-11-08 12:51:54 +0100

    fix: overwrite EventBus.off methods to clear once events
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/services/EventBus.ts
+++ src/services/EventBus.ts
@@ -11,8 +11,13 @@ type GenericEventHandler = Handler<Events[keyof Events]> | WildcardHandler<Event
 type ExtendedEmitter = Emitter<Events> & {
 	once<Key extends keyof Events>(type: Key, handler: Handler<Events[Key]>): void
 	once(type: '*', handler: WildcardHandler<Events>): void
+	cancelOnce<Key extends keyof Events>(type: Key, handler?: Handler<Events[Key]>): void
+	cancelOnce(type: '*', handler: WildcardHandler<Events>): void
+	_onceHandlersMap: Map<keyof Events | '*', Map<GenericEventHandler, GenericEventHandler>>
 }
+
 export const EventBus: ExtendedEmitter = mitt() as ExtendedEmitter
+EventBus._onceHandlersMap = new Map()
 
 /**
  * Register a one-time event handler for the given type
@@ -30,4 +35,47 @@ EventBus.once = function<Key extends keyof Events>(type: Key, handler: GenericEv
 		this.off(type, fn)
 	}
 	this.on(type, fn)
+
+	// Store reference to the original handler to be able to remove it later
+	if (!EventBus._onceHandlersMap.has(type)) {
+		EventBus._onceHandlersMap.set(type, new Map())
+	}
+	EventBus._onceHandlersMap.get(type)!.set(handler, fn)
+}
+
+/**
+ * OVERRIDING OF ORIGINAL MITT FUNCTION
+ * Remove an event handler for the given type.
+ * If `handler` is omitted, all handlers of the given type are removed.
+ * @param type Type of event to unregister `handler` from (`'*'` to remove a wildcard handler)
+ * @param [handler] Handler function to remove
+ */
+EventBus.off = function<Key extends keyof Events>(type: Key, handler?: GenericEventHandler) {
+	const handlers: Array<GenericEventHandler> | undefined = EventBus.all!.get(type)
+	const onceHandlers: Map<GenericEventHandler, GenericEventHandler> | undefined = EventBus._onceHandlersMap!.get(type)
+
+	if (handlers) {
+		if (handler) {
+			handlers.splice(handlers.indexOf(handler) >>> 0, 1)
+		} else {
+			EventBus.all!.set(type, [])
+		}
+	}
+
+	if (handlers && onceHandlers) {
+		if (handler) {
+			for (const [_handler, fn] of onceHandlers) {
+				if (handler === fn) {
+					// Event was received once and removed by original code, clearing reference only
+					onceHandlers.delete(_handler)
+				} else if (_handler === handler) {
+					// Event was not received yet, removing the fn and reference
+					handlers.splice(handlers.indexOf(fn) >>> 0, 1)
+					onceHandlers.delete(_handler)
+				}
+			}
+		} else {
+			EventBus._onceHandlersMap.set(type, new Map())
+		}
+	}
 }
