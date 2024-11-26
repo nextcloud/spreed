974f60a99c6d283d763eda73805c5fc65f995164
Maksim Sukharev <antreesy.web@gmail.com>
2024-11-26 12:07:14 +0100

    fix(LobbyStatus): migrate component to TS
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/RightSidebar/LobbyStatus.vue
+++ src/components/RightSidebar/LobbyStatus.vue
@@ -3,59 +3,61 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
-<template>
-	<NcButton type="success" @click="disableLobby">
-		<template #icon>
-			<LockOpen />
-		</template>
-		{{ t('spreed', 'Disable lobby' ) }}
-	</NcButton>
-</template>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<script>
-import LockOpen from 'vue-material-design-icons/LockOpen.vue'
+import IconLockOpen from 'vue-material-design-icons/LockOpen.vue'
 
 import { showError, showSuccess } from '@nextcloud/dialogs'
 import { t } from '@nextcloud/l10n'
 
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 
-export default {
-	name: 'LobbyStatus',
-	components: {
-		NcButton,
-		LockOpen,
-	},
-	props: {
-		token: {
-			type: String,
-			required: true,
-		},
-	},
+import { useStore } from '../../composables/useStore.js'
 
-	data() {
-		return {
-			isLobbyStateLoading: false,
-		}
-	},
+const props = defineProps<{
+	token: string,
+}>()
 
-	methods: {
-		t,
-		async disableLobby() {
-			this.isLobbyStateLoading = true
-			try {
-				await this.$store.dispatch('toggleLobby', {
-					token: this.token,
-					enableLobby: false,
-				})
-				showSuccess(t('spreed', 'You opened the conversation to everyone'))
-			} catch (e) {
-				console.error('Error occurred when opening the conversation to everyone', e)
-				showError(t('spreed', 'Error occurred when opening the conversation to everyone'))
-			}
-			this.isLobbyStateLoading = false
-		},
-	},
+const store = useStore()
+const isLobbyStateLoading = ref(false)
+
+/**
+ * Disable lobby for this conversation
+ */
+async function disableLobby() {
+	isLobbyStateLoading.value = true
+	try {
+		await store.dispatch('toggleLobby', {
+			token: props.token,
+			enableLobby: false,
+		})
+		showSuccess(t('spreed', 'You opened the conversation to everyone'))
+	} catch (e) {
+		console.error('Error occurred when opening the conversation to everyone', e)
+		showError(t('spreed', 'Error occurred when opening the conversation to everyone'))
+	} finally {
+		isLobbyStateLoading.value = false
+	}
 }
-
 </script>
+
+<template>
+	<div class="lobby-status">
+		<NcButton type="success" @click="disableLobby">
+			<template #icon>
+				<IconLockOpen :size="20" />
+			</template>
+			{{ t('spreed', 'Disable lobby' ) }}
+		</NcButton>
+	</div>
+</template>
+
+<style lang="scss">
+.lobby-status {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: var(--default-grid-baseline);
+}
+</style>
