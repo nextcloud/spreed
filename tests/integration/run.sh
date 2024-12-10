eb2e3e7ffaf5bb14b573af1f375f0c47a6703637
Joas Schilling <coding@schilljs.com>
2024-12-10 08:46:55 +0100

    test: Allow 2 different Nextcloud code paths for testing federation
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -43,29 +43,49 @@ echo -e "Running on process ID: \033[1;35m$PHPPID2\033[0m"
 # Output filtered federated php server logs
 tail -f phpserver_fed.log | grep --line-buffered -v -E ":[0-9]+ Accepted$" | grep --line-buffered -v -E ":[0-9]+ Closing$" &
 
-MAIN_SERVER_CONFIG_DIR=${ROOT_DIR}/config
-MAIN_SERVER_DATA_DIR=$(${ROOT_DIR}/occ config:system:get datadirectory)
-MAIN_SERVER_APPS_PATHS=$(${ROOT_DIR}/occ config:system:get apps_paths --output json)
-REAL_FEDERATED_SERVER_CONFIG_DIR="$MAIN_SERVER_DATA_DIR/tests-talk-real-federated-server/config"
-REAL_FEDERATED_SERVER_DATA_DIR="$MAIN_SERVER_DATA_DIR/tests-talk-real-federated-server/data"
-DESTROY_REAL_FEDERATED_SERVER=false
+occ_host() {
+	NEXTCLOUD_CONFIG_DIR=${MAIN_SERVER_CONFIG_DIR} ${ROOT_DIR}/occ "$@"
+}
 
-if [ ! -d "$REAL_FEDERATED_SERVER_CONFIG_DIR" ] || NEXTCLOUD_CONFIG_DIR="$REAL_FEDERATED_SERVER_CONFIG_DIR" ${ROOT_DIR}/occ status | grep "installed: false"; then
-	DESTROY_REAL_FEDERATED_SERVER=true
-	if [ $CI ]; then
-		DESTROY_REAL_FEDERATED_SERVER=false
-	fi
+occ_remote() {
+	NEXTCLOUD_CONFIG_DIR=${REAL_FEDERATED_SERVER_CONFIG_DIR} ${REMOTE_ROOT_DIR}/occ "$@"
+}
+
+MAIN_SERVER_CONFIG_DIR=${ROOT_DIR}/config
+MAIN_SERVER_DATA_DIR=$(occ_host config:system:get datadirectory)
+MAIN_SERVER_APPS_PATHS=$(occ_host config:system:get apps_paths --output json)
+
+if [ $REMOTE_ROOT_DIR ]; then
+	REAL_FEDERATED_SERVER_CONFIG_DIR="$REMOTE_ROOT_DIR/config"
+	REAL_FEDERATED_SERVER_DATA_DIR="$REMOTE_ROOT_DIR/data"
+	DESTROY_REAL_FEDERATED_SERVER=false
+else
 	echo ''
-	echo -e "\033[0;31mReal federated server not installed in $REAL_FEDERATED_SERVER_CONFIG_DIR\033[0m"
-	echo -e "\033[0;33mPerforming basic SQLite installation with data directory in $REAL_FEDERATED_SERVER_DATA_DIR\033[0m"
-	mkdir --parents "$REAL_FEDERATED_SERVER_CONFIG_DIR" "$REAL_FEDERATED_SERVER_DATA_DIR"
-	NEXTCLOUD_CONFIG_DIR="$REAL_FEDERATED_SERVER_CONFIG_DIR" ${ROOT_DIR}/occ maintenance:install --admin-pass=admin --data-dir="$REAL_FEDERATED_SERVER_DATA_DIR"
-	echo ''
-	if [ $MAIN_SERVER_APPS_PATHS ]; then
-		echo -e "\033[0;33mCopying custom apps_paths\033[0m"
-		echo "{\"system\":{\"apps_paths\":$MAIN_SERVER_APPS_PATHS}}" > "$REAL_FEDERATED_SERVER_CONFIG_DIR/apps_paths.json"
-		NEXTCLOUD_CONFIG_DIR="$REAL_FEDERATED_SERVER_CONFIG_DIR" ${ROOT_DIR}/occ config:import < "$REAL_FEDERATED_SERVER_CONFIG_DIR/apps_paths.json"
+	echo -e "\033[0;36m#\033[0m"
+	echo -e "\033[0;36m# Setting up real federated server\033[0m"
+	echo -e "\033[0;36m#\033[0m"
+	REMOTE_ROOT_DIR=$ROOT_DIR
+	REAL_FEDERATED_SERVER_CONFIG_DIR="$MAIN_SERVER_DATA_DIR/tests-talk-real-federated-server/config"
+	REAL_FEDERATED_SERVER_DATA_DIR="$MAIN_SERVER_DATA_DIR/tests-talk-real-federated-server/data"
+	DESTROY_REAL_FEDERATED_SERVER=false
+
+	if [ ! -d "$REAL_FEDERATED_SERVER_CONFIG_DIR" ] || occ_remote status | grep "installed: false"; then
+		DESTROY_REAL_FEDERATED_SERVER=true
+		if [ $CI ]; then
+			DESTROY_REAL_FEDERATED_SERVER=false
+		fi
 		echo ''
+		echo -e "\033[0;31mReal federated server not installed in $REAL_FEDERATED_SERVER_CONFIG_DIR\033[0m"
+		echo -e "\033[0;33mPerforming basic SQLite installation with data directory in $REAL_FEDERATED_SERVER_DATA_DIR\033[0m"
+		mkdir --parents "$REAL_FEDERATED_SERVER_CONFIG_DIR" "$REAL_FEDERATED_SERVER_DATA_DIR"
+		occ_remote maintenance:install --admin-pass=admin --data-dir="$REAL_FEDERATED_SERVER_DATA_DIR"
+		echo ''
+		if [ $MAIN_SERVER_APPS_PATHS ]; then
+			echo -e "\033[0;33mCopying custom apps_paths\033[0m"
+			echo "{\"system\":{\"apps_paths\":$MAIN_SERVER_APPS_PATHS}}" > "$REAL_FEDERATED_SERVER_CONFIG_DIR/apps_paths.json"
+			occ_remote config:import < "$REAL_FEDERATED_SERVER_CONFIG_DIR/apps_paths.json"
+			echo ''
+		fi
 	fi
 fi
 
@@ -73,7 +93,7 @@ PORT_FED_REAL=8280
 export PORT_FED_REAL
 
 echo "" > phpserver_fed_real.log
-PHP_CLI_SERVER_WORKERS=3 NEXTCLOUD_CONFIG_DIR="$REAL_FEDERATED_SERVER_CONFIG_DIR" php -S localhost:${PORT_FED_REAL} -t ${ROOT_DIR} &> phpserver_fed_real.log &
+PHP_CLI_SERVER_WORKERS=3 NEXTCLOUD_CONFIG_DIR="$REAL_FEDERATED_SERVER_CONFIG_DIR" php -S localhost:${PORT_FED_REAL} -t ${REMOTE_ROOT_DIR} &> phpserver_fed_real.log &
 PHPPID3=$!
 echo -e "Running on process ID: \033[1;35m$PHPPID3\033[0m"
 
@@ -83,30 +103,29 @@ tail -f phpserver_fed_real.log | grep --line-buffered -v -E ":[0-9]+ Accepted$" 
 # Kill all sub-processes in case of ctrl+c
 trap 'pkill -P $PHPPID1; pkill -P $PHPPID2; pkill -P $PHPPID3; pkill -P $PROCESS_ID; wait $PHPPID1; wait $PHPPID2; wait $PHPPID3;' INT TERM
 
-NEXTCLOUD_ROOT_DIR=${ROOT_DIR}
-export NEXTCLOUD_ROOT_DIR
+export NEXTCLOUD_HOST_ROOT_DIR=${ROOT_DIR}
+export NEXTCLOUD_HOST_CONFIG_DIR=${MAIN_SERVER_CONFIG_DIR}
+export NEXTCLOUD_REMOTE_ROOT_DIR=${REMOTE_ROOT_DIR}
+export NEXTCLOUD_REMOTE_CONFIG_DIR=${REAL_FEDERATED_SERVER_CONFIG_DIR}
 export TEST_SERVER_URL="http://localhost:8080/"
 export TEST_LOCAL_REMOTE_URL="http://localhost:8180/"
 export TEST_REMOTE_URL="http://localhost:8280/"
-export MAIN_SERVER_CONFIG_DIR
-export REAL_FEDERATED_SERVER_CONFIG_DIR
 
 export NEXTCLOUD_CONFIG_DIR="$MAIN_SERVER_CONFIG_DIR"
-MAIN_OVERWRITE_CLI_URL=$(${ROOT_DIR}/occ config:system:get overwrite.cli.url)
-MAIN_SKELETON_DIR=$(${ROOT_DIR}/occ config:system:get skeletondirectory)
-${ROOT_DIR}/occ config:system:set overwrite.cli.url --value "http://localhost:8080/"
+MAIN_OVERWRITE_CLI_URL=$(occ_host config:system:get overwrite.cli.url)
+MAIN_SKELETON_DIR=$(occ_host config:system:get skeletondirectory)
+occ_host config:system:set overwrite.cli.url --value "http://localhost:8080/"
 if [[ "$MAIN_SKELETON_DIR" != "" ]]; then
 	echo "Resetting custom skeletondirectory so that tests pass"
-	${ROOT_DIR}/occ config:system:delete skeletondirectory
+	${NEXTCLOUD_HOST_OCC} config:system:delete skeletondirectory
 fi
 
-export NEXTCLOUD_CONFIG_DIR="$REAL_FEDERATED_SERVER_CONFIG_DIR"
-REAL_FEDERATED_OVERWRITE_CLI_URL=$(${ROOT_DIR}/occ config:system:get overwrite.cli.url)
-REAL_FEDERATED_SKELETON_DIR=$(${ROOT_DIR}/occ config:system:get skeletondirectory)
-${ROOT_DIR}/occ config:system:set overwrite.cli.url --value "$TEST_REMOTE_URL"
+REAL_FEDERATED_OVERWRITE_CLI_URL=$(occ_remote config:system:get overwrite.cli.url)
+REAL_FEDERATED_SKELETON_DIR=$(occ_remote config:system:get skeletondirectory)
+occ_remote config:system:set overwrite.cli.url --value "$TEST_REMOTE_URL"
 if [[ "$REAL_FEDERATED_SKELETON_DIR" != "" ]]; then
 	echo "Resetting custom skeletondirectory so that tests pass"
-	${ROOT_DIR}/occ config:system:delete skeletondirectory
+	occ_remote config:system:delete skeletondirectory
 fi
 
 echo ''
@@ -114,49 +133,50 @@ echo -e "\033[0;36m#\033[0m"
 echo -e "\033[0;36m# Setting up apps\033[0m"
 echo -e "\033[0;36m#\033[0m"
 cp -R ./spreedcheats ../../../spreedcheats
-${ROOT_DIR}/occ app:getpath spreedcheats
+occ_host app:getpath spreedcheats
+
+REMOTE_SPREED_DIR=$(occ_remote app:getpath spreed)
+if [ ! -d ${REMOTE_ROOT_DIR}/apps/spreedcheats ]; then
+	cp -R ${REMOTE_SPREED_DIR}/tests/integration/spreedcheats ${REMOTE_ROOT_DIR}/apps/spreedcheats
+fi
 
 # Add apps to the parent directory of "spreed" (unless they are
 # already there or in "apps").
-${ROOT_DIR}/occ app:getpath notifications || (cd ../../../ && git clone --depth 1 --branch ${NOTIFICATIONS_BRANCH} https://github.com/nextcloud/notifications)
-${ROOT_DIR}/occ app:getpath guests || (cd ../../../ && git clone --depth 1 --branch ${GUESTS_BRANCH} https://github.com/nextcloud/guests)
-${ROOT_DIR}/occ app:getpath circles || (cd ../../../ && git clone --depth 1 --branch ${CIRCLES_BRANCH} https://github.com/nextcloud/circles)
-${ROOT_DIR}/occ app:getpath call_summary_bot || (cd ../../../ && git clone --depth 1 --branch ${CSB_BRANCH} https://github.com/nextcloud/call_summary_bot)
+occ_host app:getpath notifications || (cd ../../../ && git clone --depth 1 --branch ${NOTIFICATIONS_BRANCH} https://github.com/nextcloud/notifications)
+occ_host app:getpath guests || (cd ../../../ && git clone --depth 1 --branch ${GUESTS_BRANCH} https://github.com/nextcloud/guests)
+occ_host app:getpath circles || (cd ../../../ && git clone --depth 1 --branch ${CIRCLES_BRANCH} https://github.com/nextcloud/circles)
+occ_host app:getpath call_summary_bot || (cd ../../../ && git clone --depth 1 --branch ${CSB_BRANCH} https://github.com/nextcloud/call_summary_bot)
 
-for CONFIG_DIR in $MAIN_SERVER_CONFIG_DIR $REAL_FEDERATED_SERVER_CONFIG_DIR; do
-	export NEXTCLOUD_CONFIG_DIR="$CONFIG_DIR"
+for OCC in occ_host occ_remote; do
+	${OCC} app:enable spreed || exit 1
+	${OCC} app:enable --force spreedcheats || exit 1
+	${OCC} app:enable --force notifications || exit 1
+	${OCC} app:enable --force guests || exit 1
+	${OCC} app:enable --force circles || exit 1
+	${OCC} app:enable --force call_summary_bot || exit 1
 
-	${ROOT_DIR}/occ app:enable spreed || exit 1
-	${ROOT_DIR}/occ app:enable --force spreedcheats || exit 1
-	${ROOT_DIR}/occ app:enable --force notifications || exit 1
-	${ROOT_DIR}/occ app:enable --force guests || exit 1
-	${ROOT_DIR}/occ app:enable --force circles || exit 1
-	${ROOT_DIR}/occ app:enable --force call_summary_bot || exit 1
-
-	${ROOT_DIR}/occ app:list | grep spreed
-	${ROOT_DIR}/occ app:list | grep notifications
-	${ROOT_DIR}/occ app:list | grep guests
-	${ROOT_DIR}/occ app:list | grep circles
-	${ROOT_DIR}/occ app:list | grep call_summary_bot
+	${OCC} app:list | grep spreed
+	${OCC} app:list | grep notifications
+	${OCC} app:list | grep guests
+	${OCC} app:list | grep circles
+	${OCC} app:list | grep call_summary_bot
 done
 
 echo ''
 echo -e "\033[0;36m#\033[0m"
 echo -e "\033[0;36m# Optimizing configuration\033[0m"
 echo -e "\033[0;36m#\033[0m"
-for CONFIG_DIR in $MAIN_SERVER_CONFIG_DIR $REAL_FEDERATED_SERVER_CONFIG_DIR; do
-	export NEXTCLOUD_CONFIG_DIR="$CONFIG_DIR"
-
+for OCC in occ_host occ_remote; do
 	# Disable bruteforce protection because the integration tests do trigger them
-	${ROOT_DIR}/occ config:system:set auth.bruteforce.protection.enabled --value false --type bool
+	${OCC} config:system:set auth.bruteforce.protection.enabled --value false --type bool
 	# Disable rate limit protection because the integration tests do trigger them
-	${ROOT_DIR}/occ config:system:set ratelimit.protection.enabled --value false --type bool
+	${OCC} config:system:set ratelimit.protection.enabled --value false --type bool
 	# Allow local remote urls otherwise we can not share
-	${ROOT_DIR}/occ config:system:set allow_local_remote_servers --value true --type bool
+	${OCC} config:system:set allow_local_remote_servers --value true --type bool
 	# Enable debug mode as it is required to enable developer commands
-	${ROOT_DIR}/occ config:system:set debug --value true --type bool
+	${OCC} config:system:set debug --value true --type bool
 	# Use faster password hashing
-	${ROOT_DIR}/occ config:system:set hashing_default_password --value=true --type=bool
+	${OCC} config:system:set hashing_default_password --value=true --type=bool
 done
 
 # Restore default config dir to local server in case it is used from the tests
@@ -199,10 +219,10 @@ echo -e "\033[0;36m#\033[0m"
 
 # Main server
 export NEXTCLOUD_CONFIG_DIR="$MAIN_SERVER_CONFIG_DIR"
-${ROOT_DIR}/occ app:disable spreedcheats
-${ROOT_DIR}/occ config:system:set overwrite.cli.url --value "$MAIN_OVERWRITE_CLI_URL"
+occ_host app:disable spreedcheats
+occ_host config:system:set overwrite.cli.url --value "$MAIN_OVERWRITE_CLI_URL"
 if [[ "$MAIN_SKELETON_DIR" != "" ]]; then
-	${ROOT_DIR}/occ config:system:set skeletondirectory --value "$MAIN_SKELETON_DIR"
+	occ_host config:system:set skeletondirectory --value "$MAIN_SKELETON_DIR"
 fi
 
 # Real federated server
@@ -210,14 +230,15 @@ if $DESTROY_REAL_FEDERATED_SERVER; then
 	rm -rf "$REAL_FEDERATED_SERVER_CONFIG_DIR" "$REAL_FEDERATED_SERVER_DATA_DIR"
 else
 	export NEXTCLOUD_CONFIG_DIR="$REAL_FEDERATED_SERVER_CONFIG_DIR"
-	${ROOT_DIR}/occ app:disable spreedcheats
-	${ROOT_DIR}/occ config:system:set overwrite.cli.url --value "$REAL_FEDERATED_OVERWRITE_CLI_URL"
+	occ_remote app:disable spreedcheats
+	occ_remote config:system:set overwrite.cli.url --value "$REAL_FEDERATED_OVERWRITE_CLI_URL"
 	if [[ "$REAL_FEDERATED_SKELETON_DIR" != "" ]]; then
-		${ROOT_DIR}/occ config:system:set skeletondirectory --value "$REAL_FEDERATED_SKELETON_DIR"
+		occ_remote config:system:set skeletondirectory --value "$REAL_FEDERATED_SKELETON_DIR"
 	fi
 fi
 
 rm -rf ../../../spreedcheats
+rm -rf ${REMOTE_ROOT_DIR}/apps/spreedcheats
 
 wait $PHPPID1
 wait $PHPPID2
