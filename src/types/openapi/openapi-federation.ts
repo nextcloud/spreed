003b2f90c95db6d096eb307087fdf8a7eefd8799
Maksim Sukharev <antreesy.web@gmail.com>
2024-12-13 15:22:14 +0100

    chore: run openapi
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/types/openapi/openapi-federation.ts
+++ src/types/openapi/openapi-federation.ts
@@ -194,6 +194,8 @@ export type components = {
                 conversations: {
                     "can-create": boolean;
                     "force-passwords": boolean;
+                    /** @enum {string} */
+                    "list-style": "two-lines" | "compact";
                 };
                 federation: {
                     enabled: boolean;
