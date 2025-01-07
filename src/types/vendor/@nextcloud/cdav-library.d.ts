67ff013f6fe29ca9f84dc41ad3856cc9f306199c
Maksim Sukharev <antreesy.web@gmail.com>
2025-01-07 12:50:45 +0100

    chore(deps): add @nextcloud/cdav-library v1.5.2
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

---
+++ src/types/vendor/@nextcloud/cdav-library.d.ts
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare module '@nextcloud/cdav-library'
+
+declare class DavClient {
+
+	constructor(options: { rootUrl: string });
+	connect(options: { enableCalDAV: boolean }): Promise<void>;
+
+	currentUserPrincipal: DavPrincipal
+	calendarHomes: DavCalendarHome[]
+
+}
