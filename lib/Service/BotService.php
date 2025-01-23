5c7ee03038093080de45679ff8f8faab7e773479
Joas Schilling <coding@schilljs.com>
2025-01-23 23:56:25 +0100

    fix(bots): Add a constant for the protocol
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Service/BotService.php
+++ lib/Service/BotService.php
@@ -406,7 +406,7 @@ class BotService {
 			throw new \InvalidArgumentException('The provided secret is too short (min. 40 chars, max. 128 chars)');
 		}
 
-		if (!$url || strlen($url) > 4000 || !(str_starts_with($url, 'http://') || str_starts_with($url, 'https://') || str_starts_with($url, 'nextcloudapp://'))) {
+		if (!$url || strlen($url) > 4000 || !(str_starts_with($url, 'http://') || str_starts_with($url, 'https://') || str_starts_with($url, Bot::URL_APP_PREFIX))) {
 			throw new \InvalidArgumentException('The provided URL is not a valid URL');
 		}
 
