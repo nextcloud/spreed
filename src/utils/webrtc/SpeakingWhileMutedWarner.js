879ace49c77552e408dbdf1e377e8ff4aa874448
Maksim Sukharev <antreesy.web@gmail.com>
2025-01-27 17:34:47 +0100

    fix(SpeakingWhileMutedWarner): add timeout to hide message in case of constant noise (7 seconds)
    - also hide message on destroy
    
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/utils/webrtc/SpeakingWhileMutedWarner.js
+++ src/utils/webrtc/SpeakingWhileMutedWarner.js
@@ -3,36 +3,38 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { showWarning, TOAST_PERMANENT_TIMEOUT } from '@nextcloud/dialogs'
+import { showWarning, TOAST_PERMANENT_TIMEOUT, TOAST_DEFAULT_TIMEOUT } from '@nextcloud/dialogs'
 import { t } from '@nextcloud/l10n'
 
 /**
-	* Helper to warn the user if they are talking while muted.
-	*
-	* The WebRTC helper emits events when it detects that the user is speaking
-	* while muted; this helper shows a warning to the user based on those
-	* events.
-	*
-	* The warning is not immediately shown, though; the WebRTC helper flags
-	* even short sounds as "speaking" (provided they are strong enough), so to
-	* prevent unnecesary warnings the user has to speak for a few seconds for
-	* the warning to be shown. On the other hand, the warning is hidden as soon
-	* as the WebRTC helper detects that the speaking has stopped; in this case
-	* there is no delay, as the helper itself has a delay before emitting the
-	* event.
-	*
-	* The way of warning the user changes depending on whether Talk is visible
-	* or not; if it is visible the warning is shown in the Talk UI, but if it
-	* is not it is shown using a browser notification, which will be visible
-	* to the user even if the browser window is not in the foreground (provided
-	* the user granted the permissions to receive notifications from the site).
-	*
-	* @param {object} LocalMediaModel the model that emits "speakingWhileMuted"
-	* events.
-	* "setSpeakingWhileMutedNotification" method.
-	*/
+ * Helper to warn the user if they are talking while muted.
+ *
+ * The WebRTC helper emits events when it detects that the user is speaking
+ * while muted; this helper shows a warning to the user based on those
+ * events.
+ *
+ * The warning is not immediately shown, though; the WebRTC helper flags
+ * even short sounds as "speaking" (provided they are strong enough), so to
+ * prevent unnecessary warnings the user has to speak for a few seconds for
+ * the warning to be shown. On the other hand, the warning is hidden as soon
+ * as the WebRTC helper detects that the speaking has stopped; in this case
+ * there is no delay, as the helper itself has a delay before emitting the
+ * event.
+ *
+ * The way of warning the user changes depending on whether Talk is visible
+ * or not; if it is visible the warning is shown in the Talk UI, but if it
+ * is not it is shown using a browser notification, which will be visible
+ * to the user even if the browser window is not in the foreground (provided
+ * the user granted the permissions to receive notifications from the site).
+ *
+ * @param {object} LocalMediaModel the model that emits "speakingWhileMuted"
+ * events.
+ */
 export default function SpeakingWhileMutedWarner(LocalMediaModel) {
 	this._model = LocalMediaModel
+	this._startedSpeakingTimeout = undefined
+	this._startedShowWarningTimeout = undefined
+
 	this._toast = null
 
 	this._handleSpeakingWhileMutedChangeBound = this._handleSpeakingWhileMutedChange.bind(this)
@@ -42,6 +44,7 @@ export default function SpeakingWhileMutedWarner(LocalMediaModel) {
 SpeakingWhileMutedWarner.prototype = {
 
 	destroy() {
+		this._hideWarning()
 		this._model.off('change:speakingWhileMuted', this._handleSpeakingWhileMutedChangeBound)
 	},
 
@@ -86,6 +89,12 @@ SpeakingWhileMutedWarner.prototype = {
 				}
 			}.bind(this))
 		}
+
+		this._startedShowWarningTimeout = setTimeout(function() {
+			delete this._startedShowWarningTimeout
+
+			this._hideWarning()
+		}.bind(this), TOAST_DEFAULT_TIMEOUT)
 	},
 
 	_showNotification(message) {
@@ -158,6 +167,11 @@ SpeakingWhileMutedWarner.prototype = {
 			this._browserNotification.close()
 
 			this._browserNotification = null
+		}
+
+		if (this._startedShowWarningTimeout) {
+			clearTimeout(this._startedShowWarningTimeout)
+			delete this._startedShowWarningTimeout
 		}
 	},
 
