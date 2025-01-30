6119ece1a87750ae8727eb97607945bc634d83d2
Maksim Sukharev <antreesy.web@gmail.com>
2025-01-30 13:53:00 +0100

    fix: migrate certificateService to .ts
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/services/certificateService.ts
+++ src/services/certificateService.ts
@@ -5,46 +5,47 @@
 import axios from '@nextcloud/axios'
 import { generateOcsUrl } from '@nextcloud/router'
 
+import type { certificateExpirationParams, certificateExpirationResponse } from '../types/index.ts'
+
 /**
  * Retrieves the certificate expiration of the specified host
  *
- * @param {string} host The host to check the certificate
- * @return {number|null} Null if unable to retrieve the certificates expiration, otherwise the expiration in days (negative if already expired)
+ * @param host The host to check the certificate
  */
-const getCertificateExpiration = async (host) => {
-	try {
-		const response = await axios.get(generateOcsUrl('apps/spreed/api/v1/certificate/expiration'), {
-			params: {
-				host,
-			},
-		})
-
-		return response.data.ocs.data.expiration_in_days
-	} catch (error) {
-		console.error(error)
-	}
-
-	return null
+const getCertificateExpiration = async (host: certificateExpirationParams['host']): certificateExpirationResponse => {
+	return axios.get(generateOcsUrl('apps/spreed/api/v1/certificate/expiration'), {
+		params: {
+			host,
+		} as certificateExpirationParams,
+	})
 }
 
 /**
  * Checks if the certificate of a host is valid
  *
- * @param {string} host The host to check the certificate
+ * @param host The host to check the certificate
  * @return {boolean} true if the certificate is valid, false otherwise
  */
-const isCertificateValid = async (host) => {
-	const expiration = await getCertificateExpiration(host)
+const isCertificateValid = async (host: certificateExpirationParams['host']): Promise<boolean> => {
+	try {
+		const response = await getCertificateExpiration(host)
 
-	if (expiration == null) {
-		console.warn('Unable to check certificate of', host)
-	} else if (expiration < 0) {
-		console.error('Certificate of', host, 'expired')
-	} else {
-		console.info('Certificate of', host, 'is valid for', expiration, 'days')
+		// Null if unable to retrieve the certificates expiration, otherwise the expiration in days (negative if already expired)
+		const expiration = response.data.ocs.data.expiration_in_days
+
+		if (expiration == null) {
+			console.warn('Unable to check certificate of', host)
+			return false
+		} else if (expiration < 0) {
+			console.error('Certificate of', host, 'expired')
+		} else {
+			console.info('Certificate of', host, 'is valid for', expiration, 'days')
+		}
+		return expiration > 0
+	} catch (error) {
+		console.error(error)
+		return false
 	}
-
-	return expiration > 0
 }
 
 export {
