c3724d6a82ac7691e8a9f34bc3d840eee04d8827
Maksim Sukharev <antreesy.web@gmail.com>
2025-02-12 12:46:16 +0100

    fix: add script and workflow to generate core types
    - only from selected shipped apps, as defined by script
    
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

---
+++ src/types/generate-core-types.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+# SPDX-License-Identifier: AGPL-3.0-only
+#
+
+# By default is expected that pwd returns <server_directory>/<apps or apps-extra>/spreed
+SERVER_DIR="${1:-$(dirname $(dirname $(pwd)))}"
+TYPES_DIR="$(pwd)/src/types"
+CORE_TYPES_OUTPUT_DIR="$TYPES_DIR/openapi/core"
+TEMP_DIR="$TYPES_DIR/tmp"
+
+# Create the temporary directory if it doesn't exist
+mkdir -p "$CORE_TYPES_OUTPUT_DIR"
+mkdir -p "$TEMP_DIR"
+
+# Find and copy openapi.json files, then translate to ts types
+generate_ts_files() {
+	local full_path=$1
+	local file_name=$2
+	local openapi_file="$SERVER_DIR/$full_path/$file_name"
+	local temp_file="$TEMP_DIR/openapi_${full_path#apps/}.json"
+	local ts_file="$CORE_TYPES_OUTPUT_DIR/openapi_${full_path#apps/}.ts"
+
+	if [ -f "$openapi_file" ]; then
+		cp "$openapi_file" "$temp_file"
+	else
+		echo "Error: $openapi_file is not found"
+		return 1
+	fi
+
+	npx openapi-typescript --redocly $TYPES_DIR "$temp_file" -t -o "$ts_file"
+}
+
+generate_ts_files "core" "openapi.json"
+generate_ts_files "apps/files_sharing" "openapi.json"
+generate_ts_files "apps/dav" "openapi.json"
+generate_ts_files "apps/provisioning_api" "openapi.json"
+
+rm -rf "$TEMP_DIR"
