67323ee625e4f2f473f38f9f4be98bb07c79e5da
Joas Schilling <coding@schilljs.com>
2025-02-14 14:48:57 +0100

    fix(conversation): Only trigger event with users that really got removed
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Service/ParticipantService.php
+++ lib/Service/ParticipantService.php
@@ -978,10 +978,10 @@ class ParticipantService {
 				$participant = $this->getParticipant($room, $user->getUID());
 				$participantType = $participant->getAttendee()->getParticipantType();
 
-				$attendees[] = $participant->getAttendee();
 				if ($participantType === Participant::USER) {
 					// Only remove normal users, not moderators/admins
 					$this->removeAttendee($room, $participant, $reason, true);
+					$attendees[] = $participant->getAttendee();
 				}
 			} catch (ParticipantNotFoundException $e) {
 			}
@@ -1041,10 +1041,10 @@ class ParticipantService {
 				$participant = $this->getParticipant($room, $user->getUID());
 				$participantType = $participant->getAttendee()->getParticipantType();
 
-				$attendees[] = $participant->getAttendee();
 				if ($participantType === Participant::USER) {
 					// Only remove normal users, not moderators/admins
 					$this->removeAttendee($room, $participant, $reason, true);
+					$attendees[] = $participant->getAttendee();
 				}
 			} catch (ParticipantNotFoundException $e) {
 			}
