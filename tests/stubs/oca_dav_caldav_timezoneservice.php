cb24bc997d6109cccb79024bb15f8afbe2368b91
Anna Larch <anna@nextcloud.com>
2025-03-11 14:30:15 +0100

    fixup! feat(conversations): handle event conversation rooms

---
+++ tests/stubs/oca_dav_caldav_timezoneservice.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OCA\DAV\CalDAV {
+
+	class TimezoneService {
+		public function getUserTimezone(string $userId): ?string {}
+
+		public function getDefaultTimezone(): string {}
+	}
+}
