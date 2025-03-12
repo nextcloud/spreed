205ffe302fc721e2844b346a2542419df0c16536
Anna Larch <anna@nextcloud.com>
2025-03-12 13:30:50 +0100

    fixup! feat(conversations): handle event conversation rooms

---
+++ tests/stubs/sabre_vobject_reader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace Sabre\VObject {
+
+	class Reader {
+		public static function read($data, $options = 0, $charset = 'UTF-8'){}
+	}
+}
