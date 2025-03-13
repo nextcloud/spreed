a5593d8516644be2f9f9cd150d0d236c6fe24dc6
Maksim Sukharev <antreesy.web@gmail.com>
2025-03-13 15:57:09 +0100

    fix: update module declaration
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/types/vendor/@nextcloud/vue.d.ts
+++ src/types/vendor/@nextcloud/vue.d.ts
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-declare module '@nextcloud/vue/dist/*.js'
-declare module '@nextcloud/vue/dist/*.mjs'
-declare module '@nextcloud/vue/dist/*.cjs'
-declare module '@nextcloud/vue/dist/*.vue'
+declare module '@nextcloud/vue/components/*'
+declare module '@nextcloud/vue/composables/*'
+declare module '@nextcloud/vue/directives/*'
+declare module '@nextcloud/vue/functions/*'
