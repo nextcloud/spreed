a9ebc8a77d246582279fbca1858f4b0ccc3f1548
Anna Larch <anna@nextcloud.com>
2025-03-17 18:34:29 +0100

    feat: 🍱 Talk Dashboard - ⚙️ API
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- lib/ResponseDefinitions.php
+++ lib/ResponseDefinitions.php
@@ -232,67 +232,158 @@ namespace OCA\Talk;
  * }
  *
  * @psalm-type TalkRoom = array{
+ *     // The unique identifier for the given actor type
  *     actorId: string,
+ * 		// The cloud id of the invited user
  *     invitedActorId?: string,
+ * 	   // Actor type of the current user (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#attendee-types))
  *     actorType: string,
+ *     // Unique attendee id
  *     attendeeId: int,
+ *     // Dedicated permissions for the current participant, if not `Custom` this are not the resulting permissions (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#attendee-permissions))
  *     attendeePermissions: int,
+ *     // Unique dial-in authentication code for this user, when the conversation has SIP enabled (see `sipEnabled` attribute)
  *     attendeePin: ?string,
+ *     // Version of conversation avatar used to easier expiration of the avatar in case a moderator updates it, since the avatar endpoint should be cached for 24 hours. (only available with `avatar` capability)
  *     avatarVersion: string,
+ *     // Breakout room configuration mode (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#breakout-room-modes)) (only available with `breakout-rooms-v1` capability)
  *     breakoutRoomMode: int,
+ *     // Breakout room status (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#breakout-room-status)) (only available with `breakout-rooms-v1` capability)
  *     breakoutRoomStatus: int,
+ *     // Combined flag of all participants in the current call (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#participant-in-call-flag), only available with `conversation-call-flags` capability)
  *     callFlag: int,
+ *     // Call permissions, if not `Custom` this are not the resulting permissions, if set they will reset after the end of the call (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#attendee-permissions))
  *     callPermissions: int,
+ *     // Type of call recording (see [Constants - Call recording status](https://nextcloud-talk.readthedocs.io/en/latest/constants#call-recording-status)) (only available with `recording-v1` capability)
  *     callRecording: 0|1|2|3|4|5,
+ *     // Timestamp when the call was started (only available with `recording-v1` capability)
  *     callStartTime: int,
+ *     // Flag if the user can delete the conversation for everyone (not possible without moderator permissions or in one-to-one conversations)
  *     canDeleteConversation: bool,
+ * 	   // Whether the given user can enable SIP for this conversation. Note that when the token is not-numeric only, SIP can not be enabled even if the user is permitted and a moderator of the conversation
  *     canEnableSIP: bool,
+ *     // Flag if the user can leave the conversation (not possible for the last user with moderator permissions)
  *     canLeaveConversation: bool,
+ *     // Flag if the user can start a new call in this conversation (joining is always possible) (only available with `start-call-flag` capability)
  *     canStartCall: bool,
+ *     // Default permissions for new participants (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#attendee-permissions))
  *     defaultPermissions: int,
+ *     // Description of the conversation (can also be empty) (only available with `room-description` capability)
  *     description: string,
+ *     // `name` if non-empty, otherwise it falls back to a list of participants
  *     displayName: string,
+ *     // Flag if the conversation has an active call
  *     hasCall: bool,
+ *     // Flag if the conversation has a password
  *     hasPassword: bool,
+ *     // Numeric identifier of the conversation
  *     id: int,
+ *     // Flag if the conversation has a custom avatar (only available with `avatar` capability)
  *     isCustomAvatar: bool,
+ *     // Flag if the conversation is favorited by the user
  *     isFavorite: bool,
+ *     // Timestamp of the last activity in the conversation, in seconds and UTC time zone
  *     lastActivity: int,
+ *     // ID of the last message read by every user that has read privacy set to public in a room. When the user themself has it set to private the value is `0` (only available with `chat-read-status` capability)
  *     lastCommonReadMessage: int,
+ * 	   // Last message in a conversation if available, otherwise empty. **Note:** Even when given the message will not contain the `parent` or `reactionsSelf` attribute due to performance reasons
  *     lastMessage?: TalkRoomLastMessage,
+ *     // Timestamp of the user's session making the request
  *     lastPing: int,
+ *     // ID of the last read message in a room (only available with `chat-read-marker` capability)
  *     lastReadMessage: int,
+ *     // Listable scope for the room (only available with `listable-rooms` capability)
  *     listable: int,
+ *     // Webinar lobby restriction (0-1), if the participant is a moderator they can always join the conversation (only available with `webinary-lobby` capability) (See [Webinar lobby states](https://nextcloud-talk.readthedocs.io/en/latest/constants#webinar-lobby-states))
  *     lobbyState: int,
+ *     // Timestamp when the lobby will be automatically disabled (only available with `webinary-lobby` capability)
  *     lobbyTimer: int,
+ *     // Whether all participants can mention using `@all` or only moderators (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#mention-permissions)) (only available with `mention-permissions` capability)
  *     mentionPermissions: 0|1,
+ *     // The message expiration time in seconds in this chat. Zero if disabled. (only available with `message-expiration` capability)
  *     messageExpiration: int,
+ *     // Name of the conversation (can also be empty)
  *     name: string,
  *     notificationCalls: int,
+ *     // The notification level for the user (See [Participant notification levels](https://nextcloud-talk.readthedocs.io/en/latest/constants#participant-notification-levels))
  *     notificationLevel: int,
+ *     // See [Object types](https://nextcloud-talk.readthedocs.io/en/latest/constants#object-types) documentation for explanation
  *     objectId: string,
+ *     // The type of object that the conversation is associated with (See [Object types](https://nextcloud-talk.readthedocs.io/en/latest/constants#object-types))
  *     objectType: string,
+ *     // "In call" flags of the user's session making the request (only available with `in-call-flags` capability)
  *     participantFlags: int,
+ * 	   // Permissions level of the current user
  *     participantType: int,
+ *     // Combined final permissions for the current participant, permissions are picked in order of attendee then call then default and the first which is `Custom` will apply (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#attendee-permissions))
  *     permissions: int,
+ *     // Read-only state for the current user (only available with `read-only-rooms` capability)
  *     readOnly: int,
+ *     // Whether recording consent is required before joining a call (Only 0 and 1 will be returned, see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#recording-consent-required)) (only available with `recording-consent` capability)
  *     recordingConsent: int,
  *     remoteServer?: string,
  *     remoteToken?: string,
+ *     // `'0'` if not connected, otherwise an up to 512 character long string that is the identifier of the user's session making the request. Should only be used to pre-check if the user joined already with this session, but this might be outdated by the time of usage, so better check via [Get list of participants in a conversation](https://nextcloud-talk.readthedocs.io/en/latest/participant/#get-list-of-participants-in-a-conversation)
  *     sessionId: string,
+ *     // SIP enable status (see [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants#sip-states))
  *     sipEnabled: int,
+ *     // Optional: Only available for one-to-one conversations, when `includeStatus=true` is set and the user has a status
  *     status?: string,
+ *     // Optional: Only available for one-to-one conversations, when `includeStatus=true` is set and the user has a status, can still be null even with a status
  *     statusClearAt?: ?int,
+ *     // Optional: Only available for one-to-one conversations, when `includeStatus=true` is set and the user has a status, can still be null even with a status
  *     statusIcon?: ?string,
+ *     // Optional: Only available for one-to-one conversations, when `includeStatus=true` is set and the user has a status, can still be null even with a status
  *     statusMessage?: ?string,
+ * 	   // Token identifier of the conversation which is used for further interaction
  *     token: string,
+ *     // See list of conversation types in the [constants list](https://nextcloud-talk.readthedocs.io/en/latest/constants/#conversation-types)
  *     type: int,
+ *     // Flag if the user was mentioned since their last visit
  *     unreadMention: bool,
+ *     // Flag if the user was mentioned directly (ignoring `@all` mentions) since their last visit (only available with `direct-mention-flag` capability)
  *     unreadMentionDirect: bool,
+ *     // Number of unread chat messages in the conversation (only available with `chat-v2` capability)
  *     unreadMessages: int,
+ *     // Flag if the conversation is archived by the user (only available with `archived-conversations-v2` capability)
  *     isArchived: bool,
  *     // Required capability: `important-conversations`
  *     isImportant: bool,
+ * }
+ *
+ * @psalm-type TalkDashboardEventAttachment = array{
+ *      calendars: list<string>,
+ *      fmttype: string,
+ *      filename: string,
+ *      fileId: int,
+ *      hasPreview: boolean,
+ *      previewUrl: ?string,
+ * }
+ *
+ * @psalm-type TalkDashboardEventCalendar = array{
+ *     principalUri: string,
+ *     calendarName: string,
+ *     calendarColor: ?string,
+ * }
+ *
+ * @psalm-type TalkDashboardEvent = array{
+ *     calendars: list<TalkDashboardEventCalendar>,
+ *     eventName: string,
+ *     eventDescription: ?string,
+ *     eventAttachments: list<TalkDashboardEventAttachment>,
+ *     eventLink: string,
+ *     start: int,
+ *     end: int,
+ *     roomToken: string,
+ *     roomAvatarVersion: string,
+ *     roomName: string,
+ *     roomDisplayName: string,
+ *     roomType: int,
+ *     roomActiveSince: ?int,
+ *     invited: ?int,
+ *     accepted: ?int,
+ *     tentative: ?int,
+ *     declined: ?int,
  * }
  *
  * @psalm-type TalkRoomWithInvalidInvitations = TalkRoom&array{
---
+++ lib/Service/DashboardService.php
@@ -0,0 +1,180 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Talk\Service;
+
+use OCA\Talk\Dashboard\Event;
+use OCA\Talk\Exceptions\RoomNotFoundException;
+use OCA\Talk\Manager;
+use OCA\Talk\ResponseDefinitions;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\Calendar\ICalendar;
+use OCP\Calendar\IManager;
+use OCP\IDateTimeZone;
+use OCP\IURLGenerator;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @psalm-import-type TalkDashboardEvent from ResponseDefinitions
+ */
+class DashboardService {
+	public function __construct(
+		private Manager $manager,
+		private IManager $calendarManager,
+		private ITimeFactory $timeFactory,
+		private LoggerInterface $logger,
+		private RoomService $roomService,
+		private IDateTimeZone $dateTimeZone,
+		private AvatarService $avatarService,
+		private IURLGenerator $urlGenerator,
+	) {
+
+	}
+
+	/**
+	 * @param string $userId
+	 * @return list<TalkDashboardEvent>
+	 */
+	public function getEvents(string $userId): array {
+		$principaluri = 'principals/users/' . $userId;
+		$calendars = $this->calendarManager->getCalendarsForPrincipal($principaluri);
+		if (count($calendars) === 0) {
+			return [];
+		}
+
+		// Only use personal calendars
+		// Events for shared calendars where you are an ATTENDEE will be in your personal calendar
+		$calendars = array_filter($calendars, static function (ICalendar $calendar) {
+			if (method_exists($calendar, 'isShared')) {
+				return $calendar->isShared() === false;
+			}
+			return true;
+		});
+
+		$userTimezone = $this->dateTimeZone->getTimezone();
+		// Midnight for the current user so we also include ongoing events (might be all day events)
+		$start = $this->timeFactory->getDateTime()->setTimezone($userTimezone)->setTime(0, 0);
+		$start = $start->setTimezone(new \DateTimeZone('UTC'));
+		$end = clone($start);
+		$end = $end->add(\DateInterval::createFromDateString('1 week'));
+		$options = [
+			'timerange' => [
+				'start' => $start,
+				'end' => $end,
+			],
+		];
+
+		$pattern = '/call/';
+		$searchProperties = ['LOCATION'];
+		$events = [];
+		/** @var ICalendar $calendar */
+		foreach ($calendars as $calendar) {
+			$searchResult = $calendar->search($pattern, $searchProperties, $options, 10);
+			foreach ($searchResult as $calendarEvent) {
+				// Find first recurrence in the future
+				$event = null;
+				$dashboardEvent = new Event();
+
+				foreach ($calendarEvent['objects'] as $object) {
+					$dashboardEvent->setStart(\DateTime::createFromImmutable($object['DTSTART'][0])->setTimezone($userTimezone)->getTimestamp());
+					$dashboardEvent->setEnd(\DateTime::createFromImmutable($object['DTEND'][0])->setTimezone($userTimezone)->getTimestamp());
+					// Filter out events in the past
+					if ($dashboardEvent->getEnd() <= $this->timeFactory->getDateTime('now', $userTimezone)->getTimestamp()) {
+						continue;
+					}
+
+					$event = $object;
+					break;
+				}
+
+				$location = $event['LOCATION'][0] ?? null;
+				if ($event === null || $location === null) {
+					continue;
+				}
+
+				try {
+					$token = $this->roomService->parseRoomTokenFromUrl($location);
+					// Already returns public / open conversations
+					$room = $this->manager->getRoomForUserByToken($token, $userId);
+				} catch (RoomNotFoundException) {
+					$this->logger->debug("Room for url $location not found in dashboard service");
+					continue;
+				}
+
+				$dashboardEvent->setRoomToken($token);
+				$dashboardEvent->setRoomType($room->getType());
+				$dashboardEvent->setRoomName($room->getName());
+				$dashboardEvent->setRoomDisplayName($room->getDisplayName($userId));
+
+				if (isset($event['ATTENDEE'])) {
+					$dashboardEvent->generateAttendance($event['ATTENDEE']);
+				}
+
+				$dashboardEvent->setEventName($event['SUMMARY'][0] ?? '');
+				$dashboardEvent->setEventDescription($event['DESCRIPTION'][0] ?? null);
+
+				if (isset($event['ATTACH'])) {
+					$dashboardEvent->handleCalendarAttachments($calendar->getUri(), $event['ATTACH']);
+				}
+
+				if (isset($events[$dashboardEvent->generateEventIdentifier()])) {
+					/** @var Event $existing */
+					$existing = $events[$dashboardEvent->generateEventIdentifier()];
+					$existing->addCalendar($calendar->getUri(), $calendar->getDisplayName(), $calendar->getDisplayColor());
+					// Merge attachments
+					$existing->mergeAttachments($dashboardEvent);
+
+					// If original SUMMARY is empty, use the duplicate content if it exists
+					if ($existing->getEventDescription() === null) {
+						$existing->setEventDescription($dashboardEvent->getEventDescription() ?? '');
+					}
+
+					// We continue here as the same event already exists in a different calendar
+					$events[$existing->generateEventIdentifier()] = $existing;
+					continue;
+				}
+
+				$dashboardEvent->addCalendar($calendar->getUri(), $calendar->getDisplayName(), $calendar->getDisplayColor());
+				$dashboardEvent->setRoomAvatarVersion($this->avatarService->getAvatarVersion($room));
+				$dashboardEvent->setRoomActiveSince($room->getActiveSince()?->getTimestamp());
+				$objectId = base64_encode($this->urlGenerator->getWebroot() . '/remote.php/dav/calendars/' . $userId . '/' . $calendar->getUri() . '/' . $calendarEvent['uri']);
+
+				if (isset($event['RECURRENCE-ID'])) {
+					$dashboardEvent->setEventLink(
+						$this->urlGenerator->linkToRouteAbsolute(
+							'calendar.view.indexdirect.edit',
+							[
+								'objectId' => $objectId,
+								'recurrenceId' => $event['RECURRENCE-ID'][0],
+							]
+						)
+					);
+				} else {
+					$dashboardEvent->setEventLink(
+						$this->urlGenerator->linkToRouteAbsolute('calendar.view.indexdirect.edit', ['objectId' => $objectId])
+					);
+				}
+
+				$events[$dashboardEvent->generateEventIdentifier()] = $dashboardEvent;
+			}
+		}
+
+		if (empty($events)) {
+			return $events;
+		}
+
+		usort($events, static function (Event $a, Event $b) {
+			return $a->getStart() - $b->getStart();
+		});
+
+		return array_map(static function (Event $event) {
+			return $event->jsonSerialize();
+		}, array_slice($events, 0, 10));
+	}
+}
