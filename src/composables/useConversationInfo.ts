dfac54ceb652907dad4aa396a4422332622a45a8
Dorra Jaouad <dorra.jaoued7@gmail.com>
2025-03-24 14:37:25 +0100

    feat: add time left for the event to start as a subline for event conversations
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/composables/useConversationInfo.ts
+++ src/composables/useConversationInfo.ts
@@ -11,6 +11,7 @@ import { t } from '@nextcloud/l10n'
 
 import { ATTENDEE, CONVERSATION, PARTICIPANT } from '../constants.ts'
 import type { Conversation } from '../types/index.ts'
+import { futureRelativeTime } from '../utils/formattedTime.ts'
 import { getMessageIcon } from '../utils/getMessageIcon.ts'
 
 type Payload = {
@@ -114,6 +115,11 @@ export function useConversationInfo({
 				message: t('spreed', 'Joining conversation …'),
 				title: t('spreed', 'Joining conversation …'),
 			}
+		}
+
+		if (item.value.objectType === CONVERSATION.OBJECT_TYPE.EVENT && item.value.objectId
+			&& item.value.objectId > Date.now()) {
+			return futureRelativeTime(item.value.objectId)
 		}
 
 		if (!exposeMessages) {
