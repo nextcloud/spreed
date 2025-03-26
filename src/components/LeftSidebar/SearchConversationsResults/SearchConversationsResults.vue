f31ccac81ada43536a015d190d05eb1e94379d14
Dorra Jaouad <dorra.jaoued7@gmail.com>
2025-03-26 13:13:52 +0100

    fix: reduce props where component does not actually rely on parent. Refactor virtual list in order to reduce loops.
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/components/LeftSidebar/SearchConversationsResults/SearchConversationsResults.vue
+++ src/components/LeftSidebar/SearchConversationsResults/SearchConversationsResults.vue
@@ -7,7 +7,7 @@
 import { computed, ref } from 'vue'
 import { RecycleScroller } from 'vue-virtual-scroller'
 
-import ChatPlus from 'vue-material-design-icons/ChatPlus.vue'
+import IconChatPlus from 'vue-material-design-icons/ChatPlus.vue'
 
 import { loadState } from '@nextcloud/initial-state'
 import { t } from '@nextcloud/l10n'
@@ -20,77 +20,80 @@ import ConversationIcon from '../../ConversationIcon.vue'
 import Hint from '../../UIShared/Hint.vue'
 import Conversation from '../ConversationsList/Conversation.vue'
 
-import { useStore } from '../../../composables/useStore.js'
 import { ATTENDEE, CONVERSATION, AVATAR } from '../../../constants.ts'
+import { useSettingsStore } from '../../../stores/settings.js'
 import type { Conversation as TypeConversation, ParticipantSearchResult } from '../../../types/index.ts'
 
-const store = useStore()
-const isCirclesEnabled = loadState('spreed', 'circles_enabled')
-
 const props = defineProps<{
-    searchText: string,
-    isCompact: boolean,
-    isFocused: boolean,
-    conversationsList: Array<TypeConversation>,
-    contactsLoading: boolean,
-    canStartConversations: boolean,
-    searchResultsListedConversations: Array<TypeConversation>,
-    searchResults: Array<ParticipantSearchResult>,
-
+	searchText: string,
+	conversationsList: TypeConversation[],
+	contactsLoading: boolean,
+	searchResultsListedConversations: TypeConversation[],
+	searchResults: ParticipantSearchResult[],
 }>()
 
-const itemSize = computed(() => props.isCompact ? 28 + 2 * 2 + 0 * 2 : AVATAR.SIZE.DEFAULT + 2 * 4 + 2 * 2)
+const emit = defineEmits<{
+	(event: 'abort-search'): void,
+	(event: 'create-new-conversation', searchText: string): void,
+	(event: 'create-and-join-conversation', item: TypeConversation): void,
+}>()
 
-const searchResultsConversationList = computed(() => {
-	if (props.searchText !== '' || props.isFocused) {
-		const lowerSearchText = props.searchText.toLowerCase()
-		return props.conversationsList.filter(conversation =>
-			conversation.displayName.toLowerCase().includes(lowerSearchText)
-            || conversation.name.toLowerCase().includes(lowerSearchText)
-		)
-	} else {
-		return []
-	}
-})
+const isCirclesEnabled = loadState('spreed', 'circles_enabled')
+const canStartConversations = loadState('spreed', 'start_conversations')
+const settingsStore = useSettingsStore()
+const isCompact = computed(() => settingsStore.conversationsListStyle === CONVERSATION.LIST_STYLE.COMPACT)
 
-/**
- * Check if the user has an existing one-to-one conversation with the given user
- *
- * @param userId the user id to check
- */
-function hasOneToOneConversationWith(userId: string) {
-	return props.conversationsList.length > 0 && props.conversationsList.some((conversation) => {
-		return conversation.type === CONVERSATION.TYPE.ONE_TO_ONE && conversation.name === userId
-	})
+// Item's content (avatar) + internal_padding * 2 + external_padding * 2
+const itemSize = computed(() => isCompact.value ? 28 + 2 * 2 + 0 * 2 : AVATAR.SIZE.DEFAULT + 2 * 4 + 2 * 2)
+
+// Other sections than joined conversations in the output order
+const sections = [
+	{
+		type: 'user',
+		caption: { type: 'caption', id: 'users_caption', name: t('spreed', 'Users') },
+	},
+	{
+		type: 'group',
+		caption: { type: 'caption', id: 'groups_caption', name: t('spreed', 'Groups') },
+	},
+	{
+		type: 'circle',
+		caption: { type: 'caption', id: 'circles_caption', name: t('spreed', 'Teams') },
+	},
+	{
+		type: 'federated',
+		caption: { type: 'caption', id: 'federated_users_caption', name: t('spreed', 'Federated users') },
+	},
+] as const
+
+type SubListType = {
+	user: ParticipantSearchResult[],
+	group: ParticipantSearchResult[],
+	circle: ParticipantSearchResult[],
+	federated: ParticipantSearchResult[],
 }
 
-const hasResultsUsers = ref(false)
-const hasResultsGroups = ref(false)
-const hasResultsCircles = ref(false)
-const hasResultsFederatedUsers = ref(false)
-
-/**
- * Prepare the search results for the virtual list
- */
-function prepareSearchResultsVirtual() {
+const searchResultsVirtual = computed(() => {
 	// Initialize
 	const virtualList = []
-	hasResultsUsers.value = false
-	hasResultsGroups.value = false
-	hasResultsCircles.value = false
-	hasResultsFederatedUsers.value = false
+
+	const lowerSearchText = props.searchText.toLowerCase()
+	const searchResultsConversationList = props.conversationsList.filter(conversation =>
+		conversation.displayName.toLowerCase().includes(lowerSearchText)
+		|| conversation.name.toLowerCase().includes(lowerSearchText)
+	)
 
 	// Add conversations section
 	virtualList.push({ type: 'caption', id: 'conversations_caption', name: t('spreed', 'Conversations') })
-	if (searchResultsConversationList.value.length === 0) {
+	if (searchResultsConversationList.length === 0) {
 		virtualList.push({ type: 'hint', id: 'hint_conversations', hint: t('spreed', 'No matches found') })
 	} else {
-		virtualList.push(...searchResultsConversationList.value.map((item) => ({ type: 'conversation', id: item.id, object: item })))
+		virtualList.push(...searchResultsConversationList.map((item) => ({ type: 'conversation', id: item.id, object: item })))
 	}
 
 	// Add "New Conversation" option if allowed
-	if (props.canStartConversations) {
-		virtualList.push({ type: 'listItem', id: 'new_conversation', name: props.searchText, subname: t('spreed', 'New private conversation') })
+	if (canStartConversations) {
+		virtualList.push({ type: 'action', id: 'new_conversation', name: props.searchText, subname: t('spreed', 'New private conversation') })
 	}
 
 	// Add open conversations section if any
@@ -99,65 +102,54 @@ function prepareSearchResultsVirtual() {
 		virtualList.push(...props.searchResultsListedConversations.map((item) => ({ type: 'open_conversation', id: item.id, object: item })))
 	}
 
-	// Other sections in the output order
-	const sections = [
-		{
-			type: 'user',
-			caption: { type: 'caption', id: 'users_caption', name: t('spreed', 'Users') },
-			condition: (match : ParticipantSearchResult) => match.source === ATTENDEE.ACTOR_TYPE.USERS && match.id !== store.getters.getUserId() && !hasOneToOneConversationWith(match.id),
-		},
-		{
-			type: 'group',
-			caption: { type: 'caption', id: 'groups_caption', name: t('spreed', 'Groups') },
-			condition: (match: ParticipantSearchResult) => match.source === ATTENDEE.ACTOR_TYPE.GROUPS && props.canStartConversations,
-		},
-		{
-			type: 'circle',
-			caption: { type: 'caption', id: 'circles_caption', name: t('spreed', 'Teams') },
-			condition: (match: ParticipantSearchResult) => match.source === ATTENDEE.ACTOR_TYPE.CIRCLES && props.canStartConversations,
-		},
-		{
-			type: 'federated',
-			caption: { type: 'caption', id: 'federated_users_caption', name: t('spreed', 'Federated users') },
-			condition: (match: ParticipantSearchResult) => match.source === ATTENDEE.ACTOR_TYPE.REMOTES && props.canStartConversations,
-			transform: (match: ParticipantSearchResult) => ({ ...match, source: ATTENDEE.ACTOR_TYPE.FEDERATED_USERS }),
-		},
-	]
+	// Categorize search results into different sections
+	const subList = props.searchResults.reduce<SubListType>((acc, result) => {
+		if (result.source === ATTENDEE.ACTOR_TYPE.USERS) {
+			acc.user.push(result)
+		} else if (result.source === ATTENDEE.ACTOR_TYPE.GROUPS && canStartConversations) {
+			acc.group.push(result)
+		} else if (result.source === ATTENDEE.ACTOR_TYPE.CIRCLES && canStartConversations) {
+			acc.circle.push(result)
+		} else if (result.source === ATTENDEE.ACTOR_TYPE.REMOTES && canStartConversations) {
+			acc.federated.push({ ...result, source: ATTENDEE.ACTOR_TYPE.FEDERATED_USERS })
+		}
+		return acc
+	}, {
+		user: [],
+		group: [],
+		circle: [],
+		federated: [],
+	})
 
 	// Iterate over sections and build the virtualList
 	sections.forEach((section) => {
-		const items = props.searchResults
-			.filter(section.condition)
-			.map((match) => ({
+		if (subList[section.type].length > 0) {
+			virtualList.push(section.caption)
+			virtualList.push(...subList[section.type].map((match) => ({
 				type: section.type,
 				id: `${section.type}_${match.id}`,
-				object: section.transform ? section.transform(match) : match,
-			}))
-
-		if (items.length > 0) {
-			virtualList.push(section.caption)
-			virtualList.push(...items)
-
-			// Update hasResults flags
-			hasResultsUsers.value = hasResultsUsers.value || section.type === 'user'
-			hasResultsGroups.value = hasResultsGroups.value || section.type === 'group'
-			hasResultsCircles.value = hasResultsCircles.value || section.type === 'circle'
-			hasResultsFederatedUsers.value = hasResultsFederatedUsers.value || section.type === 'federated'
+				object: match,
+				icon: iconData(match),
+			})))
 		}
 	})
 
-	return virtualList
-}
+	// Add "No results" message if there are no results in any section
+	if (!subList.user.length || !subList.group.length || (!subList.circle.length && isCirclesEnabled) || !subList.federated.length) {
+		virtualList.push({ type: 'caption', id: 'no_results_caption', name: sourcesWithoutResults(subList) })
+		virtualList.push({ type: 'hint', id: 'no_results_hint', hint: t('spreed', 'No search results') })
+	}
 
-const searchResultsVirtual = computed(() => prepareSearchResultsVirtual())
+	return virtualList
+})
 
 /**
  * Generate the props for the AvatarWrapper component
  * @param item conversation item
  */
-function iconData(item : ParticipantSearchResult) {
+function iconData(item: ParticipantSearchResult) {
 	if (item.source === ATTENDEE.ACTOR_TYPE.USERS
-        || item.source === ATTENDEE.ACTOR_TYPE.FEDERATED_USERS) {
+		|| item.source === ATTENDEE.ACTOR_TYPE.FEDERATED_USERS) {
 		return {
 			id: item.id,
 			name: item.label,
@@ -165,70 +157,58 @@ function iconData(item : ParticipantSearchResult) {
 			disableMenu: true,
 			token: 'new',
 			showUserStatus: true,
-			size: props.isCompact ? AVATAR.SIZE.COMPACT : AVATAR.SIZE.DEFAULT,
+			size: isCompact.value ? AVATAR.SIZE.COMPACT : AVATAR.SIZE.DEFAULT,
 		}
 	}
 	return {
 		type: CONVERSATION.TYPE.GROUP,
 		name: item.label,
 		objectType: item.source,
-		size: props.isCompact ? AVATAR.SIZE.COMPACT : AVATAR.SIZE.DEFAULT,
+		size: isCompact.value ? AVATAR.SIZE.COMPACT : AVATAR.SIZE.DEFAULT,
 	}
 }
 
-const emit = defineEmits<{
-	(event: 'abort-search'): void,
-    (event: 'create-new-conversation', searchText: string): void,
-    (event: 'create-and-join-conversation', item: TypeConversation): void,
-}>()
-
-const sourcesWithoutResults = computed(() => {
-	return !hasResultsUsers.value
-		|| !hasResultsGroups.value
-		|| (isCirclesEnabled && !hasResultsCircles.value)
+const hasSourcesWithoutResults = computed(() => {
+	return !searchResultsVirtual.value.some(item => item.type === 'user' || item.type === 'group'
+	|| (item.type === 'circle' && isCirclesEnabled))
 })
 
-const hasNoCirclesResults = computed(() => {
-	return isCirclesEnabled && !hasResultsCircles.value
-})
-
-const sourcesWithoutResultsList = computed(() => {
-	if (!hasResultsUsers.value) {
-		if (!hasResultsGroups.value) {
-			return (hasNoCirclesResults.value)
+/**
+ * Generate the message for the "No results" section
+ * @param list search results
+ */
+function sourcesWithoutResults(list: SubListType): string {
+	const hasNoCirclesResults = isCirclesEnabled && !list.circle.length
+	if (!list.user.length) {
+		if (!list.group.length) {
+			return (hasNoCirclesResults)
 				? t('spreed', 'Users, groups and teams')
 				: t('spreed', 'Users and groups')
 		} else {
-			return (hasNoCirclesResults.value)
+			return (hasNoCirclesResults)
 				? t('spreed', 'Users and teams')
 				: t('spreed', 'Users')
 		}
 	} else {
-		if (!hasResultsGroups.value) {
-			return (hasNoCirclesResults.value)
+		if (!list.group.length) {
+			return (hasNoCirclesResults)
 				? t('spreed', 'Groups and teams')
 				: t('spreed', 'Groups')
 		} else {
-			return (hasNoCirclesResults.value)
+			return (hasNoCirclesResults)
 				? t('spreed', 'Teams')
 				: t('spreed', 'Other sources')
 		}
 	}
+}
+
+const footerMargin = computed(() => {
+	return isCompact.value ? '0' : '18px' // 54px (item height) - 36px (current height)
 })
 
-const computedStyleCaption = computed(() => {
-	return {
-		height: itemSize.value + 'px',
-		alignItems: props.isCompact ? 'unset' : 'self-end',
-	}
-})
-
-const computedStyleFooter = computed(() => {
-	return {
-		marginBlockStart: props.isCompact ? '0' : '18px', // 54px (item height) - 36px (current height)
-	}
-})
+const iconSize = computed(() => isCompact.value ? AVATAR.SIZE.COMPACT : AVATAR.SIZE.DEFAULT)
 </script>
+
 <template>
 	<RecycleScroller ref="scroller"
 		item-tag="ul"
@@ -242,13 +222,13 @@ const computedStyleFooter = computed(() => {
 				:item="item.object"
 				:compact="isCompact"
 				@click="emit('abort-search')" />
-			<NcListItem v-else-if="item.type === 'listItem'"
+			<NcListItem v-else-if="item.type === 'action'"
 				:name="searchText"
 				:compact="isCompact"
 				data-nav-id="conversation_create_new"
 				@click="emit('create-new-conversation', searchText)">
 				<template #icon>
-					<ChatPlus :size="isCompact ? AVATAR.SIZE.COMPACT: AVATAR.SIZE.DEFAULT" />
+					<IconChatPlus :size="iconSize" />
 				</template>
 				<template v-if="!isCompact" #subname>
 					{{ t('spreed', 'New group conversation') }}
@@ -260,7 +240,13 @@ const computedStyleFooter = computed(() => {
 				is-search-result
 				:compact="isCompact"
 				@click="emit('abort-search')" />
-			<NcAppNavigationCaption v-else-if="item.type === 'caption'" :name="item.name" :style="computedStyleCaption" />
+			<NcAppNavigationCaption v-else-if="item.type === 'caption'"
+				:name="item.name"
+				tabindex="-1"
+				:style="{
+					height: itemSize + 'px',
+					alignItems: isCompact ? 'unset' : 'self-end',
+				}" />
 			<Hint v-else-if="item.type === 'hint'" :hint="item.hint" />
 			<NcListItem v-else-if="item.type === 'user'"
 				:key="`user_${item.id}`"
@@ -269,7 +255,7 @@ const computedStyleFooter = computed(() => {
 				:compact="isCompact"
 				@click="emit('create-and-join-conversation', item.object)">
 				<template #icon>
-					<AvatarWrapper v-bind="iconData(item.object)" />
+					<AvatarWrapper v-bind="item.icon" />
 				</template>
 				<template v-if="!isCompact" #subname>
 					{{ t('spreed', 'New private conversation') }}
@@ -282,7 +268,7 @@ const computedStyleFooter = computed(() => {
 				:compact="isCompact"
 				@click="emit('create-and-join-conversation', item.object)">
 				<template #icon>
-					<ConversationIcon :item="iconData(item.object)" :size="isCompact ? AVATAR.SIZE.COMPACT: AVATAR.SIZE.DEFAULT" />
+					<ConversationIcon :item="item.icon" :size="iconSize" />
 				</template>
 				<template v-if="!isCompact" #subname>
 					{{ t('spreed', 'New group conversation') }}
@@ -295,7 +281,7 @@ const computedStyleFooter = computed(() => {
 				:compact="isCompact"
 				@click="emit('create-and-join-conversation', item.object)">
 				<template #icon>
-					<ConversationIcon :item="iconData(item.object)" :size="isCompact ? AVATAR.SIZE.COMPACT: AVATAR.SIZE.DEFAULT" />
+					<ConversationIcon :item="item.icon" :size="iconSize" />
 				</template>
 				<template v-if="!isCompact" #subname>
 					{{ t('spreed', 'New group conversation') }}
@@ -308,7 +294,7 @@ const computedStyleFooter = computed(() => {
 				:compact="isCompact"
 				@click="emit('create-and-join-conversation', item.object)">
 				<template #icon>
-					<AvatarWrapper v-bind="iconData(item.object)" />
+					<AvatarWrapper v-bind="item.icon" />
 				</template>
 				<template v-if="!isCompact" #subname>
 					{{ t('spreed', 'New group conversation') }}
@@ -317,13 +303,10 @@ const computedStyleFooter = computed(() => {
 		</template>
 		<template #after>
 			<!-- Search results: no results (yet) -->
-			<div :style="computedStyleFooter">
-				<template v-if="sourcesWithoutResults">
-					<NcAppNavigationCaption :name="sourcesWithoutResultsList" />
-					<Hint :hint="t('spreed', 'No search results')" />
-				</template>
-				<Hint v-else-if="contactsLoading" :hint="t('spreed', 'Loading …')" />
-			</div>
+			<Hint v-if="contactsLoading && !hasSourcesWithoutResults"
+				:style="{ marginBlockStart: footerMargin }"
+				tabindex="-1"
+				:hint="t('spreed', 'Loading …')" />
 		</template>
 	</RecycleScroller>
 </template>
