1b79199c7dfc9c73c8dfe7011788eb1a78d40df3
Joas Schilling <coding@schilljs.com>
2025-03-27 16:25:57 +0100

    feat(sip): Allow adding, removing and importing phone numbers
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ lib/Command/PhoneNumber/FindPhoneNumber.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Talk\Command\PhoneNumber;
+
+use OC\Core\Command\Base;
+use OCA\Talk\Model\PhoneNumberMapper;
+use OCP\AppFramework\Db\DoesNotExistException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FindPhoneNumber extends Base {
+
+	public function __construct(
+		private PhoneNumberMapper $mapper,
+	) {
+		parent::__construct();
+	}
+
+	#[\Override]
+	protected function configure(): void {
+		$this
+			->setName('talk:phone-number:find')
+			->setDescription('Find a phone number or the phone number of an account')
+			->addOption(
+				'phone',
+				null,
+				InputOption::VALUE_REQUIRED,
+				'Phone number to search for',
+			)
+			->addOption(
+				'account',
+				null,
+				InputOption::VALUE_REQUIRED,
+				'Account to get number(s) for',
+			)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$phoneNumber = (string)$input->getOption('phone');
+		$userId = (string)$input->getOption('account');
+
+		if ($phoneNumber !== '') {
+			try {
+				$entry = $this->mapper->findByPhoneNumber($phoneNumber);
+			} catch (DoesNotExistException) {
+				$output->writeln('<error>Phone number ' . $phoneNumber . ' could not be found</error>');
+				return self::FAILURE;
+			}
+			$output->writeln('Phone number ' . $entry->getPhoneNumber() . ' is assigned to ' . $entry->getActorId());
+			return self::SUCCESS;
+		}
+
+		if ($userId === '') {
+			$output->writeln('<error>Neither phone number nor account provided</error>');
+			return self::FAILURE;
+		}
+
+		$entries = $this->mapper->findByUser($userId);
+		if (empty($entries)) {
+			$output->writeln('<error>No phone number found for ' . $userId . '</error>');
+			return self::FAILURE;
+		}
+
+		if (count($entries) === 1) {
+			$entry = array_pop($entries);
+			$output->writeln($entry->getActorId() . ' has phone number ' . $entry->getPhoneNumber() . ' assigned');
+		} else {
+			$entry = array_pop($entries);
+			$output->writeln($entry->getActorId() . ' has the following phone numbers assigned:');
+			foreach ($entries as $entry) {
+				$output->writeln(' - ' . $entry->getPhoneNumber());
+			}
+		}
+
+		return self::SUCCESS;
+	}
+}
