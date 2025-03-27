351bb867fd4bc137badc2cbad3f0334b9a529559
Maksim Sukharev <antreesy.web@gmail.com>
2025-03-27 12:51:58 +0100

    feat(conversationsStore): allow to create new conversation from one-to-one
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/utils/getDisplayName.ts
+++ src/utils/getDisplayName.ts
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import { t } from '@nextcloud/l10n'
+import { getCanonicalLocale, t } from '@nextcloud/l10n'
 
 import { ATTENDEE } from '../constants.ts'
 
@@ -19,4 +19,27 @@ export const getDisplayNameWithFallback = function(displayName: string, source: 
 	// - for matching type: `source === ATTENDEE.ACTOR_TYPE.DELETED_USERS`
 	// - in other cases: should not happen, but can not be empty either
 	return t('spreed', 'Deleted user')
+}
+
+export const getDisplayNamesList = function(displayNames: string[], maxLength: number): string {
+	const sanitizedList = displayNames.reduce<string[]>((acc, name) => {
+		if (name.trim()) {
+			acc.push(name.trim())
+		}
+		return acc
+	}, [])
+
+	if (!sanitizedList.length) {
+		return ''
+	}
+
+	const string = new Intl.ListFormat(getCanonicalLocale(), {
+		style: 'narrow',
+		type: 'conjunction',
+	}).format(sanitizedList)
+
+	if (string.length > maxLength) {
+		return string.substring(0, maxLength - 1) + '…'
+	}
+	return string
 }
