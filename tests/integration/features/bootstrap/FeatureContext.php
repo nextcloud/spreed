b249583bcac1df342782c9185e33f91343cf13ff
Joas Schilling <coding@schilljs.com>
2025-04-11 17:28:12 +0200

    test: Fix broken scenario
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/integration/features/bootstrap/FeatureContext.php
+++ tests/integration/features/bootstrap/FeatureContext.php
@@ -199,6 +199,7 @@ class FeatureContext implements Context, SnippetAcceptingContext {
 	public function setUp(BeforeScenarioScope $scope) {
 		self::$identifierToToken = [];
 		self::$identifierToId = [];
+		self::$botNameToId = [];
 		self::$tokenToIdentifier = [];
 		self::$sessionNameToActorId = [];
 		self::$sessionIdToUser = [
@@ -4949,7 +4950,7 @@ class FeatureContext implements Context, SnippetAcceptingContext {
 	}
 
 	/**
-	 * @When /^user "([^"]*)" set status to "([^"]*)" with (\d+)(?: \((v1)\))?$/
+	 * @When /^user "([^"]*)" sets? status to "([^"]*)" with (\d+)(?: \((v1)\))?$/
 	 */
 	public function setUserStatus(string $user, string $status, int $statusCode, string $apiVersion = 'v1'): void {
 		$this->setCurrentUser($user);
