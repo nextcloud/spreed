9411e9f00a22d482fbae6ccd75cb7b10aa7d60f0
Maksim Sukharev <antreesy.web@gmail.com>
2025-04-12 12:46:14 +0200

    feat: update participantsStore from internal signaling
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -6,7 +6,7 @@
 import { defineStore } from 'pinia'
 import Vue from 'vue'
 
-import { ATTENDEE } from '../constants.ts'
+import { ATTENDEE, PARTICIPANT } from '../constants.ts'
 import store from '../store/index.js'
 import type {
 	InternalSignalingSession,
@@ -28,6 +28,15 @@ type SignalingSessionPayload =
 	| InternalSignalingSession
 	| StandaloneSignalingJoinSession
 	| StandaloneSignalingUpdateSession
+
+type ParticipantUpdatePayload = {
+	displayName?: string,
+	inCall: number,
+	lastPing: number,
+	permissions: number,
+	participantType?: number,
+	sessionIds?: string[],
+}
 
 type State = {
 	sessions: Record<string, Session>,
@@ -179,7 +188,7 @@ export const useSessionStore = defineStore('session', {
 			}
 
 			if (isInternalSignalingSession(users)) {
-				// this.updateParticipantsFromInternalSignaling(token, users)
+				this.updateParticipantsFromInternalSignaling(token, users)
 
 				// Internal signaling server always returns all current sessions,
 				// so if some participants are missing - they are 'offline'
@@ -203,6 +212,59 @@ export const useSessionStore = defineStore('session', {
 			for (const sessionId of sessionIds) {
 				// this.updateParticipantLeftFromStandaloneSignaling(token, sessionId)
 				this.deleteSession(sessionId)
+			}
+		},
+
+		/**
+		 * Update participants in store according to data from internal signaling server
+		 *
+		 * @param token - Conversation token
+		 * @param users - Users payload from signaling message
+		 */
+		updateParticipantsFromInternalSignaling(token: string, users: InternalSignalingSession[]) {
+			const participantsToUpdate: Record<string, ParticipantUpdatePayload> = {}
+
+			for (const user of users) {
+				const session = this.getSession(user.sessionId)
+				this.updateSession(user.sessionId, { inCall: user.inCall })
+				const attendeeId = session?.attendeeId
+				if (!attendeeId) {
+					// Skip participant update
+					continue
+				}
+
+				if (!participantsToUpdate[attendeeId]) {
+					// Prepare payload to update object in participants store
+					participantsToUpdate[attendeeId] = {
+						inCall: user.inCall,
+						lastPing: user.lastPing,
+						permissions: user.participantPermissions,
+						sessionIds: [user.sessionId],
+					}
+				} else {
+					// Payload already exists, but participant also joined from another device
+					participantsToUpdate[attendeeId].sessionIds!.push(user.sessionId)
+					participantsToUpdate[attendeeId].inCall = participantsToUpdate[attendeeId].inCall | user.inCall
+					participantsToUpdate[attendeeId].lastPing = Math.max(participantsToUpdate[attendeeId].lastPing, user.lastPing)
+				}
+			}
+
+			for (const participant of store.getters.participantsList(token) as Participant[]) {
+				const { attendeeId, sessionIds } = participant
+				if (participantsToUpdate[attendeeId]) {
+					store.commit('updateParticipant', {
+						token,
+						attendeeId,
+						updatedData: participantsToUpdate[attendeeId],
+					})
+				} else if (sessionIds.length !== 0) {
+					// Participant left conversation from all devices, setting as 'offline'
+					store.commit('updateParticipant', {
+						token,
+						attendeeId,
+						updatedData: { inCall: PARTICIPANT.CALL_FLAG.DISCONNECTED, sessionIds: [] },
+					})
+				}
 			}
 		},
 	},
