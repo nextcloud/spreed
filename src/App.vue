b024fd04a612cfb524f1cc7ff574eaf367134b27
Maksim Sukharev <antreesy.web@gmail.com>
2025-05-05 17:09:46 +0200

    fix(RightSidebar): do not close sidebar in 1-1
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -153,23 +153,12 @@ export default {
 		currentConversation() {
 			return this.$store.getters.conversation(this.token)
 		},
-
-		/**
-		 * Computes whether the current conversation is one to one
-		 *
-		 * @return {boolean} The result
-		 */
-		isOneToOne() {
-			return this.currentConversation?.type === CONVERSATION.TYPE.ONE_TO_ONE
-				|| this.currentConversation?.type === CONVERSATION.TYPE.ONE_TO_ONE_FORMER
-		},
 	},
 
 	watch: {
 		token(newValue, oldValue) {
 			const shouldShowSidebar = BrowserStorage.getItem('sidebarOpen') !== 'false'
-			// Collapse the sidebar if it's a one to one conversation
-			if (this.isOneToOne || !shouldShowSidebar || this.isMobile) {
+			if (!shouldShowSidebar || this.isMobile) {
 				this.sidebarStore.hideSidebar({ cache: false })
 			} else if (shouldShowSidebar) {
 				this.sidebarStore.showSidebar({ cache: false })
