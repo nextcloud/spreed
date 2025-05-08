63bad9f108cb22d3b30aeb8f0d914178528b7e53
Dorra Jaouad <dorra.jaoued7@gmail.com>
2025-05-08 08:33:45 +0200

    feat: add loading and empty placeholders. Establish recurrent fetch for event rooms
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/components/Dashboard/TalkDashboard.vue
+++ src/components/Dashboard/TalkDashboard.vue
@@ -3,9 +3,11 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <script lang="ts" setup>
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, onBeforeUnmount, ref } from 'vue'
 import { useRouter } from 'vue-router/composables'
 
+import IconAt from 'vue-material-design-icons/At.vue'
+import IconCalendarBlank from 'vue-material-design-icons/CalendarBlank.vue'
 import IconMicrophone from 'vue-material-design-icons/Microphone.vue'
 import IconVideo from 'vue-material-design-icons/Video.vue'
 
@@ -14,9 +16,11 @@ import { emit } from '@nextcloud/event-bus'
 import { t } from '@nextcloud/l10n'
 
 import NcButton from '@nextcloud/vue/components/NcButton'
+import NcEmptyContent from '@nextcloud/vue/components/NcEmptyContent'
 
 import EventCard from './EventCard.vue'
 import ConversationsListVirtual from '../LeftSidebar/ConversationsList/ConversationsListVirtual.vue'
+import LoadingPlaceholder from '../UIShared/LoadingPlaceholder.vue'
 
 import { useStore } from '../../composables/useStore.js'
 import { CONVERSATION } from '../../constants.ts'
@@ -24,12 +28,23 @@ import { useTalkDashboardStore } from '../../stores/talkdashboard.ts'
 import type { Conversation } from '../../types/index.ts'
 import { filterConversation } from '../../utils/conversation.ts'
 
+const FIVE_MINUTES = 5 * 60 * 1000 // 5 minutes
 const store = useStore()
 const router = useRouter()
 const talkDashboardStore = useTalkDashboardStore()
+
 const eventRooms = computed(() => talkDashboardStore.eventrooms)
+const eventsInitialised = computed(() => talkDashboardStore.eventRoomsInitialised)
+const fetchEventRoomsInterval = ref<ReturnType<typeof setInterval> | null>(null)
 onMounted(async () => {
-	await talkDashboardStore.fetchDashboardEventRooms()
+	fetchEventRooms()
+	fetchEventRoomsInterval.value = setInterval(fetchEventRooms, FIVE_MINUTES)
+})
+
+onBeforeUnmount(() => {
+	if (fetchEventRoomsInterval.value) {
+		clearInterval(fetchEventRoomsInterval.value)
+	}
 })
 
 const conversationsList = computed(() => store.getters.conversationsList)
@@ -37,6 +52,17 @@ const conversationsInitialised = computed(() => store.getters.conversationsIniti
 const filteredConversations = computed(() => conversationsList.value?.filter((conversation : Conversation) => {
 	return filterConversation(conversation, ['mentions'])
 }))
+
+/**
+ * Fetches the event rooms
+ */
+async function fetchEventRooms() {
+	try {
+		await talkDashboardStore.fetchDashboardEventRooms()
+	} catch (error) {
+		showError(t('spreed', 'Error fetching event rooms'))
+	}
+}
 
 /**
  * Creates a new group conversation and navigates to the conversation page.
@@ -75,12 +101,23 @@ async function startMeeting() {
 				{{ t('spreed', 'Start meeting now') }}
 			</NcButton>
 		</div>
-		<div class="talk-dashboard__event-cards">
+		<div v-if="eventsInitialised && eventRooms.length > 0" class="talk-dashboard__event-cards">
 			<EventCard v-for="eventRoom in eventRooms"
 				:key="eventRoom.eventLink"
 				:event-room="eventRoom"
 				class="talk-dashboard__event-card" />
 		</div>
+		<LoadingPlaceholder v-else-if="!eventsInitialised"
+			class="event-cards__loading-placeholder"
+			type="event-cards" />
+		<NcEmptyContent v-else
+			class="talk-dashboard__empty-content"
+			:name="t('spreed', 'No upcoming meetings')"
+			:description="t('spreed', 'You have no upcoming meetings scheduled in the next 7 days')">
+			<template #icon>
+				<IconCalendarBlank :size="40" />
+			</template>
+		</NcEmptyContent>
 		<NcButton class="talk-dashboard__devices-button"
 			type="tertiary"
 			@click="emit('talk:media-settings:show', 'device-check')">
@@ -92,9 +129,18 @@ async function startMeeting() {
 		<div class="talk-dashboard__unread-mentions"
 			:class="{'loading': !conversationsInitialised}">
 			<span class="title">{{ t('spreed', 'Unread mentions') }}</span>
-			<ConversationsListVirtual class="talk-dashboard__conversations-list"
+			<ConversationsListVirtual v-if="filteredConversations.length > 0 || !conversationsInitialised"
+				class="talk-dashboard__conversations-list"
 				:conversations="filteredConversations"
 				:loading="!conversationsInitialised" />
+			<NcEmptyContent v-else
+				class="talk-dashboard__empty-content"
+				:name="t('spreed', 'All caught up!')"
+				:description="t('spreed', 'You have no unread mentions')">
+				<template #icon>
+					<IconAt :size="40" />
+				</template>
+			</NcEmptyContent>
 		</div>
 	</div>
 </template>
@@ -157,6 +203,13 @@ async function startMeeting() {
 	&.loading {
 		overflow: hidden;
 	}
+}
+
+.talk-dashboard__empty-content {
+	background-color: var(--color-background-dark);
+	border-radius: var(--border-radius);
+	padding: calc(var(--default-grid-baseline) * 2);
+	margin: var(--default-grid-baseline) calc(var(--default-grid-baseline) * 4);
 }
 
 .talk-dashboard__conversations-list {
