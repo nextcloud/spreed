fa1754f41c5feaffddf8158d9b445c94d0c515f0
Dorra Jaouad <dorra.jaoued7@gmail.com>
2025-05-13 14:03:51 +0200

    feat(reminders): add reminders section in the dashboard
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/stores/talkdashboard.ts
+++ src/stores/talkdashboard.ts
@@ -6,17 +6,25 @@
 import { defineStore } from 'pinia'
 import Vue from 'vue'
 
+import { hasTalkFeature } from '../services/CapabilitiesManager.ts'
 import { getDashboardEventRooms } from '../services/dashboardService.ts'
-import type { DashboardEventRoom } from '../types/index.ts'
+import { getUpcomingReminders } from '../services/remindersService.js'
+import type { DashboardEventRoom, UpcomingReminder } from '../types/index.ts'
+
+const supportsUpcomingReminders = hasTalkFeature('local', 'upcoming-reminders')
 
 type State = {
 	eventrooms: DashboardEventRoom[],
+	upcomingReminders: UpcomingReminder[],
 	eventRoomsInitialised: boolean
+	upcomingRemindersInitialised: boolean,
 }
 export const useTalkDashboardStore = defineStore('talkdashboard', {
 	state: (): State => ({
 		eventrooms: [],
+		upcomingReminders: [],
 		eventRoomsInitialised: false,
+		upcomingRemindersInitialised: false,
 	}),
 
 	actions: {
@@ -30,5 +38,19 @@ export const useTalkDashboardStore = defineStore('talkdashboard', {
 				throw error
 			}
 		},
+
+		async fetchUpcomingReminders() {
+			try {
+				if (!supportsUpcomingReminders) {
+					return []
+				}
+				const response = await getUpcomingReminders()
+				Vue.set(this, 'upcomingReminders', response.data.ocs.data)
+				this.upcomingRemindersInitialised = true
+			} catch (error) {
+				console.error('Error fetching upcoming reminders:', error)
+				throw error
+			}
+		}
 	},
 })
