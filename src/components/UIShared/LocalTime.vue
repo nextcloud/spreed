bbd7f5653991b4c628a943b5a5a04dd87fa2a125
Maksim Sukharev <antreesy.web@gmail.com>
2025-05-16 12:14:28 +0200

    fix(RightSidebar): make local time reactive
    - introduce shared composable for Date() object
    - introduce shared component for showing the date
    
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

---
+++ src/components/UIShared/LocalTime.vue
@@ -0,0 +1,33 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { t, getCanonicalLocale } from '@nextcloud/l10n'
+
+import { useCurrentTime } from '../../composables/useCurrentTime.ts'
+
+const props = defineProps<{
+	timezone: string,
+}>()
+
+const currentTime = useCurrentTime()
+
+const time = computed(() => t('spreed', 'Local time: {time}', {
+	time: Intl.DateTimeFormat(getCanonicalLocale(), {
+		timeZone: props.timezone,
+		hour: '2-digit',
+		minute: '2-digit',
+	}).format(currentTime.value),
+}))
+</script>
+
+<template>
+	<span>
+		<slot name="icon" />
+		{{ time }}
+	</span>
+</template>
