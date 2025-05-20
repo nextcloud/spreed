1716becf0569cbba1a657d88d92398e51980bdcd
Joas Schilling <coding@schilljs.com>
2025-05-20 10:02:58 +0200

    chore(tests): Cleanup bootstrap.php to be forward-compatible
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -1,16 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+use OCP\App\IAppManager;
+use OCP\Server;
+
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
+
 require_once __DIR__ . '/../../../../lib/base.php';
-\OC::$loader->addValidRoot(\OC::$SERVERROOT . '/tests');
-\OC_App::loadApp('spreed');
-if (!class_exists('\PHPUnit\Framework\TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
-\OC_Hook::clear();
+require_once __DIR__ . '/../../../../tests/autoload.php';
+
+Server::get(IAppManager::class)->loadApp('spreed');
