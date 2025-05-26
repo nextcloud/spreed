206a05d6ce037b4558a7dc74976944c7279f5a90
Anna Larch <anna@nextcloud.com>
2025-05-26 12:33:58 +0200

    fix(conversations): remove unused db field
    Signed-off-by: Anna Larch <anna@nextcloud.com>

---
+++ lib/Migration/Version22000Date20250526102909.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Talk\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version22000Date20250526102909 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	#[\Override]
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$table = $schema->getTable('talk_rooms');
+		if ($table->hasColumn('active_guests')) {
+			$table->dropColumn('active_guests');
+		}
+
+		return $schema;
+	}
+}
