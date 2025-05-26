03d6483e4da932592eea9661449dbeec238b7967
Maksim Sukharev <antreesy.web@gmail.com>
2025-05-26 11:08:19 +0200

    fix: apply 'no-restricted-imports' rule
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,7 +32,6 @@ export default [
 			'no-constant-condition': 'off', // non-fixable
 			'no-empty': 'off', // non-fixable
 			'no-redeclare': 'off', // non-fixable
-			'no-restricted-imports': 'off', // non-fixable
 			'no-undef': 'off', // non-fixable
 			'no-unused-vars': 'off', // non-fixable
 			'no-use-before-define': 'off', // non-fixable
