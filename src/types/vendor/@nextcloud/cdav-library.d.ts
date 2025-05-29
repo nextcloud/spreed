69a08c5026b7427cd578bff659c99ee08a1efdc1
Maksim Sukharev <antreesy.web@gmail.com>
2025-05-29 11:41:23 +0200

    fix(CalDavClient): drop xhr provider logic
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/types/vendor/@nextcloud/cdav-library.d.ts
+++ src/types/vendor/@nextcloud/cdav-library.d.ts
@@ -7,8 +7,8 @@ declare module '@nextcloud/cdav-library'
 
 declare class DavClient {
 
-	constructor(options: { rootUrl: string });
-	connect(options: { enableCalDAV: boolean }): Promise<void>;
+	constructor(options: { rootUrl: string, defaultHeaders: Record<string, any> }, factories = {});
+	connect(options?: { enableCalDAV?: boolean, enableCardDAV?: boolean }): Promise<void>;
 
 	currentUserPrincipal: DavPrincipal
 	calendarHomes: DavCalendarHome[]
