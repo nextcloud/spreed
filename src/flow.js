2437bc941589681211e8223174118d97426c6228
Maksim Sukharev <antreesy.web@gmail.com>
2025-06-23 09:57:14 +0200

    chore(vue3): migrate custom component registration
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/flow.js
+++ src/flow.js
@@ -3,18 +3,14 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import wrap from '@vue/web-component-wrapper'
-import Vue from 'vue'
+import { defineCustomElement } from 'vue'
 import FlowPostToConversation from './views/FlowPostToConversation.vue'
 
-const FlowPostToConversationComponent = wrap(Vue, FlowPostToConversation)
+const FlowPostToConversationComponent = defineCustomElement(FlowPostToConversation, {
+	shadowRoot: false,
+})
 const customElementId = 'oca-spreed-flow_post_to_conversation'
 window.customElements.define(customElementId, FlowPostToConversationComponent)
-
-// In Vue 2, wrap doesn't support disabling shadow :(
-// Disable with a hack
-Object.defineProperty(FlowPostToConversationComponent.prototype, 'attachShadow', { value() { return this } })
-Object.defineProperty(FlowPostToConversationComponent.prototype, 'shadowRoot', { get() { return this } })
 
 window.OCA.WorkflowEngine.registerOperator({
 	id: 'OCA\\Talk\\Flow\\Operation',
