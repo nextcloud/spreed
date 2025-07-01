937adf08e192917c2baa117c357c6c34160452db
Grigorii K. Shartsev <me@shgk.me>
2025-07-01 16:53:26 +0200

    fix(vue3): bring uploader back
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/store/fileUploadStore.spec.js
+++ src/store/fileUploadStore.spec.js
@@ -1,5 +1,5 @@
 import { showError } from '@nextcloud/dialogs'
-// import { getUploader } from '@nextcloud/upload'
+import { getUploader } from '@nextcloud/upload'
 /**
  * SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
