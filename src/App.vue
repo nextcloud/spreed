df4444d6eb1b36fe2b76dc84967a7d40029893e3
Daniel Calviño Sánchez <danxuliu@gmail.com>
2025-07-08 07:38:45 +0200

    fix: Redirect to "notfound" only when the conversation was not found
    This prevents redirections, for example, if the request failed due to a
    temporary internal server error.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@
 
 <script>
 import { getCurrentUser } from '@nextcloud/auth'
+import { showError } from '@nextcloud/dialogs'
 import { emit, subscribe, unsubscribe } from '@nextcloud/event-bus'
 import { t } from '@nextcloud/l10n'
 import { generateUrl } from '@nextcloud/router'
@@ -595,8 +596,13 @@ export default {
 				 */
 				EventBus.emit('conversations-received', { singleConversation: response.data.ocs.data })
 			} catch (exception) {
-				console.info('Conversation received, but the current conversation is not in the list. Redirecting to /apps/spreed')
-				this.$router.push({ name: 'notfound', params: { skipLeaveWarning: true } })
+				if (exception.response?.status === 404) {
+					console.info('Conversation received, but the current conversation is not in the list. Redirecting to /apps/spreed')
+					this.$router.push({ name: 'notfound', params: { skipLeaveWarning: true } })
+				} else {
+					console.error('Error getting room data', exception)
+					showError(t('spreed', 'Error occurred when getting the conversation information'))
+				}
 			} finally {
 				this.isRefreshingCurrentConversation = false
 			}
