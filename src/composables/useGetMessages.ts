27b1e3a883a6222673676e2c16d7ed3e1edb0ce7
Maksim Sukharev <antreesy.web@gmail.com>
2025-07-19 01:57:26 +0200

    fix(useGetMessages): refactor methods
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/composables/useGetMessages.ts
+++ src/composables/useGetMessages.ts
@@ -90,7 +90,12 @@ export function useGetMessagesProvider() {
 		if (oldValue) {
 			store.dispatch('cancelPollNewMessages', { requestId: oldValue })
 		}
-		handleStartGettingMessagesPreconditions(currentToken.value)
+		if (currentToken.value && isParticipant.value && !isInLobby.value) {
+			const focusMessageId = route?.hash?.startsWith('#message_') ? parseInt(route.hash.slice(9), 10) : null
+			handleStartGettingMessagesPreconditions(currentToken.value, focusMessageId)
+		} else {
+			store.dispatch('cancelPollNewMessages', { requestId: newValue })
+		}
 
 		/** Remove expired messages when joining a room */
 		store.dispatch('removeExpiredMessages', { token: currentToken.value })
@@ -119,61 +124,49 @@ export function useGetMessagesProvider() {
 	 * Stop polling due to offline
 	 */
 	function handleNetworkOffline() {
-		console.debug('Canceling message request as we are offline')
-		store.dispatch('cancelPollNewMessages', { requestId: chatIdentifier.value })
+		if (currentToken.value) {
+			console.debug('Canceling message request as we are offline')
+			store.dispatch('cancelPollNewMessages', { requestId: chatIdentifier.value })
+		}
 	}
 
 	/**
 	 * Resume polling, when back online
 	 */
 	function handleNetworkOnline() {
-		console.debug('Restarting polling of new chat messages')
-		pollNewMessages(currentToken.value)
+		if (currentToken.value) {
+			console.debug('Restarting polling of new chat messages')
+			pollNewMessages(currentToken.value)
+		}
 	}
 
 	/**
 	 * Handle route changes to initialize chat or thread, and focus given message
 	 */
 	async function onRouteChange({ from, to }: { from: RouteLocation, to: RouteLocation }) {
+		const focusMessageId = to.hash && to.hash.startsWith('#message_') ? parseInt(to.hash.slice(9), 10) : null
+
 		if (from.name === 'conversation' && to.name === 'conversation'
 			&& from.params.token === to.params.token && typeof to.params.token === 'string') {
-			if (to.hash && to.hash.startsWith('#message_') && from.hash !== to.hash) {
+			if (from.hash !== to.hash && focusMessageId !== null) {
 				// the hash changed, need to focus/highlight another message
-				const focusedId = parseInt(to.hash.slice(9), 10)
-				if (store.getters.messagesList(to.params.token).find((message: ChatMessage) => message.id === focusedId)) {
-					// need some delay (next tick is too short) to be able to run
-					// after the browser's native "scroll to anchor" from
-					// the hash
-					window.setTimeout(() => {
-						// scroll to message in URL anchor
-						EventBus.emit('focus-message', focusedId)
-					}, 2)
-				} else {
-					// Update environment around context to fill the gaps
-					store.dispatch('setFirstKnownMessageId', {
-						token: to.params.token,
-						id: focusedId,
-					})
-					store.dispatch('setLastKnownMessageId', {
-						token: to.params.token,
-						id: focusedId,
-					})
-					await getMessageContext(to.params.token, focusedId)
-					EventBus.emit('focus-message', focusedId)
+				const hasMessageInStore = ('id' in (store.getters.message(to.params.token, focusMessageId) as ChatMessage | Record<string, never>))
+				if (!hasMessageInStore) {
+					// message not found in the list, need to fetch it first
+					await getMessageContext(to.params.token, focusMessageId)
 				}
-			} else if (to.query.threadId && from.query.threadId !== to.query.threadId) {
+				// need some delay (next tick is too short) to be able to run
+				// after the browser's native "scroll to anchor" from the hash
+				window.setTimeout(() => {
+					EventBus.emit('focus-message', focusMessageId)
+				}, 2)
+				return
+			}
+
+			if (to.query.threadId && from.query.threadId !== to.query.threadId) {
 				// FIXME temporary get thread messages from the start
-				const topMostThreadMessage = store.getters.messagesList(to.params.token).find((message: ChatMessage) => message.id === +to.query.threadId)
-				if (!topMostThreadMessage) {
-					// Update environment around context to fill the gaps
-					store.dispatch('setFirstKnownMessageId', {
-						token: to.params.token,
-						id: +to.query.threadId,
-					})
-					store.dispatch('setLastKnownMessageId', {
-						token: to.params.token,
-						id: +to.query.threadId,
-					})
+				const hasMessageInStore = ('id' in (store.getters.message(to.params.token, to.query.threadId) as ChatMessage | Record<string, never>))
+				if (!hasMessageInStore) {
 					await getMessageContext(to.params.token, +to.query.threadId)
 				}
 			}
@@ -183,51 +176,43 @@ export function useGetMessagesProvider() {
 	/**
 	 * Initialize chat context borders and start fetching messages
 	 * @param token token of conversation where a method was called
+	 * @param focusMessageId message id to get context around (null if not specified)
 	 */
-	async function handleStartGettingMessagesPreconditions(token: string) {
-		if (token && isParticipant.value && !isInLobby.value) {
-			// prevent sticky mode before we have loaded anything
-			isInitialisingMessages.value = true
-			const focusMessageId = route?.hash?.startsWith('#message_') ? parseInt(route.hash.slice(9), 10) : null
+	async function handleStartGettingMessagesPreconditions(token: string, focusMessageId: number | null) {
+		// prevent sticky mode before we have loaded anything
+		isInitialisingMessages.value = true
 
-			store.dispatch('setVisualLastReadMessageId', { token, id: conversation.value!.lastReadMessage })
+		store.dispatch('setVisualLastReadMessageId', { token, id: conversation.value!.lastReadMessage })
 
-			if (!store.getters.getFirstKnownMessageId(token)) {
-				try {
-					// Start from message hash or unread marker
-					let startingMessageId = focusMessageId !== null ? focusMessageId : conversation.value!.lastReadMessage
-					// Check if thread is initially opened
-					if (threadId.value) {
-						// FIXME temporary get thread messages from the start
-						startingMessageId = threadId.value
-					}
-
-					// First time load, initialize important properties
-					if (!startingMessageId) {
-						throw new Error(`[DEBUG] spreed: context message ID is ${startingMessageId}`)
-					}
-					store.dispatch('setFirstKnownMessageId', { token, id: startingMessageId })
-					store.dispatch('setLastKnownMessageId', { token, id: startingMessageId })
-					// If MESSAGE.CHAT_BEGIN_ID we need to get the context from the beginning
-					// using 0 as the API does not support negative values
-					// Get chat messages before last read message and after it
-					await getMessageContext(token, startingMessageId !== MESSAGE.CHAT_BEGIN_ID ? startingMessageId : 0)
-				} catch (exception) {
-					console.debug(exception)
-					// Request was cancelled, stop getting preconditions and restore initial state
-					store.dispatch('setFirstKnownMessageId', { token, id: null })
-					store.dispatch('setLastKnownMessageId', { token, id: null })
-					return
+		if (!store.getters.getFirstKnownMessageId(token)) {
+			try {
+				// Start from message hash or unread marker
+				let startingMessageId = focusMessageId !== null ? focusMessageId : conversation.value!.lastReadMessage
+				// Check if thread is initially opened
+				if (threadId.value) {
+					// FIXME temporary get thread messages from the start
+					startingMessageId = threadId.value
 				}
+
+				// First time load, initialize important properties
+				if (!startingMessageId) {
+					throw new Error(`[DEBUG] spreed: context message ID is ${startingMessageId}`)
+				}
+
+				await getMessageContext(token, startingMessageId)
+			} catch (exception) {
+				console.debug(exception)
+				// Request was cancelled, stop getting preconditions and restore initial state
+				store.dispatch('setFirstKnownMessageId', { token, id: null })
+				store.dispatch('setLastKnownMessageId', { token, id: null })
+				return
 			}
-
-			isInitialisingMessages.value = false
-
-			// Once the history is received, starts looking for new messages.
-			await pollNewMessages(token)
-		} else {
-			store.dispatch('cancelPollNewMessages', { requestId: chatIdentifier.value })
 		}
+
+		isInitialisingMessages.value = false
+
+		// Once the history is received, starts looking for new messages.
+		await pollNewMessages(token)
 	}
 
 	/**
@@ -237,13 +222,19 @@ export function useGetMessagesProvider() {
 	 * @param messageId messageId
 	 */
 	async function getMessageContext(token: string, messageId: number) {
-		// Make the request
 		loadingOldMessages.value = true
 		try {
 			debugTimer.start(`${token} | get context`)
+			// Update environment around context
+			await store.dispatch('setFirstKnownMessageId', { token, id: messageId })
+			await store.dispatch('setLastKnownMessageId', { token, id: messageId })
+			// Make the request
 			await store.dispatch('getMessageContext', {
 				token,
-				messageId,
+				// If MESSAGE.CHAT_BEGIN_ID we need to get the context from the beginning
+				// using 0 as the API does not support negative values
+				// Get chat messages before last read message and after it
+				messageId: messageId !== MESSAGE.CHAT_BEGIN_ID ? messageId : 0,
 				minimumVisible: CHAT.MINIMUM_VISIBLE,
 			})
 			debugTimer.end(`${token} | get context`, 'status 200')
