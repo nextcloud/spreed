823934912a75969a6f63e51219c1a2088fbc6bd8
Dorra Jaouad <dorra.jaoued7@gmail.com>
2025-08-05 19:21:46 +0200

    fix(fullscreen): enable fullscreen in dashboard and fix disabling fullscreen outside conversation.
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/composables/useDocumentFullscreen.ts
+++ src/composables/useDocumentFullscreen.ts
@@ -3,18 +3,19 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import type { DeepReadonly, Ref } from 'vue'
-
+import { showWarning } from '@nextcloud/dialogs'
+import { emit } from '@nextcloud/event-bus'
+import { t } from '@nextcloud/l10n'
 import { createSharedComposable } from '@vueuse/core'
-import { onBeforeMount, onBeforeUnmount, readonly, ref } from 'vue'
+import { onBeforeUnmount, readonly, ref } from 'vue'
+
+const isFullscreen = ref<boolean>(document.fullscreenElement !== null)
 
 /**
  * Composable to check whether the page is displayed at fullscreen
  * @return {DeepReadonly<Ref<boolean>>} - computed boolean whether the page is displayed at fullscreen
  */
 function useDocumentFullscreenComposable() {
-	const isFullscreen = ref<boolean>(document.fullscreenElement !== null)
-
 	const changeIsFullscreen = () => {
 		isFullscreen.value = document.fullscreenElement !== null
 
@@ -39,7 +40,19 @@ function useDocumentFullscreenComposable() {
 /**
  * Enable a fullscreen with Fullscreen API
  */
-export async function enableFullscreen() {
+async function enableFullscreen() {
+	// Don't toggle fullscreen if there is an open modal
+	// FIXME won't be needed without Fulscreen API
+	if (Array.from(document.body.children).filter((child) => {
+		return child.nodeName === 'DIV' && child.classList.contains('modal-mask')
+			&& window.getComputedStyle(child).display !== 'none'
+	}).length !== 0) {
+		showWarning(t('spreed', 'You need to close a dialog to toggle full screen'))
+		return
+	}
+
+	emit('toggle-navigation', { open: false })
+
 	if (document.body.requestFullscreen) {
 		await document.body.requestFullscreen()
 	} else if (document.body.webkitRequestFullscreen) {
@@ -50,11 +63,24 @@ export async function enableFullscreen() {
 /**
  * Disable a fullscreen
  */
-export async function disableFullscreen() {
+async function disableFullscreen() {
 	if (document.exitFullscreen) {
 		await document.exitFullscreen()
 	} else if (document.webkitExitFullscreen) {
 		await document.webkitExitFullscreen()
+	}
+}
+
+/**
+ * Toggles the full screen mode of the call view.
+ * If the sidebar is open, it does nothing.
+ * If there is an open modal, it shows a warning.
+ */
+export function toggleFullscreen() {
+	if (isFullscreen.value) {
+		disableFullscreen()
+	} else {
+		enableFullscreen()
 	}
 }
 
