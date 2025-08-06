04a7408d4542dad9da41ce41e8f3b875edc9ae1c
Dorra Jaouad <dorra.jaoued7@gmail.com>
2025-08-06 13:32:55 +0200

    fix(Grid): highlight own video when it is one to one
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/components/CallView/Grid/Grid.vue
+++ src/components/CallView/Grid/Grid.vue
@@ -100,6 +100,7 @@
 				<LocalVideo v-if="isStripe && !isRecording"
 					ref="localVideo"
 					class="video"
+					:class="{ 'local-video--highlighted': isLessThanTwoVideos }"
 					:is-stripe="true"
 					:show-controls="false"
 					:token="token"
@@ -1127,6 +1128,10 @@ export default {
 		/* needed again to override default active button style */
 		background: none;
 	}
+}
+
+.local-video--highlighted {
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 
 </style>
