0258fe5ea990700f60fbb0194e893ca458a0d85f
Louis Chemineau <louis@chmn.me>
2025-09-09 17:59:36 +0200

    fix: Remove unread messages notification
    This is unnecessarily spamming the view when the chat tab is closed.
    Also, this is kind of a duplicate with the blue dot on the message icon.
    
    Signed-off-by: Louis <louis@chmn.me>

--- src/components/RightSidebar/RightSidebar.vue
+++ src/components/RightSidebar/RightSidebar.vue
@@ -427,17 +427,6 @@ export default {
 			},
 		},
 
-		unreadMessagesCounter(newValue, oldValue) {
-			if (!this.isInCall || this.opened) {
-				return
-			}
-
-			// new messages arrived
-			if (newValue > 0 && oldValue === 0 && !this.hasUnreadMentions) {
-				this.notifyUnreadMessages(t('spreed', 'You have new unread messages in the chat.'))
-			}
-		},
-
 		hasUnreadMentions(newValue) {
 			if (!this.isInCall || this.opened) {
 				return
