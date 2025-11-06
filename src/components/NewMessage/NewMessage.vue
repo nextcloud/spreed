0e782f26b9aecca3d1f90fa4339a78ca83df8539
Maksim Sukharev <antreesy.web@gmail.com>
2025-11-06 13:34:14 +0100

    fix(NewMessage): limit 'set cursor to the end' to editing
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/NewMessage/NewMessage.vue
+++ src/components/NewMessage/NewMessage.vue
@@ -582,6 +582,11 @@ export default {
 			} else {
 				this.text = this.chatInput
 			}
+
+			this.$nextTick(() => {
+				// set cursor at the end
+				selectRange(getRangeAtEnd(this.getContenteditable()), this.getContenteditable())
+			})
 		},
 
 		parentMessage(newValue) {
@@ -978,8 +983,7 @@ export default {
 		},
 
 		restoreSelectionRange() {
-			// If nothing to restore, set cursor at the end
-			selectRange(this.preservedSelectionRange ?? getRangeAtEnd(this.getContenteditable()), this.getContenteditable())
+			selectRange(this.preservedSelectionRange, this.getContenteditable())
 			this.preservedSelectionRange = null
 		},
 
