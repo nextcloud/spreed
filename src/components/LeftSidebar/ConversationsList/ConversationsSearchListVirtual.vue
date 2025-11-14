b4116ca349b76f87e103cb5eda5ae2c9dd0bcb7f
Maksim Sukharev <antreesy.web@gmail.com>
2025-11-14 15:57:08 +0100

    fix(ConversationsSearchListVirtual): migrate to useVirtualList from vueuse/core
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/LeftSidebar/ConversationsList/ConversationsSearchListVirtual.vue
+++ src/components/LeftSidebar/ConversationsList/ConversationsSearchListVirtual.vue
@@ -3,65 +3,55 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
-<template>
-	<RecycleScroller
-		ref="scroller"
-		item-tag="ul"
-		:items="conversations"
-		:item-size="CONVERSATION_ITEM_SIZE"
-		key-field="token">
-		<template #default="{ item }">
-			<ConversationSearchResult :item="item" @click="onClick" />
-		</template>
-		<template #after>
-			<LoadingPlaceholder v-if="loading" type="conversations" />
-		</template>
-	</RecycleScroller>
-</template>
+<script setup lang="ts">
+import type { Conversation } from '../../../types/index.ts'
 
-<script>
-import { RecycleScroller } from 'vue-virtual-scroller'
+import { useVirtualList } from '@vueuse/core'
+import { toRef } from 'vue'
 import LoadingPlaceholder from '../../UIShared/LoadingPlaceholder.vue'
 import ConversationSearchResult from './ConversationSearchResult.vue'
 import { AVATAR } from '../../../constants.ts'
 
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+const props = defineProps<{
+	conversations: Conversation[]
+	loading?: boolean
+}>()
 
-const CONVERSATION_ITEM_SIZE = AVATAR.SIZE.DEFAULT + 2 * 4 + 2 * 2
+const emit = defineEmits<{
+	(event: 'select', item: Conversation): void
+}>()
 
-export default {
-	name: 'ConversationsSearchListVirtual',
+const itemHeight = AVATAR.SIZE.DEFAULT + 2 * 4 + 2 * 2
 
-	components: {
-		LoadingPlaceholder,
-		ConversationSearchResult,
-		RecycleScroller,
-	},
+const { list, containerProps, wrapperProps } = useVirtualList<Conversation>(toRef(() => props.conversations), {
+	itemHeight,
+	overscan: 10,
+})
 
-	props: {
-		conversations: {
-			type: Array,
-			required: true,
-		},
-
-		loading: {
-			type: Boolean,
-			default: false,
-		},
-	},
-
-	emits: ['select'],
-
-	setup() {
-		return {
-			CONVERSATION_ITEM_SIZE,
-		}
-	},
-
-	methods: {
-		onClick(item) {
-			this.$emit('select', item)
-		},
-	},
+/**
+ * Pass selected conversation to parent component
+ *
+ * @param item - selected conversation
+ */
+function handleClick(item: Conversation) {
+	emit('select', item)
 }
 </script>
+
+<template>
+	<li
+		:ref="containerProps.ref"
+		:style="containerProps.style"
+		@scroll="containerProps.onScroll">
+		<LoadingPlaceholder v-if="loading" type="conversations" />
+		<ul
+			v-else
+			:style="wrapperProps.style">
+			<ConversationSearchResult
+				v-for="item in list"
+				:key="item.data.id"
+				:item="item.data"
+				@click="handleClick" />
+		</ul>
+	</li>
+</template>
