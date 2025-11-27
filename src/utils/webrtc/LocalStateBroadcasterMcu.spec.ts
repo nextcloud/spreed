9cafada6993732d8ad349b5c4adb63033c8731c2
Daniel Calviño Sánchez <danxuliu@gmail.com>
2025-11-27 05:43:47 +0100

    fix: Send initial media state in the LocalStateBroadcaster
    Besides changing where they are sent in the code the new code also
    reduces the number of signaling messages, as now it sends them directly
    to each participant rather than to every participant. Moreover, when
    Janus is used, the state is sent now when the remote participant joins
    the call rather than when a connection was established with that
    participant (or joined, if that participant was not a publisher, as in
    that case no connection was established with it). Sending the state when
    a connection was established with the remote participant did not
    guarantee in any way that the signaling message would be received by the
    remote participant (hence the repeated sending with an exponential
    backoff), so sending it instead when the participant joins has
    essentially the same result (as the repeated sending with an exponential
    backoff is kept), but simplifies the code and makes more sense
    conceptually (as before the state of the publisher was sent when a
    connection with a subscriber was established). Similarly, when Janus is
    not used the state is sent now the first time that a connection is
    established with the remote participant, but not on every change to the
    connected state, as doing it only the first time is actually needed.
    
    Data channel messages are also sent now directly to the other
    participant when Janus is not used. However, when Janus is used they are
    still sent to all participants, as Janus does not support sending data
    channel messages to a specific participant. Unfortunately that means
    that data channel messages are still repeated for every participant in
    the call when another remote participant joins it. This is specially bad
    when the local participant joins a call, as in that case all the remote
    participants joined at the same time from the point of view of the local
    participant.
    
    Independently of that, note that when Janus is used the media state is
    always sent even if the local participant is not publishing, but if
    Janus is not used it will not be sent if neither the local participant
    nor the remote participant are publishing. While it should be possible
    to not sent the media state in all cases if the local participant is not
    publishing (because the audio and video will be implicitly disabled in
    that case) this is not properly handled by the iOS client yet, so for
    now the media state is still sent in that case.
    
    Note that this means that without Janus and when neither the local nor
    remote participants are publishing the iOS client will show the audio as
    available. In the previous code this was less problematic, because as
    the media state was sent to all participants, the media state could be
    received by a remote participant (through a signaling message) even if
    neither the remote participant nor the local participant was publishing
    if the local participant established a connection with a different
    remote participant that was publishing. This is no longer the case, but
    is nevertheless something to be fixed in the iOS client (and not a big
    deal anyway, as limited publishing permissions are unlikely to be used
    in a setup without Janus).
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

---
+++ src/utils/webrtc/LocalStateBroadcasterMcu.spec.ts
@@ -0,0 +1,318 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type {
+	InternalWebRtc,
+	WebRtc,
+} from '../../types/index.ts'
+
+import {
+	afterEach,
+	beforeEach,
+	describe,
+	expect,
+	test,
+	vi,
+} from 'vitest'
+import WildEmitter from 'wildemitter'
+import { LocalStateBroadcasterMcu } from './LocalStateBroadcasterMcu.ts'
+import { CallParticipantCollection } from './models/CallParticipantCollection.js'
+
+describe('LocalStateBroadcasterMcu', () => {
+	let webRtc: WebRtc
+	let internalWebRtc: InternalWebRtc
+	let callParticipantCollection: CallParticipantCollection
+
+	let localStateBroadcasterMcu: LocalStateBroadcasterMcu
+
+	beforeEach(() => {
+		vi.useFakeTimers()
+
+		internalWebRtc = new (function(this: InternalWebRtc) {
+			this.isAudioEnabled = vi.fn()
+			this.isSpeaking = vi.fn()
+			this.isVideoEnabled = vi.fn()
+		} as any)()
+
+		webRtc = new (function(this: WebRtc) {
+			WildEmitter.mixin(this)
+
+			this.webrtc = internalWebRtc
+
+			this.sendDataChannelToAll = vi.fn()
+
+			this.sendTo = vi.fn()
+		} as any)()
+
+		callParticipantCollection = new CallParticipantCollection()
+	})
+
+	afterEach(() => {
+		vi.clearAllMocks()
+	})
+
+	test('add single participant', () => {
+		vi.mocked(internalWebRtc.isAudioEnabled).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isSpeaking).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isVideoEnabled).mockReturnValue(true)
+
+		localStateBroadcasterMcu = new LocalStateBroadcasterMcu(webRtc, callParticipantCollection)
+
+		callParticipantCollection.add({ peerId: 'thePeerId', webRtc })
+
+		vi.advanceTimersByTime(0)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(3)
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(1, 'status', 'audioOn')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(2, 'status', 'speaking')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(3, 'status', 'videoOn')
+
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(2)
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(1, 'thePeerId', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(2, 'thePeerId', 'unmute', { name: 'video' })
+
+		let timeoutCount = 1
+
+		// Test after 1, 2, 4, 8 and 16 seconds have passed since the
+		// participant was added
+		const timeouts = [1, 2, 4, 8, 16]
+		timeouts.forEach((second) => {
+			vi.advanceTimersByTime(second * 1000 - 1)
+
+			expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(3 * timeoutCount)
+			expect(webRtc.sendTo).toHaveBeenCalledTimes(2 * timeoutCount)
+
+			vi.advanceTimersByTime(1)
+
+			expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(3 * timeoutCount + 3)
+			expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(3 * timeoutCount + 1, 'status', 'audioOn')
+			expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(3 * timeoutCount + 2, 'status', 'speaking')
+			expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(3 * timeoutCount + 3, 'status', 'videoOn')
+
+			expect(webRtc.sendTo).toHaveBeenCalledTimes(2 * timeoutCount + 2)
+			expect(webRtc.sendTo).toHaveBeenNthCalledWith(2 * timeoutCount + 1, 'thePeerId', 'unmute', { name: 'audio' })
+			expect(webRtc.sendTo).toHaveBeenNthCalledWith(2 * timeoutCount + 2, 'thePeerId', 'unmute', { name: 'video' })
+
+			timeoutCount++
+		})
+
+		vi.advanceTimersByTime(100000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(18)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(12)
+	})
+
+	test('change current state while sending initial state', () => {
+		vi.mocked(internalWebRtc.isAudioEnabled).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isSpeaking).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isVideoEnabled).mockReturnValue(true)
+
+		localStateBroadcasterMcu = new LocalStateBroadcasterMcu(webRtc, callParticipantCollection)
+
+		callParticipantCollection.add({ peerId: 'thePeerId', webRtc })
+
+		vi.advanceTimersByTime(1000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(6)
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(1, 'status', 'audioOn')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(2, 'status', 'speaking')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(3, 'status', 'videoOn')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(4, 'status', 'audioOn')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(5, 'status', 'speaking')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(6, 'status', 'videoOn')
+
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(4)
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(1, 'thePeerId', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(2, 'thePeerId', 'unmute', { name: 'video' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(3, 'thePeerId', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(4, 'thePeerId', 'unmute', { name: 'video' })
+
+		vi.mocked(internalWebRtc.isAudioEnabled).mockReturnValue(false)
+		vi.mocked(internalWebRtc.isSpeaking).mockReturnValue(false)
+		vi.mocked(internalWebRtc.isVideoEnabled).mockReturnValue(false)
+
+		vi.advanceTimersByTime(2000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(8)
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(7, 'status', 'audioOff')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(8, 'status', 'videoOff')
+
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(6)
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(5, 'thePeerId', 'mute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(6, 'thePeerId', 'mute', { name: 'video' })
+
+		// Note that, even if they are not explicitly checked, further messages
+		// would be nevertheless sent for a few more seconds.
+	})
+
+	test('add several participants', () => {
+		vi.mocked(internalWebRtc.isAudioEnabled).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isSpeaking).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isVideoEnabled).mockReturnValue(true)
+
+		localStateBroadcasterMcu = new LocalStateBroadcasterMcu(webRtc, callParticipantCollection)
+
+		callParticipantCollection.add({ peerId: 'thePeerId', webRtc })
+
+		vi.advanceTimersByTime(1000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(6)
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(1, 'status', 'audioOn')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(2, 'status', 'speaking')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(3, 'status', 'videoOn')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(4, 'status', 'audioOn')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(5, 'status', 'speaking')
+		expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(6, 'status', 'videoOn')
+
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(4)
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(1, 'thePeerId', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(2, 'thePeerId', 'unmute', { name: 'video' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(3, 'thePeerId', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(4, 'thePeerId', 'unmute', { name: 'video' })
+
+		callParticipantCollection.add({ peerId: 'thePeerId2', webRtc })
+
+		vi.advanceTimersByTime(3000)
+
+		// Data channel messages were reset when the second participant was
+		// added and sent at timeouts 0, 1 and 2 after that.
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(15)
+		for (let i = 0; i < 3; i++) {
+			expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(i * 3 + 1, 'status', 'audioOn')
+			expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(i * 3 + 2, 'status', 'speaking')
+			expect(webRtc.sendDataChannelToAll).toHaveBeenNthCalledWith(i * 3 + 3, 'status', 'videoOn')
+		}
+
+		// Total elapsed time and relative time for each peer:
+		// - 0: peer 1: 0, messages for timeout 0
+		// - 1: peer 1: 1, messages for timeout 1
+		//      peer 2: 0, messages for timeout 0
+		// - 2: peer 1: 2
+		//      peer 2: 1, messages for timeout 1
+		// - 3: peer 1: 3, messages for timeout 2 (0 + 1 + 2)
+		// -    peer 2: 2
+		// - 4: peer 1: 4
+		//      peer 2: 3, messages for timeout 2 (0 + 1 + 2)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(12)
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(5, 'thePeerId2', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(6, 'thePeerId2', 'unmute', { name: 'video' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(7, 'thePeerId2', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(8, 'thePeerId2', 'unmute', { name: 'video' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(9, 'thePeerId', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(10, 'thePeerId', 'unmute', { name: 'video' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(11, 'thePeerId2', 'unmute', { name: 'audio' })
+		expect(webRtc.sendTo).toHaveBeenNthCalledWith(12, 'thePeerId2', 'unmute', { name: 'video' })
+
+		vi.advanceTimersByTime(100000)
+
+		// Data channel messages at timeouts 0 and 1 since first participant was
+		// added, and then at timeouts 0, 1, 2, 4, 8 and 16 after second
+		// participant was added (2+6=8 blocks of messages in total).
+		// Signaling messages at timeouts 0, 1, 2, 4, 8  and 16 after each
+		// participant was added (6+6=12 blocks of messages in total).
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(24)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(24)
+	})
+
+	test('remove one of several participants while sending initial state', () => {
+		vi.mocked(internalWebRtc.isAudioEnabled).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isSpeaking).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isVideoEnabled).mockReturnValue(true)
+
+		localStateBroadcasterMcu = new LocalStateBroadcasterMcu(webRtc, callParticipantCollection)
+
+		callParticipantCollection.add({ peerId: 'thePeerId', webRtc })
+
+		vi.advanceTimersByTime(1000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(6)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(4)
+
+		callParticipantCollection.add({ peerId: 'thePeerId2', webRtc })
+
+		vi.advanceTimersByTime(3000)
+
+		// Data channel messages at timeouts 0 and 1 since first participant was
+		// added, and then at timeouts 0, 1 and 2 after second participant was
+		// added (2+3=5 blocks of messages in total).
+		// Signaling messages at timeouts 0, 1 and 2 after each participant was
+		// added (3+3=6 blocks of messages in total).
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(15)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(12)
+
+		callParticipantCollection.remove('thePeerId')
+
+		vi.advanceTimersByTime(100000)
+
+		// Data channel messages at timeouts 0 and 1 since first participant was
+		// added, and then at timeouts 0, 1, 2, 4, 8 and 16 after second
+		// participant was added (2+6=8 blocks of messages in total).
+		// Signaling messages at timeouts 0, 1 and 2 for first participant, and
+		// timeouts 0, 1, 2, 4, 8 and 16 for second participant (3+6=9 blocks of
+		// messages in total).
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(24)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(18)
+	})
+
+	test('remove the last participant', () => {
+		vi.mocked(internalWebRtc.isAudioEnabled).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isSpeaking).mockReturnValue(true)
+		vi.mocked(internalWebRtc.isVideoEnabled).mockReturnValue(true)
+
+		localStateBroadcasterMcu = new LocalStateBroadcasterMcu(webRtc, callParticipantCollection)
+
+		callParticipantCollection.add({ peerId: 'thePeerId', webRtc })
+
+		vi.advanceTimersByTime(1000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(6)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(4)
+
+		callParticipantCollection.remove('thePeerId')
+
+		vi.advanceTimersByTime(100000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(6)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(4)
+	})
+
+	test('destroy while sending initial state', () => {
+		localStateBroadcasterMcu = new LocalStateBroadcasterMcu(webRtc, callParticipantCollection)
+
+		callParticipantCollection.add({ peerId: 'thePeerId', webRtc })
+
+		vi.advanceTimersByTime(1000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(4)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(4)
+
+		localStateBroadcasterMcu.destroy()
+
+		vi.advanceTimersByTime(10000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(4)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(4)
+	})
+
+	test('add and remove participant after destroying', () => {
+		localStateBroadcasterMcu = new LocalStateBroadcasterMcu(webRtc, callParticipantCollection)
+
+		localStateBroadcasterMcu.destroy()
+
+		callParticipantCollection.add({ peerId: 'thePeerId', webRtc })
+
+		vi.advanceTimersByTime(10000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(0)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(0)
+
+		callParticipantCollection.remove('thePeerId')
+
+		vi.advanceTimersByTime(10000)
+
+		expect(webRtc.sendDataChannelToAll).toHaveBeenCalledTimes(0)
+		expect(webRtc.sendTo).toHaveBeenCalledTimes(0)
+	})
+})
---
+++ src/utils/webrtc/LocalStateBroadcasterMcu.ts
@@ -0,0 +1,195 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type {
+	CallParticipantCollection,
+	CallParticipantModel,
+	WebRtc,
+} from '../../types/index.ts'
+
+import { LocalStateBroadcaster } from './LocalStateBroadcaster.ts'
+import { ConnectionState } from './models/CallParticipantModel.js'
+
+/**
+ * Helper class to run a callback with an exponential backoff.
+ *
+ * The callback is run after 0, 1, 2, 4, 8 and 16 seconds. Running the callback
+ * can be aborted by calling "destroy()".
+ */
+class ExponentialBackoffCallback {
+	private _timeout?: ReturnType<typeof setTimeout> | null | undefined
+	private _callback: () => void
+
+	constructor(callback: () => void) {
+		this._callback = callback
+
+		this._runCallbackWithRepetition()
+	}
+
+	destroy(): void {
+		if (this._timeout) {
+			clearTimeout(this._timeout)
+		}
+
+		this._timeout = null
+	}
+
+	private _runCallbackWithRepetition(timeout?: number): void {
+		if (!timeout) {
+			timeout = 0
+		}
+
+		this._timeout = setTimeout(() => {
+			this._callback()
+
+			if (!timeout) {
+				timeout = 1000
+			} else {
+				timeout *= 2
+			}
+
+			if (timeout > 16000) {
+				this._timeout = null
+				return
+			}
+
+			this._runCallbackWithRepetition(timeout)
+		}, timeout)
+	}
+}
+
+/**
+ * Helper class to send the local participant state to the other participants in
+ * the call when an MCU is used.
+ *
+ * Sending the state when it changes is handled by the base class; this subclass
+ * only handles sending the initial state when a remote participant is added.
+ *
+ * When Janus is used data channel messages are sent to all remote participants
+ * (with a subscriber connection to receive from the local participant).
+ * Moreover, it is not possible to know when the remote participants open the
+ * data channel to receive the messages, or even when they establish the
+ * subscriber connection; it is only possible to know when the data channel is
+ * open for the publisher connection of the local participant. Due to all that
+ * the state is sent several times with an increasing delay whenever a
+ * participant joins the call. If the state was already being sent the sending
+ * is restarted with each new participant that joins.
+ *
+ * Similarly, in the case of signaling messages it is not possible either to
+ * know when the remote participants have "seen" the local participant and thus
+ * are ready to handle signaling messages about the state. However, in the case
+ * of signaling messages it is possible to send them to a specific participant,
+ * so the current state is sent several times with an increasing delay directly
+ * to the participant that joined. Moreover, if the participant leaves the call
+ * the state is no longer directly sent.
+ *
+ * Sending the data channel and signaling messages when a remote participant
+ * joins implicitly broadcasts the initial state when the local participant
+ * joins the call, as in that case all the remote participants joined from the
+ * point of view of the local participant. Unfortunately, as sending the data
+ * channel messages is restarted with each new participant that joins, the data
+ * channel messages are unnecessarily repeated for each participant in the call
+ * in that case, there is currently no special handling for that.
+ *
+ * In any case, note that the state is sent only when the remote participant
+ * joins the call. Even in case of temporary disconnections the normal state
+ * updates sent when the state changes are expected to be received by the
+ * other participant, as signaling messages are sent through a WebSocket and are
+ * therefore reliable. Moreover, even if the WebSocket is restarted and the
+ * connection resumed (rather than joining with a new session ID) the messages
+ * would be also received, as in that case they would be queued until the
+ * WebSocket is connected again.
+ *
+ * Data channel messages, on the other hand, could be lost if the remote
+ * participant restarts the peer receiver connection (although they would be
+ * received in case of temporary disconnections, as data channels use a reliable
+ * transport by default). Therefore, as the speaking state is sent only through
+ * data channels, updates of the speaking state could be not received by remote
+ * participants.
+ */
+export class LocalStateBroadcasterMcu extends LocalStateBroadcaster {
+	private _sendStateWithRepetition?: ExponentialBackoffCallback | null
+	private _sendStateWithRepetitionToParticipant: Map<string, ExponentialBackoffCallback>
+
+	public constructor(webRtc: WebRtc, callParticipantCollection: CallParticipantCollection) {
+		super(webRtc, callParticipantCollection)
+
+		this._sendStateWithRepetition = null
+		this._sendStateWithRepetitionToParticipant = new Map()
+	}
+
+	public destroy(): void {
+		super.destroy()
+
+		this._sendStateWithRepetition?.destroy()
+
+		this._sendStateWithRepetitionToParticipant.forEach((sendStateWithRepetitionToParticipant) => {
+			sendStateWithRepetitionToParticipant.destroy()
+		})
+	}
+
+	protected _handleAddCallParticipantModel(callParticipantCollection: CallParticipantCollection, callParticipantModel: CallParticipantModel): void {
+		this._sendStateWithRepetition?.destroy()
+
+		this._sendStateWithRepetition = new ExponentialBackoffCallback(() => {
+			this._sendCurrentMediaStateToAll()
+		})
+
+		const peerId = callParticipantModel.get('peerId') as string
+
+		this._sendStateWithRepetitionToParticipant.get(peerId)?.destroy()
+
+		this._sendStateWithRepetitionToParticipant.set(peerId, new ExponentialBackoffCallback(() => {
+			this._sendCurrentMediaStateTo(peerId)
+		}))
+	}
+
+	protected _handleRemoveCallParticipantModel(callParticipantCollection: CallParticipantCollection, callParticipantModel: CallParticipantModel): void {
+		if (callParticipantCollection.callParticipantModels.length === 0 && this._sendStateWithRepetition) {
+			this._sendStateWithRepetition.destroy()
+			this._sendStateWithRepetition = null
+		}
+
+		const peerId = callParticipantModel.get('peerId') as string
+
+		this._sendStateWithRepetitionToParticipant.get(peerId)?.destroy()
+
+		this._sendStateWithRepetitionToParticipant.delete(peerId)
+	}
+
+	private _sendCurrentMediaStateToAll(): void {
+		if (!this._webRtc.webrtc.isAudioEnabled()) {
+			this._webRtc.sendDataChannelToAll('status', 'audioOff')
+		} else {
+			this._webRtc.sendDataChannelToAll('status', 'audioOn')
+
+			if (!this._webRtc.webrtc.isSpeaking()) {
+				this._webRtc.sendDataChannelToAll('status', 'stoppedSpeaking')
+			} else {
+				this._webRtc.sendDataChannelToAll('status', 'speaking')
+			}
+		}
+
+		if (!this._webRtc.webrtc.isVideoEnabled()) {
+			this._webRtc.sendDataChannelToAll('status', 'videoOff')
+		} else {
+			this._webRtc.sendDataChannelToAll('status', 'videoOn')
+		}
+	}
+
+	private _sendCurrentMediaStateTo(peerId: string): void {
+		if (!this._webRtc.webrtc.isAudioEnabled()) {
+			this._webRtc.sendTo(peerId, 'mute', { name: 'audio' })
+		} else {
+			this._webRtc.sendTo(peerId, 'unmute', { name: 'audio' })
+		}
+
+		if (!this._webRtc.webrtc.isVideoEnabled()) {
+			this._webRtc.sendTo(peerId, 'mute', { name: 'video' })
+		} else {
+			this._webRtc.sendTo(peerId, 'unmute', { name: 'video' })
+		}
+	}
+}
