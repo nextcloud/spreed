5e195b3443f8c829934133fda9f83fc7435cc246
Dorra Jaouad <dorra.jaoued7@gmail.com>
2026-02-05 17:09:03 +0100

    fix(FilePreview): adjust loading bar for outgoing messages
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/components/MessagesList/MessagesGroup/Message/MessageItem.vue
+++ src/components/MessagesList/MessagesGroup/Message/MessageItem.vue
@@ -505,6 +505,10 @@ export default {
 				background-color: var(--color-primary-element-light-hover);
 				border: 1px solid var(--color-border-dark);
 			}
+
+			:deep(.file-preview__progress) {
+				inset-inline-start: calc((var(--progress-bar-height) + var(--default-grid-baseline) * 3) * -1)
+			}
 		}
 
 		&.incoming {
