6166c8670c884cb3bdcb2136891752e2591b249b
Joas Schilling <coding@schilljs.com>
2026-02-20 00:02:44 +0100

    fixup! fix(commands): Improve phone number input validation of OCC commands

--- lib/Service/PhoneNumberValidation.php
+++ lib/Service/PhoneNumberValidation.php
@@ -8,14 +8,8 @@ declare(strict_types=1);
 
 namespace OCA\Talk\Service;
 
-use OCA\Talk\Model\PhoneNumber;
-use OCA\Talk\Model\PhoneNumberMapper;
-use OCP\AppFramework\Db\DoesNotExistException;
 use OCP\IConfig;
 use OCP\IPhoneNumberUtil;
-use OCP\IUser;
-use OCP\IUserManager;
-use OCP\User\IAvailabilityCoordinator;
 
 class PhoneNumberValidation {
 
