ba6e346773f165d069ffdd673eb943fa85e26ff8
Joas Schilling <coding@schilljs.com>
2026-02-20 08:48:22 +0100

    fix(sip): Check for invalid phone numbers in setupcheck
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/SetupCheck/SIPConfiguration.php
+++ lib/SetupCheck/SIPConfiguration.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace OCA\Talk\SetupCheck;
 
 use OCA\Talk\Config;
+use OCP\IDBConnection;
 use OCP\IL10N;
 use OCP\SetupCheck\ISetupCheck;
 use OCP\SetupCheck\SetupResult;
@@ -16,6 +17,7 @@ use OCP\SetupCheck\SetupResult;
 class SIPConfiguration implements ISetupCheck {
 	public function __construct(
 		protected readonly Config $talkConfig,
+		protected readonly IDBConnection $connection,
 		protected readonly IL10N $l,
 	) {
 	}
@@ -39,9 +41,35 @@ class SIPConfiguration implements ISetupCheck {
 		if ($this->talkConfig->getSignalingMode() === Config::SIGNALING_INTERNAL) {
 			return SetupResult::success($this->l->t('Using the SIP functionality requires a High-performance backend.'));
 		}
+
+		$query = $this->connection->getQueryBuilder();
+		$query->select('phone_number')
+			->from('talk_phone_numbers')
+			->where($query->expr()->like('phone_number', $query->createNamedParameter(
+				$this->connection->escapeLikeParameter('+') . '%'
+			)))
+			->orWhere($query->expr()->like('phone_number', $query->createNamedParameter(
+				$this->connection->escapeLikeParameter('00') . '%'
+			)));
+
+		$result = $query->executeQuery();
+		$invalidNumbers = $result->fetchFirstColumn();
+		$result->closeCursor();
+
+		if (!empty($invalidNumbers)) {
+			$message = $this->l->t("Assigned Talk phone numbers must not start with + or 00. Please remove the following numbers:\n{list}");
+			$message = str_replace(
+				'{list}',
+				implode("\n", $invalidNumbers),
+				$message
+			);
+			return SetupResult::error($message, 'https://portal.nextcloud.com/article/Nextcloud-Talk/Nextcloud-Talk-Phone/Direct-Dial-in#content-provisioning');
+		}
+
 		if ($this->talkConfig->getSIPSharedSecret() === '' && $this->talkConfig->getDialInInfo() === '') {
 			return SetupResult::info($this->l->t('No SIP backend configured'));
 		}
+
 		return SetupResult::success();
 	}
 }
