c9bbc40d05499729402f42dba03fb172bc1488f4
Joas Schilling <coding@schilljs.com>
2026-03-05 17:01:06 +0100

    chore(assets): Recompile assets
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/types/openapi/openapi-backend-recording.ts
+++ src/types/openapi/openapi-backend-recording.ts
@@ -74,6 +74,10 @@ export type components = {
                     "live-transcription": boolean;
                     "live-translation": boolean;
                     "live-transcription-target-language-id": string;
+                    "play-sounds": boolean;
+                    /** Format: int64 */
+                    "grid-limit": number;
+                    "grid-limit-enforced": boolean;
                 };
                 chat: {
                     /** Format: int64 */
@@ -88,6 +92,7 @@ export type components = {
                     "summary-threshold": number;
                     /** @enum {string} */
                     style: "split" | "unified";
+                    "matterbridge-enabled": boolean;
                 };
                 conversations: {
                     "can-create": boolean;
@@ -116,6 +121,8 @@ export type components = {
                 signaling: {
                     /** Format: int64 */
                     "session-ping-limit": number;
+                    /** @enum {string} */
+                    mode: "internal" | "external" | "conversation_cluster";
                     "hello-v2-token-key"?: string;
                 };
                 experiments: {
@@ -127,6 +134,8 @@ export type components = {
                     "max-default": number;
                     /** Format: int64 */
                     "max-custom": number;
+                    /** Format: int64 */
+                    default: number;
                 };
             };
             "config-local": {
--- src/types/openapi/openapi-backend-signaling.ts
+++ src/types/openapi/openapi-backend-signaling.ts
@@ -60,6 +60,10 @@ export type components = {
                     "live-transcription": boolean;
                     "live-translation": boolean;
                     "live-transcription-target-language-id": string;
+                    "play-sounds": boolean;
+                    /** Format: int64 */
+                    "grid-limit": number;
+                    "grid-limit-enforced": boolean;
                 };
                 chat: {
                     /** Format: int64 */
@@ -74,6 +78,7 @@ export type components = {
                     "summary-threshold": number;
                     /** @enum {string} */
                     style: "split" | "unified";
+                    "matterbridge-enabled": boolean;
                 };
                 conversations: {
                     "can-create": boolean;
@@ -102,6 +107,8 @@ export type components = {
                 signaling: {
                     /** Format: int64 */
                     "session-ping-limit": number;
+                    /** @enum {string} */
+                    mode: "internal" | "external" | "conversation_cluster";
                     "hello-v2-token-key"?: string;
                 };
                 experiments: {
@@ -113,6 +120,8 @@ export type components = {
                     "max-default": number;
                     /** Format: int64 */
                     "max-custom": number;
+                    /** Format: int64 */
+                    default: number;
                 };
             };
             "config-local": {
