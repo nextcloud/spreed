b6ad56fb492bf2b60a66c2422fad84699fc3efae
Joas Schilling <coding@schilljs.com>
2026-03-05 13:38:09 +0100

    fix(initial-state): Removed unused federation state - Replaced by capability config => federation
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/TInitialState.php
+++ lib/TInitialState.php
@@ -63,11 +63,6 @@ trait TInitialState {
 			'grid_videos_limit_enforced',
 			$this->talkConfig->getGridVideosLimitEnforced()
 		);
-
-		$this->initialState->provideInitialState(
-			'federation_enabled',
-			$this->talkConfig->isFederationEnabled()
-		);
 	}
 
 	protected function publishInitialStateForUser(IUser $user, IRootFolder $rootFolder, IAppManager $appManager): void {
