3c5a1c092c8d17919c1e2f9beadaf6a9ef673480
Joas Schilling <coding@schilljs.com>
2026-03-10 08:45:55 +0100

    fix(capabilities): be more specific about the return values
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ tests/stubs/oca_guests.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OCA\Guests {
+	class UserBackend implements \OCP\IUserBackend {
+	}
+}
