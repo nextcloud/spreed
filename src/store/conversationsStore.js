b274542e5201f0c6b299071a3d0a3b2dda540d6c
Maksim Sukharev <antreesy.web@gmail.com>
2026-04-07 17:45:45 +0200

    fix: lint code with new rules
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/store/conversationsStore.js
+++ src/store/conversationsStore.js
@@ -1273,7 +1273,7 @@ const actions = {
 			context.commit('addConversation', conversation)
 			showSuccess(t('spreed', 'Conversation picture set'))
 		} catch (error) {
-			throw new Error(error.response?.data?.ocs?.data?.message ?? error.message)
+			throw new Error(error.response?.data?.ocs?.data?.message ?? error.message, { cause: error })
 		}
 	},
 
@@ -1284,7 +1284,7 @@ const actions = {
 			context.commit('addConversation', conversation)
 			showSuccess(t('spreed', 'Conversation picture set'))
 		} catch (error) {
-			throw new Error(error.response?.data?.ocs?.data?.message ?? error.message)
+			throw new Error(error.response?.data?.ocs?.data?.message ?? error.message, { cause: error })
 		}
 	},
 
