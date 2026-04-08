07cdc84591215c3f4876b83464137ceeb65d80a8
Maksim Sukharev <antreesy.web@gmail.com>
2026-04-08 15:54:28 +0200

    fix(typescript): declare OCA, OCP, OC under Window interface
    - document OCA.Talk and OCA.Viewer types
    
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,10 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 import type { getCSPNonce } from '@nextcloud/auth'
+import type { createApp, defineCustomElement } from 'vue'
+import type { createStore } from 'vuex'
+import type { SettingsAPI } from './services/SettingsAPI.ts'
+import type MediaDevicesManager from './utils/webrtc/MediaDevicesManager.js'
 
 type ExitFullscreen = typeof document.exitFullscreen
 type RequestFullscreen = typeof document.documentElement.requestFullscreen
@@ -16,20 +20,71 @@ declare global {
 		webkitRequestFullscreen: RequestFullscreen
 	}
 
-	const OCP: {
-		AppConfig: {
-			setValue: (app: string, key: string, value: string | number | boolean, options?: { success?: () => void, error?: () => void }) => void
+	interface Window {
+		OCP: {
+			AppConfig: {
+				setValue: (app: string, key: string, value: string | number | boolean, options?: { success?: () => void, error?: () => void }) => void
+			}
+			Accessibility: {
+				disableKeyboardShortcuts: () => boolean
+			}
 		}
-		Accessibility: {
-			disableKeyboardShortcuts: () => boolean
+
+		OC: {
+			MimeType: {
+				getIconUrl: (mimetype?: string) => string | undefined
+			}
+		}
+
+		OCA: {
+			Talk: {
+				/** Vue app instance and optional destroyer */
+				instance?: ReturnType<createApp>
+				unmountInstance?: () => void
+				newTab?: defineCustomElement
+
+				/** Public API */
+				Settings?: SettingsAPI
+
+				/** Exposed internals */
+				store?: ReturnType<createStore>
+				mediaDevicesManager?: ReturnType<MediaDevicesManager>
+				SimpleWebRTC?: undefined
+
+				/** Added by Desktop client */
+				Desktop?: IS_DESKTOP extends true
+					? {
+							getDesktopMediaSource?: () => void
+						}
+					: never
+
+				/** Exposed functions */
+				signalingGetSettingsForRecording?: () => void
+				signalingJoinCallForRecording?: () => void
+				signalingKill?: () => void
+				registerMessageAction?: () => void
+				registerParticipantSearchAction?: () => void
+				gridDebugInformation?: () => void
+				gridDevModeEnable?: () => void
+
+				fetchCurrentConversationIntervalId?: NodeJS.Timeout | undefined
+				fetchCurrentConversationWrapper?: () => Promise<void>
+				fetchCurrentConversationBound?: () => Promise<void>
+			}
+
+			/** Public API */
+			Viewer: {
+				open: () => void
+				close: () => void
+				mimetypes: string[]
+				availableHandlers: Record<string, unknown>[]
+			}
 		}
 	}
 
-	const OC: {
-		MimeType: {
-			getIconUrl: (mimetype?: string) => string | undefined
-		}
-	}
+	const OCP: Window['OCP']
+	const OC: Window['OC']
+	const OCA: Window['OCA']
 
 	declare module '*.svg?raw' {
 		const content: string
@@ -43,7 +98,7 @@ declare global {
 	/**
 	 * Build constant to divide build for web app and desktop client
 	 */
-	const IS_DESKTOP: false
+	const IS_DESKTOP: boolean
 
 	let __webpack_nonce__: ReturnType<typeof getCSPNonce>
 	let __webpack_public_path__: string
