090a23530bf6d08a03389205eee0aff2b1813386
Maksim Sukharev <antreesy.web@gmail.com>
2026-05-07 11:04:24 +0200

    fix: set up mainFilesSidebar*.js files for async instance mount
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- rspack.config.js
+++ rspack.config.js
@@ -44,10 +44,7 @@ module.exports = defineConfig((env) => {
 			collections: path.join(__dirname, 'src', 'collections.js'),
 			main: path.join(__dirname, 'src', 'main.js'),
 			recording: path.join(__dirname, 'src', 'mainRecording.js'),
-			'files-sidebar': [
-				path.join(__dirname, 'src', 'mainFilesSidebar.js'),
-				path.join(__dirname, 'src', 'mainFilesSidebarLoader.js'),
-			],
+			'files-sidebar': path.join(__dirname, 'src', 'mainFilesSidebarLoader.js'),
 			'public-share-auth-form': path.join(__dirname, 'src', 'publicShareAuthForm.ts'),
 			'public-share-auth-sidebar': path.join(__dirname, 'src', 'mainPublicShareAuthSidebar.js'),
 			'public-share-sidebar': path.join(__dirname, 'src', 'mainPublicShareSidebar.js'),
--- src/mainFilesSidebarLoader.js
+++ src/mainFilesSidebarLoader.js
@@ -3,13 +3,24 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import { getCSPNonce } from '@nextcloud/auth'
 import { FileType, getSidebar } from '@nextcloud/files'
+import { generateFilePath } from '@nextcloud/router'
+import { defineAsyncComponent, defineCustomElement } from 'vue'
 import IconTalk from '../img/app-dark.svg?raw'
-import { initializeTalkOnce } from './init.js'
 
-initializeTalkOnce()
+// CSP config for webpack dynamic chunk loading
+__webpack_nonce__ = getCSPNonce()
+
+// Correct the root of the app for chunk loading
+// OC.linkTo matches the apps folders
+// OC.generateUrl ensure the index.php (or not)
+// We do not want the index.php since we're loading files
+__webpack_public_path__ = generateFilePath('spreed', '', 'js/')
 
 const TAB_TAG_NAME = 'talk-files_sidebar_tab'
+
+const FilesSidebarLoaderApp = defineAsyncComponent(() => import('./FilesSidebarLoaderApp.vue'))
 
 getSidebar()?.registerTab({
 	id: 'chat',
@@ -19,6 +30,6 @@ getSidebar()?.registerTab({
 	enabled: ({ node }) => node.type === FileType.File,
 	tagName: TAB_TAG_NAME,
 	onInit() {
-		window.customElements.define(TAB_TAG_NAME, OCA.Talk.newTab())
+		window.customElements.define(TAB_TAG_NAME, defineCustomElement(FilesSidebarLoaderApp, { shadowRoot: false }))
 	},
 })
