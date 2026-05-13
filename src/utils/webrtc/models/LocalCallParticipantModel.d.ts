97685b446ec6f177a2bd8af7548fc1ff982a28d2
Maksim Sukharev <antreesy.web@gmail.com>
2026-05-13 15:33:47 +0200

    chore(ts): declare WebRTC models with individual d.ts files instead of env.d.ts augmentation - workaround before models are migrated to .ts themselves
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

---
+++ src/utils/webrtc/models/LocalCallParticipantModel.d.ts
@@ -0,0 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { LocalCallParticipantModel } from '../../../types/index.ts'
+
+export declare const LocalCallParticipantModel: new () => LocalCallParticipantModel
