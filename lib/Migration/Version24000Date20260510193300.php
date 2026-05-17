3bc6cb7c66966c4f1b7c6aef02418b5163279a41
Christian Lorang <madcatcl2@gmx.de>
2026-05-17 17:14:19 +0200

    chore: fix git merge gone wrong
    Signed-off-by: Christian Lorang <madcatcl2@gmx.de>

--- lib/Migration/Version24000Date20260510193300.php
+++ lib/Migration/Version24000Date20260510193300.php
@@ -42,26 +42,8 @@ class Version24000Date20260510193300 extends SimpleMigrationStep {
 			$table->addColumn('last_metadata_activity', Types::DATETIME, [
 				'notnull' => false,
 			]);
-<<<<<<< HEAD
-<<<<<<< HEAD
 			$table->addIndex(['last_metadata_activity'], 'talkroom_lastmetadataactive');
 
-		}
-		
-		$table = $schema->getTable('talk_threads');
-
-		if (!$table->hasColumn('last_metadata_activity')) {
-			$table->addColumn('last_metadata_activity', Types::DATETIME, [
-				'notnull' => false,
-			]);
-			$table->addIndex(['last_metadata_activity'], 'talkthread_lastmetadataactive');
-=======
-			$table->addIndex(['last_metadata_activity'], 'talkthread_lastmetadataactive');
-=======
-			$table->addIndex(['last_metadata_activity'], 'talkroom_lastmetadataactive');
->>>>>>> a798ce9366 (change(conversations): change behavior in all necessary places for Rooms and Threads to use / set lastMetadataActivity instaed of lastActivity where appropriate, i.e. where system messages are signalled and not real chat messages.)
-
->>>>>>> 2ed52550f0 (feature(api): Add a new field and corresponding functionality for lastMetadaActivity for Rooms and Threads, similar to lastActivity. This also adds a database migration for the oc_talk_rooms and oc_talk_threads tables as well. Functions are introduced and prepared for later use. The use of the field lastActivity to signal when the last real message in a conversation appeared remains unchanged to keep the API stable. The intended use of this feature is to better distinguish between real messages (lastActivity) to notify and bump conversations in the thread list to the top, and other status / metadata related messages (lastMetadataActivity) like room state and participant list changes that shall get synced and be updated in the clients, but not trigger an activity bump of its conversations in the thread list.)
 		}
 		
 		$table = $schema->getTable('talk_threads');
@@ -83,10 +65,6 @@ class Version24000Date20260510193300 extends SimpleMigrationStep {
 	 */
 	#[Override]
 	public function postSchemaChange(IOutput $output, \Closure $schemaClosure, array $options) : void {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a798ce9366 (change(conversations): change behavior in all necessary places for Rooms and Threads to use / set lastMetadataActivity instaed of lastActivity where appropriate, i.e. where system messages are signalled and not real chat messages.)
 		$update = $this->connection->getQueryBuilder();
 		$update->update('talk_rooms')
 			->set('last_metadata_activity', 'last_activity');
@@ -95,15 +73,6 @@ class Version24000Date20260510193300 extends SimpleMigrationStep {
 		$update->update('talk_threads')
 			->set('last_metadata_activity', 'last_activity');
 		$update->executeStatement();
-<<<<<<< HEAD
-=======
-	   $update = $this->connection->getQueryBuilder();
-	   $update->update('talk_rooms')
-               ->set('last_metadata_activity', 'last_activity');
-       $update->executeStatement();
->>>>>>> 2ed52550f0 (feature(api): Add a new field and corresponding functionality for lastMetadaActivity for Rooms and Threads, similar to lastActivity. This also adds a database migration for the oc_talk_rooms and oc_talk_threads tables as well. Functions are introduced and prepared for later use. The use of the field lastActivity to signal when the last real message in a conversation appeared remains unchanged to keep the API stable. The intended use of this feature is to better distinguish between real messages (lastActivity) to notify and bump conversations in the thread list to the top, and other status / metadata related messages (lastMetadataActivity) like room state and participant list changes that shall get synced and be updated in the clients, but not trigger an activity bump of its conversations in the thread list.)
-=======
->>>>>>> a798ce9366 (change(conversations): change behavior in all necessary places for Rooms and Threads to use / set lastMetadataActivity instaed of lastActivity where appropriate, i.e. where system messages are signalled and not real chat messages.)
 	}	
 
 }
