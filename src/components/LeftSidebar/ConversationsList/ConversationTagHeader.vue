aefb7b0c1aa87fabfa92b7958e90f858978c03ed
Maksim Sukharev <antreesy.web@gmail.com>
2026-07-08 17:56:58 +0200

    feat(tags): allow to mark all tagged conversation as read
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/LeftSidebar/ConversationsList/ConversationTagHeader.vue
+++ src/components/LeftSidebar/ConversationsList/ConversationTagHeader.vue
@@ -14,11 +14,13 @@ import NcActionButton from '@nextcloud/vue/components/NcActionButton'
 import NcActionSeparator from '@nextcloud/vue/components/NcActionSeparator'
 import NcAppNavigationItem from '@nextcloud/vue/components/NcAppNavigationItem'
 import NcCounterBubble from '@nextcloud/vue/components/NcCounterBubble'
+import NcIconSvgWrapper from '@nextcloud/vue/components/NcIconSvgWrapper'
 import IconArrowDown from 'vue-material-design-icons/ArrowDown.vue'
 import IconArrowUp from 'vue-material-design-icons/ArrowUp.vue'
 import IconPencilOutline from 'vue-material-design-icons/PencilOutline.vue'
 import IconTrashCanOutline from 'vue-material-design-icons/TrashCanOutline.vue'
 import ConfirmDialog from '../../UIShared/ConfirmDialog.vue'
+import IconMarkChatRead from '../../../../img/material-icons/mark-chat-read.svg?raw'
 import { useConversationTagsStore } from '../../../stores/conversationTags.ts'
 
 export type TagHeaderItem = ConversationTag & {
@@ -85,6 +87,34 @@ async function handleDeleteTag() {
 		}
 	}
 }
+
+/**
+ * Clear last read message of a tag from all conversations
+ */
+async function handleMarkReadTag() {
+	const conversations = (vuexStore.getters.conversationsList as Conversation[]).filter((conversation) => {
+		if (!conversation.unreadMessages) {
+			return false
+		}
+
+		switch (props.item.type) {
+			case 'favorites': {
+				return conversation.isFavorite
+			}
+			case 'other': {
+				return !conversation.isFavorite && !conversation.tagIds?.length
+			}
+			case 'custom':
+			default: {
+				return conversation.tagIds?.includes(props.item.id)
+			}
+		}
+	})
+
+	for (const conversation of conversations) {
+		vuexStore.dispatch('clearLastReadMessage', { token: conversation.token })
+	}
+}
 </script>
 
 <template>
@@ -102,6 +132,15 @@ async function handleDeleteTag() {
 			<NcCounterBubble v-if="item.unreadCount > 0" :count="item.unreadCount" />
 		</template>
 		<template #actions>
+			<NcActionButton
+				v-if="item.unreadCount > 0"
+				closeAfterClick
+				@click="handleMarkReadTag">
+				<template #icon>
+					<NcIconSvgWrapper :svg="IconMarkChatRead" :size="20" />
+				</template>
+				{{ t('spreed', 'Mark as read') }}
+			</NcActionButton>
 			<template v-if="isCustomTag">
 				<NcActionButton closeAfterClick @click="handleRenameTag">
 					<template #icon>
