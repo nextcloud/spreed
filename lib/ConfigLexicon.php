da24493c394595102053e8b2da2df53f59b2f05f
Christian Lorang <christian.lorang@nextcloud.com>
2026-07-23 17:27:52 +0200

    chore(config): refactor ALLOWED_GROUPS_FEDERATION ConfigLexicon entry and settings.md addition
    Signed-off-by: Christian Lorang <christian.lorang@nextcloud.com>

--- lib/ConfigLexicon.php
+++ lib/ConfigLexicon.php
@@ -30,7 +30,8 @@ class ConfigLexicon implements ILexicon {
 			new Entry(SignalingManager::HAS_FEATURE_CHANGED_USERS, ValueType::BOOL, false),
 			new Entry(Config::ALLOWED_GROUPS_TALK, ValueType::ARRAY, [], definition: 'List of group ids that are allowed to use Talk'),
 			new Entry(Config::ALLOWED_GROUPS_SIP, ValueType::ARRAY, [], definition: 'List of group ids that are allowed to enable SIP dial-in in a conversation'),
-			new Entry(Config::FEDERATION_ENABLED, ValueType::BOOL, definition: 'Whether or not federation with this instance is allowed'),
+			new Entry(Config::ALLOWED_GROUPS_FEDERATION, ValueType::ARRAY, [], definition: 'List of local group ids that are allowed to use federated features'),
+			new Entry(Config::FEDERATION_ENABLED, ValueType::BOOL, false, definition: 'Whether or not federation with this instance is allowed'),
 		];
 	}
 
