1c7b074bbbc0724c4e3ad0f9c2bd91f82a1f03b4
Joas Schilling <coding@schilljs.com>
2026-09-03 14:11:06 +0200

    chore(behat): Migrate Contexts to real classes for behat4
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/integration/Contexts/RecordingTrait.php
+++ tests/integration/Contexts/RecordingTrait.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+namespace OCA\Talk\Tests\Integration\Contexts;
+
 use Behat\Gherkin\Node\TableNode;
 use Behat\Hook\AfterScenario;
 use Behat\Step\Given;
@@ -165,8 +167,8 @@ trait RecordingTrait {
 
 		$descriptorSpec = [
 			0 => $stdin,
-			1 => [ 'file', $stdoutf, 'a' ],
-			2 => [ 'file', $stderrf, 'a' ],
+			1 => ['file', $stdoutf, 'a'],
+			2 => ['file', $stderrf, 'a'],
 		];
 
 		$process = proc_open($fullCmd, $descriptorSpec, $pipes, $cwd, $env, [
@@ -365,7 +367,7 @@ trait RecordingTrait {
 				$write = true;
 			}
 			if (isset($actualDataJson['participants']['users'])) {
-				usort($actualDataJson['participants']['users'], static fn (array $u1, array $u2) => $u1['userId'] <=> $u2['userId']);
+				usort($actualDataJson['participants']['users'], static fn(array $u1, array $u2) => $u1['userId'] <=> $u2['userId']);
 				$write = true;
 			}
 			if (isset($actualDataJson['message']['data']['chat']['comment'])) {
