dae5186d98470287b718986897456f8f01db2d60
Christian Lorang <christian.lorang@nextcloud.com>
2026-09-14 15:24:18 +0200

    chore(config): refactor summary_threshhold to AppConfig, fix tests
    Signed-off-by: Christian Lorang <christian.lorang@nextcloud.com>

--- lib/Config.php
+++ lib/Config.php
@@ -77,6 +77,7 @@ class Config {
 	public const string MATTERBRIDGE_ENABLED = 'enable_matterbridge';
 	public const string DELETE_ONE_TO_ONE_CONVERSATIONS = 'delete_one_to_one_conversations';
 	public const string MAX_GIF_SIZE = 'max_gif_size';
+	public const string SUMMARY_THRESHOLD = 'summary_threshold';
 
 	/**
 	 * 1. Call recording, …
