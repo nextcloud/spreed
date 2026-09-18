6ee31dadf2d78a5388b43338a108da4e407cff19
Marcel Müller <marcel-mueller@gmx.de>
2026-09-18 18:05:15 +0200

    fix(sip): Add fallback for old sip bridges
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- lib/Controller/RoomController.php
+++ lib/Controller/RoomController.php
@@ -2582,8 +2582,23 @@ class RoomController extends AEnvironmentAwareOCSController {
 		try {
 			$entity = $this->phoneService->getAccountToCallForPhoneNumber($phoneNumber);
 		} catch (DoesNotExistException) {
-			$this->logger->info('No account found for direct dial-in with number: ' . $phoneNumber);
-			return new DataResponse(null, Http::STATUS_NOT_FOUND);
+			$dialoutPrefix = $this->appConfig->getAppValueString('sip_bridge_dialout_prefix', '+');
+
+			if ($dialoutPrefix === '') {
+				$this->logger->info('No account found for direct dial-in with number: ' . $phoneNumber);
+				return new DataResponse(null, Http::STATUS_NOT_FOUND);
+			}
+
+			try {
+				// Try to find the number with added dialout prefix as a fallback for old sip-bridges
+				// TODO: Log as error in 26, remove fallback and dialout prefix with 27
+				$entity = $this->phoneService->getAccountToCallForPhoneNumber($dialoutPrefix . $phoneNumber);
+			} catch (DoesNotExistException) {
+				$this->logger->info('No account found for direct dial-in with number: ' . $phoneNumber);
+				return new DataResponse(null, Http::STATUS_NOT_FOUND);
+			}
+
+			$this->logger->warning('Found account for direct dial-in only by adding the dial-out prefix. Please adjust the mapping for number: ' . $phoneNumber);
 		}
 
 		$caller = trim($caller);
